92135158b4bb868514dfcb71d62cad0b894162c5
Dzmitry Kurachkin <dzmitry_kurachkin@epam.com>
2016-09-21 11:05:07 +0300

    update defect editor

--- src/main/resources/public/js/src/launches/stepLevel/StepItemIssueView.js
+++ src/main/resources/public/js/src/launches/stepLevel/StepItemIssueView.js
@@ -28,19 +28,52 @@ define(function (require, exports, module) {
     var App = require('app');
     var Localization = require('localization');
     var Textile = require('textile');
+    var CoreService = require('coreService');
     var SingletonDefectTypeCollection = require('defectType/SingletonDefectTypeCollection');
 
     var config = App.getInstance();
 
-
     var StepItemIssueView = Epoxy.View.extend({
         template: 'tpl-launch-step-issue',
-        events: {},
+        ticketsTpl: 'tpl-launch-step-issue-tickets',
+        events: {
+            'click [data-js-issue-remove-ticket]': 'removeTicket'
+        },
         bindings: {
             '[data-js-issue-type]': 'classes:{disabled:disableIssue}, attr:{title: switchTitle}',
-            '[data-js-issue-checkbox]': 'attr:{disabled: disableIssue}'
+            '[data-js-issue-checkbox]': 'attr:{disabled: disableIssue}',
+            '[data-js-issue-name]': 'text: setIssueName',
+            '[data-js-issue-title]': 'attr: {title: setIssueTitle}',
+            '[data-js-issue-color]': 'attr: {style: setIssueColor}',
+            '[data-js-issue-comment]': 'html: setComment',
+            '[data-js-issue-tickets]': 'html: attachTickets'
         },
         computeds: {
+            setIssueName: function(){
+                var issue = this.getBinding('issue'),
+                    defect = this.getDefectType();
+                return defect.longName.setMaxLength(20);
+            },
+            setIssueTitle: function(){
+                var issue = this.getBinding('issue'),
+                    defect = this.getDefectType();
+                return defect.longName;
+            },
+            setIssueColor: function(){
+                var issue = this.getBinding('issue'),
+                    defect = this.getDefectType();
+                return 'background: ' + defect.color;
+            },
+            setComment: function(){
+                var issue = this.getBinding('issue'),
+                    comment = this.getComment();
+                return Textile(comment.replace('*IssueDescription:*',"<br>*IssueDescription:*")).replaceNewLines().escapeScript();
+            },
+            attachTickets: function(){
+                var issue = this.getBinding('issue'),
+                    tickets = this.getTickets();
+                return Util.templates(this.ticketsTpl, {tickets: tickets});
+            },
             disableIssue: function(){
                 return this.inProgress();
             },
@@ -57,15 +90,14 @@ define(function (require, exports, module) {
         },
         initialize: function(options) {
             this.$container = options.$container;
-            this.render();
+            this.defetTypesCollection = new SingletonDefectTypeCollection();
+            this.defetTypesCollection.ready.done(function(){
+                this.render();
+            }.bind(this));
         },
         render: function() {
             this.$el.html(Util.templates(this.template, {
                 model: this.model.toJSON({computed: true}),
-                issueType: this.getIssueType(),
-                btsIssues: this.getBtsIssues(),
-                comment: this.getComment(),
-                textile: Textile,
                 needMatchIssue: this.needMatchIssue()
             }));
             this.$container.append(this.$el);
@@ -73,23 +105,49 @@ define(function (require, exports, module) {
         needMatchIssue: function(){
             return this.model.get('level') === 'LOG';
         },
-        getIssue: function(){
-            var issue = this.model.get('issue');
+        getIssueType: function(){
+            var issue = this.model.getIssue();
             return issue ? issue.issue_type : null;
         },
-        getIssueType: function(){
-            var issue = this.getIssue(),
-                defects = new SingletonDefectTypeCollection(),
-                defect = issue ? defects.getDefectType(issue) : null;
+        getDefectType: function(){
+            var issue = this.getIssueType(),
+                defect = issue ? this.defetTypesCollection.getDefectType(issue) : null;
             return defect;
         },
-        getBtsIssues: function(){
-            var issue = this.model.get('issue');
-            return issue ? issue.externalSystemIssues : null;
-        },
         getComment: function(){
-            var issue = this.model.get('issue');
-            return issue ? issue.comment : null;
+            var issue = this.model.getIssue();
+            return issue && issue.comment ? issue.comment : '';
+        },
+        getTickets: function(){
+            var issue = this.model.getIssue();
+            return issue && issue.externalSystemIssues ? issue.externalSystemIssues : [];
+        },
+        removeTicket: function(e){
+            e.preventDefault();
+            e.stopPropagation();
+
+            var $el = $(e.currentTarget).closest('[data-js-issue-ticket]'),
+                ticketId = $el.data('ticket-id').toString(),
+                issue = this.model.getIssue(),
+                tickets = this.getTickets(),
+                self = this;
+
+            $el.tooltip('hide').tooltip('destroy');
+            $el.remove();
+
+            tickets = _.reject(tickets, function (tk) {
+                return tk.ticketId === ticketId;
+            });
+            issue.externalSystemIssues = tickets;
+
+            CoreService.removeTicket({issues: [{issue: issue, test_item_id: this.model.get('id')}]})
+                .done(function (data) {
+                    self.model.setIssue(issue);
+                    Util.ajaxSuccessMessenger("removeTicket");
+                })
+                .fail(function (error) {
+                    Util.ajaxFailMessenger(error);
+                });
         },
         canMatchIssues: function(){
             var statistics = this.model.get('statistics');
@@ -107,7 +165,6 @@ define(function (require, exports, module) {
             delete this;
         }
     });
-
 
     return StepItemIssueView;
 });
