7121f4faf502c4fb38d79193020f41a031f70674
Dzmitry Kurachkin <dzmitry_kurachkin@epam.com>
2016-09-26 13:07:59 +0300

    fix duration test item if launch was stopped

--- src/main/resources/public/js/src/launch/launch-grid.js
+++ src/main/resources/public/js/src/launch/launch-grid.js
@@ -335,18 +335,9 @@ define(function (require, exports, module) {
                 },
 
                 renderDuration: function (item) {
-                    if (item) {
-                        var launch = _.filter(this.navigationInfo.toJSON(), function (suit) {
-                            return suit.id == item.launchId;
-                        })[0];
-                        if (launch && launch.data && launch.data.status == 'STOPPED') {
-                            var isStopped = true;
-                        }
-                    }
                     return Util.templates(that.durationTpl, {
                         item: item,
                         util: Util,
-                        isStopped: isStopped,
                         validator: that.navigationInfo.getValidator()
                     });
                 },
