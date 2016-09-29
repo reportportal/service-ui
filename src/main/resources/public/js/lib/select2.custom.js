ead1d3c297d64ec6e52eb2579265a3f39dae361d
Dzmitry Kurachkin <dzmitry_kurachkin@epam.com>
2016-09-29 17:38:15 +0300

    fix select2 sizer

--- src/main/resources/public/js/lib/select2.custom.js
+++ src/main/resources/public/js/lib/select2.custom.js
@@ -846,6 +846,7 @@ the specific language governing permissions and limitations under the Apache Lic
 
         // abstract
         destroy: function () {
+            sizer = null;
             var element=this.opts.element, select2 = element.data("select2"), self = this;
 
             this.close();
