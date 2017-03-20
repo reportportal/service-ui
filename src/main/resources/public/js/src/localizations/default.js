d0d5b31a0148212740fefb5b3fb6547938af6629
Dzmitry Kurachkin <dzmitry_kurachkin@epam.com>
2017-03-20 16:29:40 +0300

    EPMRPP-23029 fixed user search component for external users

--- src/main/resources/public/js/src/localizations/default.js
+++ src/main/resources/public/js/src/localizations/default.js
@@ -1324,6 +1324,7 @@ define(['util'], function () {
             titleNameLoginEmail: 'Name / Login / Email',
             enterLoginEmail: 'Enter Login or Email',
             alredyAdded: 'User has already assigned to the project',
+            externalUserAdded: 'Impossible to assign %%% user to %%% project',
             invitePref: 'Invite',
             sendInvite: 'Send invite via e-mail'
         },
