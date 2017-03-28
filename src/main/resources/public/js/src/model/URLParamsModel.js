686321aebec897fefee606e77aea85a9a4a9460d
Artsiom_Yeliseyenka <artsiom_yeliseyenka@epam.com>
2017-03-28 16:33:59 +0300

    EPMRPP-24925. Fix for behavior of browser "back" button.

--- src/main/resources/public/js/src/model/URLParamsModel.js
+++ src/main/resources/public/js/src/model/URLParamsModel.js
@@ -17,10 +17,10 @@
  * 
  * You should have received a copy of the GNU General Public License
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
- */ 
+ */
 'use strict';
 
-define(function(require, exports, module) {
+define(function (require, exports, module) {
     var Backbone = require('backbone'),
         App = require('app');
 
@@ -28,22 +28,22 @@ define(function(require, exports, module) {
 
     var URLParamsModel = Backbone.Model.extend({
         default: {},
-        initialize: function(){
+        initialize: function () {
             this.listenTo(this, 'change', this.onChange);
             this.parseURL();
         },
-        parseURL: function(){
+        parseURL: function () {
             try {
-                var hashSplit =  decodeURI(window.location.hash).split('?');
+                var hashSplit = decodeURI(window.location.hash).split('?');
                 this.mainHash = hashSplit[0];
                 var paramsString = hashSplit[1];
-                if(!paramsString) {
+                if (!paramsString) {
                     this.clear({silent: true});
                     return false;
                 }
                 var self = this;
                 var parameters = paramsString.split('&');
-                parameters.forEach(function(parameter){
+                parameters.forEach(function (parameter) {
                     var splitParam = parameter.split('=');
                     self.set(splitParam[0], splitParam[1], {silent: true});
                 });
@@ -51,32 +51,32 @@ define(function(require, exports, module) {
 
             }
         },
-        delaySet: function(data){
+        delaySet: function (data) {
             var self = this;
-            setTimeout(function(){
-               self.set(data)
+            setTimeout(function () {
+                self.set(data)
             }, 0);
         },
-        _getQueryString: function(){
+        _getQueryString: function () {
             var queryMas = [];
             var params = this.toJSON();
-            for(var key in params){
-                if(params[key] != null){
+            for (var key in params) {
+                if (params[key] != null) {
                     queryMas.push(key + '=' + encodeURIComponent(params[key]));
                 }
             }
             return queryMas.join('&');
         },
-        onChange: function(model, attribute){
-            if(this.mainHash != window.location.hash.split('?')[0]){
-                console.log('URLParamsModel: error, main hash it was changed')
+        onChange: function (model, attribute) {
+            if (this.mainHash != window.location.hash.split('?')[0]) {
+                console.log('URLParamsModel: error, main hash it was changed');
                 return;
             }
             var queryString = this._getQueryString();
-            if(!config.router) return;
-            config.router.navigate(this.mainHash + '?' + queryString, {trigger: false});
+            if (!config.router) return;
+            config.router.navigate(this.mainHash + '?' + queryString, {trigger: false, replace: true});
         }
     });
 
     return URLParamsModel;
-})
+});
