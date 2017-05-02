d6daa7846e8db2413c150fbb39ca6a1f1e859742
Artsiom_Yeliseyenka <artsiom_yeliseyenka@epam.com>
2017-05-02 11:19:10 +0300

    ES Lint error fixes.

--- src/main/resources/public/js/src/launches/LaunchBodyView.js
+++ src/main/resources/public/js/src/launches/LaunchBodyView.js
@@ -14,13 +14,10 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
  */
-define(function (require, exports, module) {
+define(function (require) {
     'use strict';
 
-    var SingletonDefectTypeCollection = require('defectType/SingletonDefectTypeCollection');
-
     var $ = require('jquery');
-    var Backbone = require('backbone');
     var Epoxy = require('backbone-epoxy');
     var App = require('app');
 
@@ -47,7 +44,7 @@ define(function (require, exports, module) {
 
     var LaunchBodyView = Epoxy.View.extend({
         template: 'tpl-launch-body',
-        initialize: function(options) {
+        initialize: function (options) {
             this.context = options.context;
             this.render();
             this.crumbs = new LaunchCrumbs({
@@ -55,10 +52,10 @@ define(function (require, exports, module) {
                 context: this.context
             });
             this.currentLevel = '';
-            this.collectionItems = new LaunchSuiteStepItemCollection({context: options.context});
+            this.collectionItems = new LaunchSuiteStepItemCollection({ context: options.context });
             this.multipleSelected = new LaunchMultipleSelect({
                 el: $('[data-js-multiple-selected]', this.$el),
-                collectionItems: this.collectionItems,
+                collectionItems: this.collectionItems
             });
             this.listenTo(this.collectionItems, 'change:params', this.onChangeParamsFilter);
             this.listenTo(this.collectionItems, 'drill:item', this.onDrillItem);
@@ -70,80 +67,87 @@ define(function (require, exports, module) {
             this.listenTo(this.multipleSelected, 'activate:true', this.onActivateMultipleSelect);
             this.listenTo(this.multipleSelected, 'activate:false', this.onDisableMultipleSelect);
         },
-        onActivateMultipleSelect: function() {
+        onActivateMultipleSelect: function () {
             this.trigger('change:level', 'MULTIPLE');
             this.body && this.body.destroyStickyHeader();
             this.stickyHeader && this.stickyHeader.destroy();
             $('[data-js-header-bar]', this.$el).addClass('multiple-select');
-            this.stickyHeader = new StickyHeader({fixedBlock: $('[data-js-header-bar]', this.$el), topMargin: 0});
+            this.stickyHeader = new StickyHeader({ fixedBlock: $('[data-js-header-bar]', this.$el), topMargin: 0 });
             this.control && this.control.activateMultiple();
         },
-        onDisableMultipleSelect: function() {
+        onDisableMultipleSelect: function () {
             this.trigger('change:level', this.currentLevel);
             $('[data-js-header-bar]', this.$el).removeClass('multiple-select');
             this.body && this.body.setupStickyHeader();
             this.stickyHeader && this.stickyHeader.destroy();
             this.control && this.control.disableMultiple();
         },
-        onMultiAction: function(actionName) {
+        onMultiAction: function (actionName) {
             this.multipleSelected.setAction(actionName);
         },
-        onChangeParamsFilter: function(params) {
+        onChangeParamsFilter: function (params) {
             var mainHash = window.location.hash.split('?')[0];
-            setTimeout(function() {
-                config.router.navigate(mainHash + '?' + params, {trigger: false, replace: true});
-            })
+            setTimeout(function () {
+                config.router.navigate(mainHash + '?' + params, { trigger: false, replace: true });
+            });
         },
-        onChangeLogItem: function(logItemId) {
+        onChangeLogItem: function (logItemId) {
             this.crumbs.setLogItem(this.collectionItems.get(logItemId));
-            config.router.navigate(this.collectionItems.getPathByLogItemId(logItemId), {trigger: false});
+            config.router.navigate(
+                this.collectionItems.getPathByLogItemId(logItemId), { trigger: false }
+            );
         },
-        onSetLogItem: function(logItemId) {
+        onSetLogItem: function (logItemId) {
             var logItemModel = this.collectionItems.get(logItemId);
             this.crumbs.setLogItem(logItemModel, logItemId);
         },
-        onDrillItem: function(itemModel) {
+        onDrillItem: function (itemModel) {
             this.crumbs.cacheItem(itemModel);
         },
-        onChangeItemCrumbs: function(launchModel, parentModel, optionsURL, nextItemId) {
+        onChangeItemCrumbs: function (launchModel, parentModel, optionsURL, nextItemId) {
             var self = this;
             this.collectionItems.update(launchModel, parentModel, optionsURL, this.crumbs)
-                .done(function() {
+                .done(function () {
                     self.onChangePathId();
                 })
-                .always(function() {
+                .always(function () {
                     $('[data-js-preloader-launch-body]', self.$el).removeClass('rp-display-block');
-                    nextItemId && optionsURL && self.body && self.body.activateNextId && self.body.activateNextId(nextItemId)
+                    nextItemId && optionsURL
+                    && self.body && self.body.activateNextId
+                    && self.body.activateNextId(nextItemId);
                 })
-                .fail(function() {
+                .fail(function () {
                     self.onChangePathId();  // not valid filter
                     // config.router.show404Page();
-                })
+                });
         },
-        onRestoreItemCrumbs: function() {
+        onRestoreItemCrumbs: function () {
             this.collectionItems.restorePath();
         },
-        onFailCrumbs: function(error) {
+        onFailCrumbs: function (error) {
             $('[data-js-preloader-launch-body]', this.$el).removeClass('rp-display-block');
-            switch(error) {
-                case 1: { // launch not found
-                    $('[data-js-launch-not-found]', this.$el).addClass('rp-display-block');
-                    this.trigger('change:level', 'SUITE');
-                    this.currentLevel = 'SUITE';
-                    break;
-                }
-                case 2: {
-                    $('[data-js-item-not-found]', this.$el).addClass('rp-display-block');
-                    this.trigger('change:level', 'SUITE');
-                    this.currentLevel = 'SUITE';
-                    break;
-                }
+            switch (error) {
+            case 1: { // launch not found
+                $('[data-js-launch-not-found]', this.$el).addClass('rp-display-block');
+                this.trigger('change:level', 'SUITE');
+                this.currentLevel = 'SUITE';
+                break;
+            }
+            case 2: {
+                $('[data-js-item-not-found]', this.$el).addClass('rp-display-block');
+                this.trigger('change:level', 'SUITE');
+                this.currentLevel = 'SUITE';
+                break;
+            }
+            default: {
+                break;
+            }
             }
         },
-        render: function() {
+        render: function () {
             this.$el.html(Util.templates(this.template, {}));
         },
-        update: function(partPath, optionsURL) {
+        update: function (partPath, optionsURL) {
             this.multipleSelected.reset();
             this.body && this.body.destroy();
             this.control && this.stopListening(this.control) && this.control.destroy();
@@ -152,92 +156,94 @@ define(function (require, exports, module) {
             $('[data-js-item-not-found]', this.$el).removeClass('rp-display-block');
             this.crumbs.update(partPath, optionsURL);
         },
-        onChangePathId: function() {
+        onChangePathId: function () {
             this.collectionItems.getInfoByCollection()
-                .done(function(info) {
-                    switch(info.type) {
-                        case 'LAUNCH': {
-                            this.renderLaunchLevel(info.filterModel);
-                            this.trigger('change:level', 'LAUNCH');
-                            this.currentLevel = 'LAUNCH';
-                            break;
-                        }
-                        case 'SUITE':
-                        case 'STORY':
-                        case 'TEST':
-                        case 'SCENARIO': {
-                            this.renderSuiteLevel(info);
-                            this.trigger('change:level', 'SUITE');
-                            this.currentLevel = 'SUITE';
-                            break;
-                        }
-                        case 'STEP':
-                        case 'BEFORE_CLASS':
-                        case 'BEFORE_GROUPS':
-                        case 'BEFORE_METHOD':
-                        case 'BEFORE_SUITE':
-                        case 'BEFORE_TEST':
-                        case 'AFTER_CLASS':
-                        case 'AFTER_GROUPS':
-                        case 'AFTER_METHOD':
-                        case 'AFTER_SUITE':
-                        case 'AFTER_TEST': {
-                            this.renderStepLevel(info);
-                            this.trigger('change:level', 'STEP');
-                            this.currentLevel = 'STEP';
-                            break;
-                        }
-                        case 'HISTORY': {
-                            this.trigger('change:level', 'HISTORY');
-                            this.currentLevel = 'HISTORY';
-                            this.renderHistory(info);
-                            break;
-                        }
-                        case 'LOG': {
-                            this.trigger('change:level', 'LOG');
-                            this.currentLevel = 'LOG';
-                            this.renderLogLevel(info);
-                            break;
-                        }
+                .done(function (info) {
+                    switch (info.type) {
+                    case 'LAUNCH': {
+                        this.renderLaunchLevel(info.filterModel);
+                        this.trigger('change:level', 'LAUNCH');
+                        this.currentLevel = 'LAUNCH';
+                        break;
+                    }
+                    case 'SUITE':
+                    case 'STORY':
+                    case 'TEST':
+                    case 'SCENARIO': {
+                        this.renderSuiteLevel(info);
+                        this.trigger('change:level', 'SUITE');
+                        this.currentLevel = 'SUITE';
+                        break;
+                    }
+                    case 'STEP':
+                    case 'BEFORE_CLASS':
+                    case 'BEFORE_GROUPS':
+                    case 'BEFORE_METHOD':
+                    case 'BEFORE_SUITE':
+                    case 'BEFORE_TEST':
+                    case 'AFTER_CLASS':
+                    case 'AFTER_GROUPS':
+                    case 'AFTER_METHOD':
+                    case 'AFTER_SUITE':
+                    case 'AFTER_TEST': {
+                        this.renderStepLevel(info);
+                        this.trigger('change:level', 'STEP');
+                        this.currentLevel = 'STEP';
+                        break;
+                    }
+                    case 'HISTORY': {
+                        this.trigger('change:level', 'HISTORY');
+                        this.currentLevel = 'HISTORY';
+                        this.renderHistory(info);
+                        break;
+                    }
+                    case 'LOG': {
+                        this.trigger('change:level', 'LOG');
+                        this.currentLevel = 'LOG';
+                        this.renderLogLevel(info);
+                        break;
+                    }
+                    default: {
+                        break;
+                    }
                     }
                     this.listenTo(this.control, 'multi:action', this.onMultiAction);
                 }.bind(this));
-
         },
-        renderLogLevel: function(info) {
+        renderLogLevel: function (info) {
             this.control = new LogControlView({
                 el: $('[data-js-controls-container]', this.$el),
-                collectionItems: this.collectionItems,
+                collectionItems: this.collectionItems
             });
             this.body = new LogBodyView({
                 el: $('[data-js-info-container]', this.$el),
                 context: this.context,
                 collectionItems: this.collectionItems,
-                launchModel: info.launchModel,
-            })
+                launchModel: info.launchModel
+            });
         },
-        renderHistory: function(info){
+        renderHistory: function (info) {
             this.control = new HistoryControlView({
                 el: $('[data-js-controls-container]', this.$el),
                 filterModel: info.filterModel,
                 parentModel: (info.parentModel || info.launchModel),
-                collectionItems: this.collectionItems,
+                collectionItems: this.collectionItems
             });
             this.body = new HistoryBodyView({
                 el: $('[data-js-info-container]', this.$el),
                 filterModel: info.filterModel,
                 control: this.control,
-                collectionItems: this.collectionItems,
-            })
+                collectionItems: this.collectionItems
+            });
         },
-        renderStepLevel: function(info) {
+        renderStepLevel: function (info) {
             this.control = new StepControlView({
                 el: $('[data-js-controls-container]', this.$el),
                 context: this.context,
                 filterModel: info.filterModel,
                 launchModel: info.launchModel,
                 parentModel: (info.parentModel || info.launchModel),
-                collectionItems: this.collectionItems,
+                collectionItems: this.collectionItems
             });
             this.body = new StepTableView({
                 el: $('[data-js-info-container]', this.$el),
@@ -248,24 +254,24 @@ define(function (require, exports, module) {
             });
             this.body.onShow && this.body.onShow();
         },
-        renderSuiteLevel: function(info) {
+        renderSuiteLevel: function (info) {
             this.control = new SuiteControlView({
                 context: this.context,
                 filterModel: info.filterModel,
                 launchModel: info.launchModel,
                 parentModel: (info.parentModel || info.launchModel),
                 el: $('[data-js-controls-container]', this.$el),
-                collectionItems: this.collectionItems,
+                collectionItems: this.collectionItems
             });
             this.body = new SuiteTableView({
                 el: $('[data-js-info-container]', this.$el),
                 filterModel: info.filterModel,
                 model: (info.launchModel || info.parentModel),
-                collectionItems: this.collectionItems,
+                collectionItems: this.collectionItems
             });
             this.body.onShow && this.body.onShow();
         },
-        renderLaunchLevel: function(filterModel) {
+        renderLaunchLevel: function (filterModel) {
             this.control = new LaunchControlView({
                 el: $('[data-js-controls-container]', this.$el),
                 collectionItems: this.collectionItems,
@@ -276,21 +282,18 @@ define(function (require, exports, module) {
                 el: $('[data-js-info-container]', this.$el),
                 filterModel: filterModel,
                 userDebugMode: (this.context === 'userdebug'),
-                collectionItems: this.collectionItems,
+                collectionItems: this.collectionItems
             });
             this.body.onShow && this.body.onShow();
         },
-        destroy: function () {
+        onDestroy: function () {
             this.stickyHeader && this.stickyHeader.destroy();
             this.multipleSelected.destroy();
             this.control && this.control.destroy();
             this.body && this.body.destroy();
             this.$el.html('');
-            this.undelegateEvents();
-            this.stopListening();
-            this.unbind();
             delete this;
-        },
+        }
     });
 
     return LaunchBodyView;
