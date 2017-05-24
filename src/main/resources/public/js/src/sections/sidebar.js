eb063f1d0f0d9c78ed34098fa447466d159cec3c
Artsiom_Yeliseyenka <artsiom_yeliseyenka@epam.com>
2017-05-24 10:30:57 +0300

    ES-Lint Errors fix.

--- src/main/resources/public/js/src/sections/sidebar.js
+++ src/main/resources/public/js/src/sections/sidebar.js
@@ -19,8 +19,9 @@
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-define(function (require, exports, module) {
-    "use strict";
+define(function (require) {
+    'use strict';
+
     var $ = require('jquery');
     var Backbone = require('backbone');
     var Util = require('util');
@@ -31,7 +32,7 @@ define(function (require, exports, module) {
     var config = App.getInstance();
 
     var SideBar = Backbone.View.extend({
-        el: "#pageSidebar",
+        el: '#pageSidebar',
 
         events: {
             'click .main-menu a': 'onClickSidebarLink',
@@ -48,8 +49,8 @@ define(function (require, exports, module) {
             this.projectUrl = options.projectUrl;
             this.currentPage = options.currentPage;
             this.canDebug = Util.isAdmin() || !Util.isCustomer();
-            this.currentHash = "#" + Backbone.history.getFragment().split('?')[0].split('/', 2).join('/');
-            this.listenTo(config.router, "route", this.updateActiveLink);
+            this.currentHash = '#' + Backbone.history.getFragment().split('?')[0].split('/', 2).join('/');
+            this.listenTo(config.router, 'route', this.updateActiveLink);
 
             this.userStorage = new SingletonUserStorage();
         },
@@ -59,6 +60,7 @@ define(function (require, exports, module) {
         },
 
         render: function () {
+            var self = this;
             var param = {
                 projectUrl: this.projectUrl,
                 userLogin: Storage.getDebugUser() || config.userModel.get('name'),
@@ -71,8 +73,6 @@ define(function (require, exports, module) {
             this.setupAnchors();
             this.updateActiveLink();
             Util.setupBaronScroll(this.$el.find('[data-js-scroll-container]'));
-
-            var self = this;
             $(window)
                 .off('resize.sidebar')
                 .on('resize.sidebar', function () {
@@ -87,23 +87,23 @@ define(function (require, exports, module) {
         },
         setLastActivePage: function () {
             var page = Backbone.history.getFragment();
-            (page == "user-profile") ? this.userStorage.set('lastActiveURL', this.projectUrl) : this.userStorage.set('lastActiveURL', page);
+            (page === 'user-profile') ? this.userStorage.set('lastActiveURL', this.projectUrl) : this.userStorage.set('lastActiveURL', page);
         },
 
         updateActiveLink: function () {
             this.clearActives();
-            this.currentHash = "#" + Backbone.history.getFragment().split('?')[0].split('/', 2).join('/');
-            if (this.currentHash === "#" + this.projectUrl) {
-                this.currentHash += "/dashboard";
+            this.currentHash = '#' + Backbone.history.getFragment().split('?')[0].split('/', 2).join('/');
+            if (this.currentHash === '#' + this.projectUrl) {
+                this.currentHash += '/dashboard';
             }
-            if (this.currentHash === "#" + this.projectUrl + "/") {
-                this.currentHash += "dashboard";
+            if (this.currentHash === '#' + this.projectUrl + '/') {
+                this.currentHash += 'dashboard';
             }
-            if (this.currentHash === "#administrate") {
-                this.currentHash += "/projects";
+            if (this.currentHash === '#administrate') {
+                this.currentHash += '/projects';
             }
-            if (this.currentHash === "#administrate/project-details") {
-                this.currentHash = "#administrate/projects";
+            if (this.currentHash === '#administrate/project-details') {
+                this.currentHash = '#administrate/projects';
             }
             this.$el.find('a[href^="' + this.currentHash + '"]', this.$el).addClass('active');
         },
@@ -119,37 +119,44 @@ define(function (require, exports, module) {
         onClickSidebarLink: function (e) {
             e.preventDefault();
             switch ($(e.currentTarget).attr('id')) {
-                case 'dashboard': {
-                    config.trackingDispatcher.trackEventNumber(1);
-                    break;
-                }
-                case 'filters': {
-                    config.trackingDispatcher.trackEventNumber(2);
-                    break;
-                }
-                case 'userdebug': {
-                    config.trackingDispatcher.trackEventNumber(3);
-                    break;
-                }
-                case 'projectsSection':
-                    config.trackingDispatcher.trackEventNumber(502);
-                    break;
-                case 'usersSection':
-                    config.trackingDispatcher.trackEventNumber(503);
-                    break;
-                case 'settingsSection':
-                    config.trackingDispatcher.trackEventNumber(504);
-                    break;
+            case 'dashboard': {
+                config.trackingDispatcher.trackEventNumber(1);
+                break;
             }
+            case 'filters': {
+                config.trackingDispatcher.trackEventNumber(2);
+                break;
+            }
+            case 'userdebug': {
+                config.trackingDispatcher.trackEventNumber(3);
+                break;
+            }
+            case 'projectsSection': {
+                config.trackingDispatcher.trackEventNumber(502);
+                break;
+            }
+            case 'usersSection': {
+                config.trackingDispatcher.trackEventNumber(503);
+                break;
+            }
+            case 'settingsSection': {
+                config.trackingDispatcher.trackEventNumber(504);
+                break;
+            }
+            default: {
+                break;
+            }
+            }
+
             this.closeMenu();
-            config.router.navigate($(e.currentTarget).attr('href'), {trigger: true});
+            config.router.navigate($(e.currentTarget).attr('href'), { trigger: true });
         },
         closeMenu: function () {
             this.$bodyElement.removeClass('menu-open');
         },
 
         clearActives: function () {
-            $("a.active", this.$el).removeClass('active');
+            $('a.active', this.$el).removeClass('active');
         },
         onClickLogout: function (e) {
             e.preventDefault();
@@ -157,12 +164,9 @@ define(function (require, exports, module) {
             config.userModel.logout();
         },
 
-        destroy: function () {
+        onDestroy: function () {
             $(window).off('resize.sidebar');
             this.$el.html('');
-            this.undelegateEvents();
-            this.stopListening();
-            this.unbind();
             delete this;
         }
     });
