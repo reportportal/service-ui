52930f95ce24c1a4afd7f07cae73f9cb1a752c1f
Artsiom_Yeliseyenka <artsiom_yeliseyenka@epam.com>
2017-06-05 16:58:55 +0300

    Encapsulation of common methods of bts fields into a separate view.

--- src/main/resources/public/js/src/projectSettings/tabViews/bts/btsFieldsView.js
+++ src/main/resources/public/js/src/projectSettings/tabViews/bts/btsFieldsView.js
@@ -24,14 +24,13 @@ define(function (require) {
 
     var $ = require('jquery');
     var _ = require('underscore');
-    var Epoxy = require('backbone-epoxy');
     var Util = require('util');
     var App = require('app');
     var DropDownComponent = require('components/DropDownComponent');
     var config = App.getInstance();
-    require('jquery-ui/widgets/datepicker');
+    var BtsFieldsCommonView = require('bts/BtsFieldsCommonView');
 
-    var FieldsView = Epoxy.View.extend({
+    var FieldsView = BtsFieldsCommonView.extend({
 
         fieldsTpl: 'tpl-dynamic-fields',
 
@@ -62,73 +61,6 @@ define(function (require) {
 
         setupMultiSelect: function (fields) {
             this.applyTypeForBtsFields(fields, this.$el);
-        },
-
-        applyTypeForBtsFields: function (fields, holder) {
-            var self = this;
-            _.forEach(fields, function (field) {
-                var clearId = field.id.replaceAll('.', '');
-                if (self.validForMultiSelect(field)) {
-                    var values = _.map(field.definedValues, function (option) {
-                            var name = option.valueName.trim();
-                            return { id: name, text: name };
-                        }),
-                        element = $('#' + field.id, holder);
-                    Util.setupSelect2WhithScroll(element, { tags: false, data: values });
-                    element.on('remove', function () {
-                        element.select2('destroy');
-                        element = null;
-                        values = null;
-                        $('body > #select2-drop-mask, body > .select2-sizer').remove();
-                    });
-                } else if (self.validForDatePicker(field)) {
-                    console.log(1);
-                    console.log($('[data-id=' + clearId + ']'));
-                    $('[data-id=' + clearId + ']')
-                        .prop('readonly', true)
-                        .datepicker({ dateFormat: config.dateTimeFormat })
-                        .on('remove', function () {
-                            $(this).datepicker('destroy');
-                        });
-                } else if (self.validForIntegers(field)) {
-                    Util.bootValidator($('[data-id=' + clearId + ']', holder), [{
-                        validator: 'matchRegex',
-                        type: 'onlyIntegersRegex',
-                        pattern: config.patterns.onlyIntegers
-                    }]);
-                } else if (self.validForDropDown(field)) {
-                    if (field.definedValues && field.definedValues.length
-                        > config.dynamicDropDownVisible) {
-                        Util.setupSelect2WhithScroll($('[data-id=' + clearId + ']', holder), {
-                            allowClear: true
-                        });
-                        $('[data-id=' + clearId + ']', holder).on('remove', function () {
-                            $(this).select2('destroy');
-                            $('body > #select2-drop-mask, body > .select2-sizer').remove();
-                        });
-                    }
-                } else if (self.validForDouble(field)) {
-                    Util.bootValidator($('[data-id=' + clearId + ']', holder), [
-                        { validator: 'matchRegex', type: 'doublesRegex', pattern: config.patterns.doubles }
-                    ]);
-                }
-            });
-        },
-
-        validForMultiSelect: function (field) {
-            return field.fieldType === 'array' && field.definedValues && field.definedValues.length;
-        },
-        validForDatePicker: function (field) {
-            return field.fieldType === 'date' || field.fieldType.toLowerCase() === 'datetime';
-        },
-        validForIntegers: function (field) {
-            return field.fieldType === 'Integer' || field.fieldType === 'number';
-        },
-        validForDropDown: function (field) {
-            return field.definedValues && field.definedValues.length && field.fieldType !== 'array';
-        },
-        validForDouble: function (field) {
-            return field.fieldType === 'Double';
         },
 
         setupDropdowns: function (fields) {
