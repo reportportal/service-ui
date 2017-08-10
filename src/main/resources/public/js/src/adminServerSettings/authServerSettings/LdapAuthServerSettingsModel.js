022501ce8988cf6fda9d1ac0dc1fb0693c607db3
Artsiom_Yeliseyenka <artsiom_yeliseyenka@epam.com>
2017-08-10 18:06:52 +0300

    LDAP Password Encoder Type filed default state fix.

--- src/main/resources/public/js/src/adminServerSettings/authServerSettings/LdapAuthServerSettingsModel.js
+++ src/main/resources/public/js/src/adminServerSettings/authServerSettings/LdapAuthServerSettingsModel.js
@@ -36,7 +36,7 @@ define(function (require) {
             passwordAttribute: '',
             managerDn: '',
             managerPassword: '',
-            passwordEncoderType: 'disabled',
+            passwordEncoderType: 'LDAP_SHA',
             // synchronization attributes
             email: '',
             fullName: '',
