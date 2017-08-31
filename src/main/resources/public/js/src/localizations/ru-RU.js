f960885f61ff7ce3fd77613f4c66e9ecd4a24e85
Yana_Zaitsava <Zaitsava_Yana@epam.com>
2017-08-31 15:34:59 +0300

    fix translation REFINE

--- src/main/resources/public/js/src/localizations/ru-RU.js
+++ src/main/resources/public/js/src/localizations/ru-RU.js
@@ -1129,7 +1129,7 @@ define(['util'], function () {
             launchName: 'Имя запуска',
             suiteName: 'Имя набора',
             testName: 'Имя теста',
-            refine: 'УЛУЧШИТЬ:'
+            refine: 'УТОЧНИТЬ:'
         },
         filter: {
             name: 'Имя',
