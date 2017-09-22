7c1b2ce3c7bc1653aef56bec09d3ccacdcc3d73e
alexey_krylov1 <alexey_krylov1@epam.com>
2017-09-22 16:43:23 +0300

    fix history of actions on log level

--- src/main/resources/public/js/src/launches/logLevel/LogItemInfoActivity.js
+++ src/main/resources/public/js/src/launches/logLevel/LogItemInfoActivity.js
@@ -18,67 +18,66 @@
  * You should have received a copy of the GNU General Public License
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
  */
-define(function (require, exports, module) {
+define(function (require) {
     'use strict';
 
     var $ = require('jquery');
-    var Backbone = require('backbone');
+    var _ = require('underscore');
     var Epoxy = require('backbone-epoxy');
-    var Util = require('util')
+    var Util = require('util');
     var Service = require('coreService');
     var Moment = require('moment');
     var MarkdownViewer = require('components/markdown/MarkdownViewer');
-    // var Textile = require('textile');
 
     var LogItemInfoActivityItemModel = Epoxy.Model.extend({
         defaults: {
             lastModifiedDate: 0,
             userRef: '',
             history: {},
-            actionType: '',
+            actionType: ''
         },
         computeds: {
             fullTime: {
                 deps: ['lastModifiedDate'],
-                get: function(lastModifiedDate) {
+                get: function (lastModifiedDate) {
                     return Util.dateFormat(lastModifiedDate);
                 }
             },
             momentTime: {
                 deps: ['lastModifiedDate'],
-                get: function(lastModifiedDate) {
+                get: function (lastModifiedDate) {
                     return Moment(Util.dateFormat(lastModifiedDate)).fromNow();
                 }
             },
             fullName: {
                 deps: ['userRef'],
-                get: function(userRef) {
+                get: function (userRef) {
                     return userRef.replace('_', ' ');
                 }
             },
             actionName: {
                 deps: ['history', 'actionType'],
-                get: function(history, actionType) {
-                    var field = _.size(history) > 1 ? 'issue' : _.first(Object.keys(history)).split('_').join(' ');
+                get: function (history, actionType) {
+                    var field = _.size(history) > 1 ? 'issue' : history[0].field.split('_').join(' ');
                     var aType = actionType.split('_');
-                    aType[0] = (aType[0].match("e$") ? aType[0] + 'd' : aType[0] + 'ed');
+                    aType[0] = (aType[0].match('e$') ? aType[0] + 'd' : aType[0] + 'ed');
                     return aType.join(' ') + ' ' + field;
                 }
             },
             actionsHtml: {
                 deps: ['history'],
-                get: function(history) {
+                get: function (history) {
                     var result = '';
-                    _.each(history, function(item, key) {
+                    _.each(history, function (item, key) {
                         result += '<div class="action-row">';
-                        if(key == 'ticketId') {
+                        if (key == 'ticketId') {
                             result += '<div class="action-item"><span>FROM: </span>' + this.getActionTicketsHtml(item.oldValue) + '</div>' +
                                 '<div class="separate-items"><i class="material-icons">keyboard_arrow_right</i></div>' +
-                                '<div class="action-item"><span>TO: </span>' + this.getActionTicketsHtml(item.newValue) +'</div>';
+                                '<div class="action-item"><span>TO: </span>' + this.getActionTicketsHtml(item.newValue) + '</div>';
                         } else {
                             result += '<div class="action-item"><span>FROM: </span>' + this.getActionValueHtml(item.oldValue) + '</div>' +
                                 '<div class="separate-items"><i class="material-icons">keyboard_arrow_right</i></div>' +
-                                '<div class="action-item"><span>TO: </span>' + this.getActionValueHtml(item.newValue) +'</div>';
+                                '<div class="action-item"><span>TO: </span>' + this.getActionValueHtml(item.newValue) + '</div>';
                         }
                         result += '</div>';
                     }, this);
@@ -86,19 +85,19 @@ define(function (require, exports, module) {
                 }
             }
         },
-        getActionTicketsHtml: function(tickets) {
+        getActionTicketsHtml: function (tickets) {
             var tickets = tickets || '';
             var ticketMas = tickets.split(',');
             var resultMas = [];
-            _.each(ticketMas, function(ticket) {
+            _.each(ticketMas, function (ticket) {
                 var ticketObj = Util.getTicketUrlId(ticket);
                 resultMas.push(ticketObj.url ? '<a target="_blank" class="rp-blue-link-undrl" href="' +
                     ticketObj.url + '">' + ticketObj.id + '</a>' : ticketObj.id);
             });
             return resultMas.join(',');
         },
-        getActionValueHtml: function(value) {
-            var markdownViewer = new MarkdownViewer({text: value});
+        getActionValueHtml: function (value) {
+            var markdownViewer = new MarkdownViewer({ text: value });
             return markdownViewer.$el.wrap('<p/>').parent().html();
         }
     });
@@ -112,13 +111,13 @@ define(function (require, exports, module) {
             '[data-js-action-name]': 'text: actionName',
             '[data-js-moment-time]': 'text: momentTime',
             '[data-js-full-time]': 'text: fullTime',
-            '[data-js-difference]': 'html: actionsHtml',
+            '[data-js-difference]': 'html: actionsHtml'
         },
 
-        initialize: function() {
+        initialize: function () {
             this.render();
         },
-        render: function() {
+        render: function () {
             this.$el.html(Util.templates(this.template, {}));
         }
     });
@@ -127,68 +126,67 @@ define(function (require, exports, module) {
         template: 'tpl-launch-log-item-info-activity',
 
         events: {
-            'click [data-js-close]': 'onClickClose',
+            'click [data-js-close]': 'onClickClose'
         },
 
-        initialize: function(options) {
+        initialize: function (options) {
             this.render();
             this.isLoad = false;
             this.itemModel = options.itemModel;
             this.parentModel = options.parentModel;
             this.listenTo(this.parentModel, 'change:activity', this.onShow);
         },
-        onShow: function(model, show) {
-              if(show && !this.isLoad) {
-                  this.isLoad = true;
-                  this.listenTo(this.itemModel, 'change:issue', this.load);
-                  this.load();
-              }
+        onShow: function (model, show) {
+            if (show && !this.isLoad) {
+                this.isLoad = true;
+                this.listenTo(this.itemModel, 'change:issue', this.load);
+                this.load();
+            }
         },
-        load: function() {
+        load: function () {
             $('[data-js-activity-container]', this.$el).html('');
             $('[data-js-activity-wrapper]', this.$el).addClass('load');
             $('[data-js-activity-wrapper]', this.$el).removeClass('not-found');
             var self = this;
             Service.loadActivityItems(this.itemModel.get('id'))
-                .done(function(data) {
+                .done(function (data) {
                     self.parse(data);
                 })
-                .fail(function() {
+                .fail(function () {
                     $('[data-js-activity-wrapper]', self.$el).addClass('not-found');
                 })
-                .always(function() {
+                .always(function () {
                     $('[data-js-activity-wrapper]', self.$el).removeClass('load');
-                })
+                });
         },
-        parse: function(data) {
+        parse: function (data) {
             var self = this;
             var $activityContainer = $('[data-js-activity-container]', this.$el);
-            if(data.length == 0) {
+            if (data.length == 0) {
                 $('[data-js-activity-wrapper]', self.$el).addClass('not-found');
-                return;
             } else {
                 $('[data-js-activity-wrapper]', self.$el).removeClass('not-found');
-                _.each(data, function(dataModel){
+                _.each(data, function (dataModel) {
                     var model = new LogItemInfoActivityItemModel(dataModel);
-                    $activityContainer.append((new LogItemInfoActivityItemView({model: model})).$el);
-                })
+                    $activityContainer.append((new LogItemInfoActivityItemView({ model: model })).$el);
+                });
             }
         },
-        render: function() {
+        render: function () {
             this.$el.html(Util.templates(this.template), {});
         },
-        onClickClose: function() {
-            this.parentModel.set({activity: false});
+        onClickClose: function () {
+            this.parentModel.set({ activity: false });
         },
 
-        destroy: function() {
+        destroy: function () {
             this.undelegateEvents();
             this.stopListening();
             this.unbind();
             this.$el.html('');
             delete this;
         }
-    })
+    });
 
     return LogItemInfoActivityView;
 });
