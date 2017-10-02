fb9dcaaa527e6b1e59f3c71250fb0d0e00908c6c
Yana_Zaitsava <Zaitsava_Yana@epam.com>
2017-10-02 10:11:51 +0300

    fix login style and util dependencies

--- src/main/resources/public/js/src/core/util.js
+++ src/main/resources/public/js/src/core/util.js
@@ -34,6 +34,8 @@ define(function (require, exports, module) {
 
     require('baron');
     require('cookie');
+    require('select2');
+
     // require('nicescroll');
     // require('isLoading');
 
