3de740a454a9628c6affde4605843b217e3d3c46
alexey_krylov1 <alexey_krylov1@epam.com>
2017-10-05 11:31:02 +0300

    add Reseted status

--- src/main/resources/public/js/src/localizations/en-EU.js
+++ src/main/resources/public/js/src/localizations/en-EU.js
@@ -852,7 +852,8 @@ define(['util'], function () {
             STOPPED: 'STOPPED',
             SKIPPED: 'SKIPPED',
             INTERRUPTED: 'INTERRUPTED',
-            IN_PROGRESS: 'IN PROGRESS'
+            IN_PROGRESS: 'IN PROGRESS',
+            RESETED: 'RESETED'
         },
 
         historyLine: {
--- src/main/resources/public/js/src/localizations/ru-RU.js
+++ src/main/resources/public/js/src/localizations/ru-RU.js
@@ -905,7 +905,8 @@ define(['util'], function () {
             STOPPED: 'ОСТАНОВЛЕНО',
             SKIPPED: 'ПРОПУЩЕНО',
             INTERRUPTED: 'ПРЕРВАНО',
-            IN_PROGRESS: 'В ПРОЦЕССЕ'
+            IN_PROGRESS: 'В ПРОЦЕССЕ',
+            RESETED: 'RESETED'
         },
 
         historyLine: {
