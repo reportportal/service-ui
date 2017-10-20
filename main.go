bef9a7de8fec1492a6712c3a218460897eaa9aff
Andrei Varabyeu <andrei_varabyeu@epam.com>
2017-10-20 13:59:43 +0300

    update csp policy

--- main.go
+++ main.go
@@ -53,7 +53,7 @@ func main() {
 				FrameDeny:             true,
 				ContentTypeNosniff:    true,
 				BrowserXssFilter:      true,
-				ContentSecurityPolicy: "default-src 'self'; script-src 'self' www.google-analytics.com *.uservoice.com; img-src www.google-analytics.com; object-src 'self' status.reportportal.io",
+				ContentSecurityPolicy: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' www.google-analytics.com *.uservoice.com; img-src www.google-analytics.com; object-src 'self' status.reportportal.io",
 			}).Handler(next)
 		})
 
