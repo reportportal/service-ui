ef28bdec7416ee657f16ac18b301b21d40e1fab0
Artsiom_Yeliseyenka <artsiom_yeliseyenka@epam.com>
2017-12-01 16:51:43 +0300

    React. Added Input component.

---
+++ app/src/components/inputs/input/Input.jsx
@@ -0,0 +1,52 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import styles from './Input.scss';
+
+const cx = classNames.bind(styles);
+
+const Input = ({ type, value, placeholder, maxLength, disabled, onChange, onFocus, onBlur }) => {
+  const classes = cx({
+    input: true,
+    disabled,
+  });
+  const handlerOnChange = (e) => {
+    onChange({ value: e.target.value });
+  };
+  return (
+    <input
+      type={type}
+      className={classes}
+      value={value}
+      placeholder={placeholder}
+      maxLength={maxLength}
+      disabled={disabled}
+      onChange={handlerOnChange}
+      onFocus={onFocus}
+      onBlur={onBlur}
+    />
+  );
+};
+
+Input.propTypes = {
+  type: PropTypes.string,
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+  maxLength: PropTypes.string,
+  disabled: PropTypes.string,
+  onChange: PropTypes.func,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func,
+};
+
+Input.defaultProps = {
+  type: 'text',
+  value: '',
+  placeholder: '',
+  maxLength: '254',
+  disabled: '',
+  onChange: () => {},
+  onFocus: () => {},
+  onBlur: () => {},
+};
+
+export default Input;
---
+++ app/src/components/inputs/input/README.md
@@ -0,0 +1,10 @@
+## **Typical field for text input**
+
+Has no own size. Width and Height = 100% of it's parent.
+
+### Props:
+* **type**: string, optional, default = text
+* **defaultValue**: string, optional
+* **placeholder**: string, optional
+* **maxLength**: number, optional, default = 256
+* **disabled**: bool, optional, default = false
