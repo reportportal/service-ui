3a29ed869c3704dd23610f0eae57b615d5e6a017
alexey_krylov1 <alexey_krylov1@epam.com>
2018-01-24 17:13:45 +0300

    add react-router

--- app/src/components/main/app/app.jsx
+++ app/src/components/main/app/app.jsx
@@ -1,89 +1,74 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from '@cerebral/react';
-import { state } from 'cerebral/tags';
-import classNames from 'classnames/bind';
+// import classNames from 'classnames/bind';
+import { Switch, Route } from 'react-router-dom';
 import LocalizationContainer from '../localizationContainer/localizationContainer';
-import styles from './app.scss';
+// import styles from './app.scss';
 
-import Sidebar from '../sidebar/sidebar';
-import Header from '../header/header';
-import Notification from '../notification/notification';
+// import Sidebar from '../sidebar/sidebar';
+// import Header from '../header/header';
+// import Notification from '../notification/notification';
+//
+// import LoginPage from '../../../pages/loginPage/loginPage';
+// import RegistrationPage from '../../../pages/registrationPage/registrationPage';
+// import AppPage from '../../../pages/appPage/appPage';
 
-import LoginPage from '../../../pages/loginPage/loginPage';
-import RegistrationPage from '../../../pages/registrationPage/registrationPage';
-import AppPage from '../../../pages/appPage/appPage';
 
-const PAGES = {
-  login: {
-    container: 'empty',
-    page: LoginPage,
-  },
-  registration: {
-    container: 'empty',
-    page: RegistrationPage,
-  },
-  app: {
-    container: 'app',
-    page: AppPage,
-  },
-};
+// const cx = classNames.bind(styles);
 
-const cx = classNames.bind(styles);
+const AdminLayout = () => (
+  <div>
+    <h1>admin header</h1>
+    <h1>admin sidebar</h1>
+    <Switch>
+      <Route exec path="/administrate" render={() => (<h1>admin projects page</h1>)} />
+      <Route path="/administrate/projects" render={() => (<h1>admin projects page</h1>)} />
+      <Route path="/administrate/users" render={() => (<h1>admin Users page</h1>)} />
+      <Route path="/administrate/settings" render={() => (<h1>admin settings page</h1>)} />
+    </Switch>
+  </div>
+);
 
-const App = ({ currentPage }) => {
-  function getContainer(pageName) {
-    let result = <div />;
-    if (pageName === '' || !PAGES[pageName]) {
-      return result;
-    }
-    const Page = PAGES[pageName].page;
-    switch (PAGES[pageName].container) {
-      case 'empty':
-        result = (
-          <div className={cx('empty-container')}>
-            <Page />
-            <Notification />
-          </div>
-        );
-        break;
-      case 'app':
-        result = (
-          <div className={cx('app-container')}>
-            <div className={cx('sidebar-container')}>
-              <Sidebar />
-            </div>
-            <div className={cx('content')}>
-              <div className={cx('header-container')}>
-                <Header />
-              </div>
-              <div className={cx('page-container')}>
-                <Page />
-              </div>
-            </div>
-            <Notification />
-          </div>
-        );
-        break;
-      default:
-        break;
-    }
-    return result;
-  }
+const PageLayout = ({ match }) => {
+  console.dir(match);
   return (
-    <LocalizationContainer>
-      {getContainer(currentPage)}
-    </LocalizationContainer>
+    <div className="page-content">
+      <p>Page name: <b>{match.params.projectId}</b></p>
+      <Switch>
+        <Route path="/:projectId" render={() => (<h1>Dashboard page</h1>)} />
+        <Route path="/:projectId/dashboard" render={() => (<h1>Dashboard page</h1>)} />
+        <Route path="/:projectId/dashboard" render={() => (<h1>Dashboard page</h1>)} />
+      </Switch>
+    </div>
   );
 };
-App.propTypes = {
-  currentPage: PropTypes.string,
+PageLayout.propTypes = {
+  match: PropTypes.object.isRequired,
 };
 
-App.defaultProps = {
-  currentPage: '',
-};
+const AppLayout = () => (
+  <div className="app-layout">
+    <h1>App header</h1>
+    <h1>App sidebar</h1>
+    <div className="page-container">
+      <Switch>
+        <Route path="/user-profile" render={() => (<h1>profile page</h1>)} />
+        <Route path="/api" render={() => (<h1>api page</h1>)} />
+        <Route path="/:projectId" component={PageLayout} />
+      </Switch>
+    </div>
+  </div>
+);
 
-export default connect({
-  currentPage: state`route.currentPage`,
-}, App);
+const App = () => (
+  <LocalizationContainer>
+    <Switch>
+      <Route exact path="/" render={() => (<h1>login page</h1>)} />
+      <Route path="/login" render={() => (<h1>login page</h1>)} />
+      <Route path="/administrate" component={AdminLayout} />
+      <AppLayout />
+    </Switch>
+  </LocalizationContainer>
+  );
+
+export default App;
--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Container } from '@cerebral/react';
+import { HashRouter } from 'react-router-dom';
 
 import 'reset-css';
 import 'common/css/fonts/fonts.scss';
 import 'common/css/common.scss';
 
-import controller from './controller/controller';
 import App from './components/main/app/app';
 
 const rerenderApp = (AppContainer) => {
   render((
-    <Container controller={controller} >
+    <HashRouter>
       <AppContainer />
-    </Container>
+    </HashRouter>
   ), document.querySelector('#app'));
 };
 
