6df14739c92ca686285784016bd3af0649066c2e
Artsiom_Yeliseyenka <artsiom_yeliseyenka@epam.com>
2018-02-07 11:35:53 +0300

    React. App layout styling. Added scrollbars to app layout.

--- app/src/layouts/appLayout/appLayout.jsx
+++ app/src/layouts/appLayout/appLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
+import ScrollWrapper from 'components/main/scrollWrapper/scrollWrapper';
 import Sidebar from '../../components/main/sidebar/sidebar';
 import Header from '../../components/main/header/header';
 import styles from './appLayout.scss';
@@ -8,19 +9,24 @@ import styles from './appLayout.scss';
 const cx = classNames.bind(styles);
 
 const AppLayout = ({ children }) => (
-  <div className={cx('app-container')}>
-    <div className={cx('sidebar-container')}>
-      <Sidebar />
-    </div>
-    <div className={cx('content')}>
-      <div className={cx('header-container')} >
-        <Header />
+  <ScrollWrapper>
+    <div className={cx('app-container')}>
+      <div className={cx('sidebar-container')}>
+        <div className={cx('rp-logo')} />
+        <Sidebar />
       </div>
-      <div className={cx('page-container')}>
-        {children}
+      <div className={cx('content')}>
+        <ScrollWrapper>
+          <div className={cx('header-container')} >
+            <Header />
+          </div>
+          <div className={cx('page-container')}>
+            {children}
+          </div>
+        </ScrollWrapper>
       </div>
     </div>
-  </div>
+  </ScrollWrapper>
 );
 
 AppLayout.propTypes = {
