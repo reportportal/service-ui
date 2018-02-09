f9810df10ddad12a6d2b0f292ec26b2b916f5f6f
Maxim Tumas <DiscoElevator@users.noreply.github.com>
2018-02-09 16:28:20 +0300

    EPMRPP-33110: Add restore password logic

--- app/src/pages/outside/loginPage/forgotPasswordBlock/forgotPasswordForm/forgotPasswordForm.jsx
+++ app/src/pages/outside/loginPage/forgotPasswordBlock/forgotPasswordForm/forgotPasswordForm.jsx
@@ -19,18 +19,18 @@
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { Component } from 'react';
+import { PureComponent } from 'react';
 import classNames from 'classnames/bind';
 import { reduxForm } from 'redux-form';
 import { FormattedMessage, injectIntl, intlShape, defineMessages } from 'react-intl';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { FieldProvider } from 'components/fields/fieldProvider';
 import { FieldWithIcon } from 'components/fields/fieldWithIcon';
 import { FieldErrorHint } from 'components/fields/fieldErrorHint';
 import { Input } from 'components/inputs/input';
 import { BigButton } from 'components/buttons/bigButton';
-import { validate } from 'common/utils';
+import { validate, fetch } from 'common/utils';
 import EmailIcon from './img/email-icon.svg';
 import styles from './forgotPasswordForm.scss';
 
@@ -43,6 +43,7 @@ const placeholders = defineMessages({
   },
 });
 
+@withRouter
 @reduxForm({
   form: 'forgotPassword',
   validate: ({ email }) => ({
@@ -50,21 +51,31 @@ const placeholders = defineMessages({
   }),
 })
 @injectIntl
-export class ForgotPasswordForm extends Component {
+export class ForgotPasswordForm extends PureComponent {
   static propTypes = {
     intl: intlShape.isRequired,
     handleSubmit: PropTypes.func.isRequired,
-    submitForm: PropTypes.func,
+    history: PropTypes.object.isRequired,
   };
+
   static defaultProps = {
-    submitForm: () => {},
     intl: {},
   };
+
+  submitForm = ({ email }) => {
+    fetch('api/v1/user/password/restore', {
+      method: 'post',
+      data: {
+        email,
+      },
+    }).then(() => this.props.history.push('/login'));
+  };
+
   render() {
-    const { intl, handleSubmit, submitForm } = this.props;
+    const { intl, handleSubmit } = this.props;
     const { formatMessage } = intl;
     return (
-      <form className={cx('forgot-password-form')} onSubmit={handleSubmit(submitForm)}>
+      <form className={cx('forgot-password-form')} onSubmit={handleSubmit(this.submitForm)}>
         <div className={cx('email-field')}>
           <FieldProvider name="email">
             <FieldErrorHint>
