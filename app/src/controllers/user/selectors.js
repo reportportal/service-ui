d59df7129535c857de7f05004cdb76f1fa322496
Artsiom_Yeliseyenka <artsiom_yeliseyenka@epam.com>
2018-02-14 16:18:54 +0300

    Pull Request review fixes.

--- app/src/controllers/user/selectors.js
+++ app/src/controllers/user/selectors.js
@@ -1,6 +1,5 @@
 const userSelector = state => state.user || {};
-const useInfoSelector = state => state.user.info || {};
 
 export const userInfoSelector = state => userSelector(state).info || {};
-export const defaultProjectSelector = state => useInfoSelector(state).default_project || '';
+export const defaultProjectSelector = state => userInfoSelector(state).default_project || '';
 export const activeProjectSelector = state => userSelector(state).activeProject || defaultProjectSelector(state) || '';
