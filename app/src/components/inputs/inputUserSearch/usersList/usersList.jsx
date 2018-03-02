37fe85602ba1ef31990fad4f6ee9ecd23aed20a0
Yana_Zaitsava <Zaitsava_Yana@epam.com>
2018-03-02 12:38:53 +0300

    fixes EPMRPP-33247 UserSearchInput

--- app/src/components/inputs/inputUserSearch/usersList/usersList.jsx
+++ app/src/components/inputs/inputUserSearch/usersList/usersList.jsx
@@ -19,37 +19,13 @@
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
  */
 import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
 import { ScrollWrapper } from 'components/main/scrollWrapper';
-import { UserItem } from 'components/inputs/inputUserSearch/userItem';
-import classNames from 'classnames/bind';
-import styles from './usersList.scss';
-
-const cx = classNames.bind(styles);
+import { UserItem } from './userItem';
+import { InviteNewUserItem } from './inviteNewUserItem';
 
 export const UsersList = ({ options, selectValue }) => {
   if (options[0] && options[0].externalUser) {
-    return (
-      <div className={'select-menu-outer'}>
-        <div className={'select-menu'} role="listbox">
-          <div
-            onClick={() => selectValue(options[0])}
-          >
-            <div className={cx('invite-new-user')}>
-              <div className={cx('msg-icon')} />
-              <div className={cx('invite-info')}>
-                <p className={cx('user-info')} >
-                  <FormattedMessage id={'InputUserSearch.inviteNewUser'} defaultMessage={'Invite {userEmail}'} values={{ userEmail: options[0].label }} />
-                </p>
-                <p className={cx('action-info')}>
-                  <FormattedMessage id={'InputUserSearch.inviteNewUserInfo'} defaultMessage={'Send invite via e-mail'} />
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+    return <InviteNewUserItem option={options[0]} selectValue={selectValue} />;
   }
   return (
     <ScrollWrapper autoHeight autoHeightMax={200}>
