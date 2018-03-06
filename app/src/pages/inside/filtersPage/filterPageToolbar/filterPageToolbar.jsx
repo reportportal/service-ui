17635344036920d5fbc37e42633cf34038b62f5d
Maxim Tumas <DiscoElevator@users.noreply.github.com>
2018-03-06 18:02:47 +0300

    EPMRPP-33153: Move toolbar out of the filters table

---
+++ app/src/pages/inside/filtersPage/filterPageToolbar/filterPageToolbar.jsx
@@ -0,0 +1,72 @@
+import PropTypes from 'prop-types';
+import { reduxForm } from 'redux-form';
+import classNames from 'classnames/bind';
+import { injectIntl, defineMessages, intlShape } from 'react-intl';
+import { GhostButton } from 'components/buttons/ghostButton';
+import { FieldProvider } from 'components/fields/fieldProvider';
+import { FilterSearchInput } from './filterSearchInput';
+import AddFilterIcon from './img/ic-add-filter-inline.svg';
+import styles from './filterPageToolbar.scss';
+
+const cx = classNames.bind(styles);
+
+const messages = defineMessages({
+  favoriteFilters: {
+    id: 'FilterPage.msgFavoriteFilters',
+    defaultMessage: 'Filters are issue searches that have been saved for re-use.',
+  },
+});
+
+@reduxForm({
+  form: 'filterSearch',
+  validate: ({ filter }) => ({ filter: filter && filter.length < 3 ? 'searchInputError' : undefined }),
+  onChange: (vals, dispatch, props) => {
+    if (vals.filter && vals.filter.length < 3) {
+      return;
+    }
+    props.onFilterChange(vals.filter || undefined);
+  },
+})
+@injectIntl
+export class FilterPageToolbar extends React.Component {
+  static propTypes = {
+    change: PropTypes.func,
+    invalid: PropTypes.bool,
+    filter: PropTypes.string,
+    intl: intlShape,
+  };
+
+  static defaultProps = {
+    invalid: false,
+    filter: null,
+    intl: {},
+    change: () => {
+    },
+  };
+
+  componentDidMount() {
+    this.props.change('filter', this.props.filter);
+  }
+
+  componentWillReceiveProps({ filter, invalid }) {
+    if (filter !== this.props.filter && !invalid) {
+      this.props.change('filter', filter);
+    }
+  }
+
+  render() {
+    return (
+      <div className={cx('filter-page-toolbar')}>
+        <FieldProvider name="filter">
+          <FilterSearchInput />
+        </FieldProvider>
+        <div className={cx('label')}>
+          {this.props.intl.formatMessage(messages.favoriteFilters)}
+        </div>
+        <GhostButton icon={AddFilterIcon}>
+          Add filter
+        </GhostButton>
+      </div>
+    );
+  }
+}
--- app/src/pages/inside/filtersPage/filtersPage.jsx
+++ app/src/pages/inside/filtersPage/filtersPage.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
-import { injectIntl, defineMessages } from 'react-intl';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { injectIntl, defineMessages, intlShape } from 'react-intl';
+import { withFilter } from 'controllers/filter';
+import { userIdSelector } from 'controllers/user';
+import { withPagination } from 'components/containers/pagination';
 import { PageLayout } from 'layouts/pageLayout';
 import { FilterTable } from './filterTable';
+import { FilterPageToolbar } from './filterPageToolbar';
 
 const messages = defineMessages({
   filtersPageTitle: {
@@ -10,8 +16,57 @@ const messages = defineMessages({
   },
 });
 
-export const FiltersPage = injectIntl(({ intl }) => (
-  <PageLayout title={intl.formatMessage(messages.filtersPageTitle)}>
-    <FilterTable />
-  </PageLayout>
-));
+@withFilter
+@withPagination({
+  url: activeProject => `/api/v1/${activeProject}/filter`,
+})
+@connect(state => ({
+  userId: userIdSelector(state),
+}))
+@injectIntl
+export class FiltersPage extends PureComponent {
+  static propTypes = {
+    intl: intlShape.isRequired,
+    data: PropTypes.arrayOf(PropTypes.object),
+    activePage: PropTypes.number,
+    itemCount: PropTypes.number,
+    pageCount: PropTypes.number,
+    pageSize: PropTypes.number,
+    onChangePage: PropTypes.func,
+    onChangePageSize: PropTypes.func,
+    userId: PropTypes.string,
+    filter: PropTypes.string,
+    onFilterChange: PropTypes.func,
+  };
+
+  static defaultProps = {
+    data: [],
+    activePage: 1,
+    itemCount: 0,
+    pageCount: 0,
+    pageSize: 20,
+    userId: '',
+    filter: '',
+    onFilterChange: () => {
+    },
+    onChangePage: () => {
+    },
+    onChangePageSize: () => {
+    },
+  };
+
+  render() {
+    const { filter, intl, onFilterChange, ...rest } = this.props;
+    return (
+      <PageLayout title={intl.formatMessage(messages.filtersPageTitle)}>
+        <FilterPageToolbar
+          filter={filter}
+          onFilterChange={onFilterChange}
+        />
+        <FilterTable
+          {...rest}
+        />
+      </PageLayout>
+    );
+  }
+}
