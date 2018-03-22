442c12ed2fd14e74b7935f09c9180b355ce08e8b
Maxim Tumas <DiscoElevator@users.noreply.github.com>
2018-03-22 17:16:52 +0300

    EPMRPP-33955: Make grid items selectable

--- app/src/components/main/grid/gridBody/gridBody.jsx
+++ app/src/components/main/grid/gridBody/gridBody.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { GridRow } from './gridRow';
 
-export const GridBody = ({ columns, data }) =>
+export const GridBody = ({ columns, data, ...rest }) =>
   data.map((row, i) => (
-    <GridRow key={row.id || i} columns={columns} value={row} />
+    <GridRow key={row.id || i} columns={columns} value={row} {...rest} />
   ));
---
+++ app/src/components/main/grid/gridHeader/checkboxHeaderCell/checkboxHeaderCell.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import { InputCheckbox } from 'components/inputs/inputCheckbox';
+import styles from './checkboxHeaderCell.scss';
+
+const cx = classNames.bind(styles);
+
+export const CheckboxHeaderCell = ({ value, onChange }) => (
+  <div className={cx('checkbox-header-cell')}>
+    <InputCheckbox value={value} onChange={onChange} />
+  </div>
+);
+CheckboxHeaderCell.propTypes = {
+  value: PropTypes.bool,
+  onChange: PropTypes.func,
+};
+CheckboxHeaderCell.defaultProps = {
+  value: false,
+  onChange: () => {
+  },
+};
