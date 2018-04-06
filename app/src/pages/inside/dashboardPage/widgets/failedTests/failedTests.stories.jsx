c24c5b82ff0c329339da837105e9e26412a9b320
PETERSBURG\Sergei_Baluhto <Sergei_Baluhto@epam.com>
2018-04-06 17:37:00 +0300

    Extract table row into separate component

--- app/src/pages/inside/dashboardPage/widgets/failedTests/failedTests.stories.jsx
+++ app/src/pages/inside/dashboardPage/widgets/failedTests/failedTests.stories.jsx
@@ -1,4 +1,5 @@
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { host } from 'storybook-host';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
