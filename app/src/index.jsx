f9af6f60c0d1cf0469e0cb502b9b1d9e5e0d0507
Ihor <serkov.ihor@gmail.com>
2018-04-09 22:51:37 +0200

    added why-did-you-update tool

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -16,29 +16,42 @@ import 'c3/c3.css';
 import App from './app';
 import store from './store';
 
+if (!process.env.production) {
+  const query = parse(location.search, { ignoreQueryPrefix: true });
+  const whyDidYouUpdateComponent =
+    'whyDidYouUpdateComponent' in query && (query.whyDidYouUpdateComponent || '.*');
 
-const queryParseHistory = qhistory(
-  createHashHistory({ hashType: 'noslash' }),
-  stringify,
-  parse,
-);
+  if (whyDidYouUpdateComponent) {
+    const { whyDidYouUpdate } = require('why-did-you-update'); // eslint-disable-line global-require
+    whyDidYouUpdate(React, { include: new RegExp(whyDidYouUpdateComponent) });
+    console.log(
+      'Use http://localhost:3000/?whyDidYouUpdateComponent=^Component$# (with regex as query value) to check why certain component rerender.',
+    );
+  }
+}
+
+const queryParseHistory = qhistory(createHashHistory({ hashType: 'noslash' }), stringify, parse);
 queryParseHistory.listen((location) => {
   const match = matchPath(location.pathname, '/:projectId');
   const hashProject = match.params.projectId;
   const userProjects = userInfoSelector(store.getState()).assigned_projects;
-  if (userProjects && Object.prototype.hasOwnProperty.call(userProjects, hashProject)
-    && hashProject !== activeProjectSelector(store.getState())) {
+  if (
+    userProjects &&
+    Object.prototype.hasOwnProperty.call(userProjects, hashProject) &&
+    hashProject !== activeProjectSelector(store.getState())
+  ) {
     store.dispatch(setActiveProjectAction(hashProject));
     store.dispatch(fetchProjectAction(hashProject));
   }
 });
 
 const rerenderApp = (AppContainer) => {
-  render((
+  render(
     <Router history={queryParseHistory}>
       <AppContainer />
-    </Router>
-  ), document.querySelector('#app'));
+    </Router>,
+    document.querySelector('#app'),
+  );
 };
 
 if (module.hot) {
