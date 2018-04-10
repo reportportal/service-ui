f5d826207406d0fc769cac8b1b7413dd508556e2
PETERSBURG\Sergei_Baluhto <Sergei_Baluhto@epam.com>
2018-04-10 17:09:40 +0300

    Count and matrix row components + localization

---
+++ app/src/pages/inside/dashboardPage/widgets/mostFailedTests/mostFailedTestsTable/matrix.jsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { arrayOf, bool, string } from 'prop-types';
+import classNames from 'classnames/bind';
+import styles from '../mostFailedTests.scss';
+
+const cx = classNames.bind(styles);
+
+const Matrix = ({ tests, id }) => (
+  <div className={cx('matrix')}>
+    <div className={cx('squares-wrapper')}>
+      {/* eslint-disable */
+      tests.map((failed, idx) => (
+        <div key={`${id}-square-${idx}`} className={cx('square', { failed })} />
+      ))
+      /* eslint-disable */
+      }
+    </div>
+  </div>
+);
+
+Matrix.propTypes = {
+  tests: arrayOf(bool).isRequired,
+  id: string.isRequired,
+};
+
+export default Matrix;
--- app/src/pages/inside/dashboardPage/widgets/mostFailedTests/mostFailedTestsTable/mostFailedTestsTableRow.jsx
+++ app/src/pages/inside/dashboardPage/widgets/mostFailedTests/mostFailedTestsTable/mostFailedTestsTableRow.jsx
@@ -3,54 +3,39 @@ import { func } from 'prop-types';
 import classNames from 'classnames/bind';
 import { AbsRelTime } from 'components/main/absRelTime';
 import { PTTest } from '../pTypes';
+import Count from './count';
+import Matrix from './matrix';
 import styles from '../mostFailedTests.scss';
 
 const cx = classNames.bind(styles);
 
 class FailedTestsTableRow extends React.PureComponent {
-
   static propTypes = {
     data: PTTest.isRequired,
     nameClickHandler: func.isRequired,
-  }
+  };
 
   nameClickHandler = () => {
     const { nameClickHandler, data } = this.props;
     nameClickHandler(data.uniqueId);
-  }
+  };
 
   render() {
     const { data } = this.props;
-    const {
-      name, total, isFailed, percentage,
-      lastTime, uniqueId, failedCount,
-    } = data;
+    const { name, total, isFailed, percentage, lastTime, uniqueId, failedCount } = data;
 
     return (
       <div key={uniqueId} className={cx('row')}>
-        <div
-          className={cx('col', 'col-name')}
-          onClick={this.nameClickHandler}
-        >
+        <div className={cx('col', 'col-name')} onClick={this.nameClickHandler}>
           <span>{name}</span>
         </div>
         <div className={cx('col', 'col-count')}>
-          <div className={cx('count')}>{`${failedCount} of ${total}`}</div>
-          <div className={cx('matrix')}>
-            <div className={cx('squares-wrapper')}>
-              {
-                /* eslint-disable */
-                isFailed.map((failed, idx) => <div key={`${uniqueId}-square-${idx}`} className={cx('square', { failed })} />)
-                /* eslint-disable */
-              }
-            </div>
-          </div>
+          <Count count={failedCount} total={total} />
+          <Matrix tests={isFailed} id={uniqueId} />
         </div>
         <div className={cx('col', 'col-percents')}>{percentage}</div>
         <div className={cx('col', 'col-date')}>
-          <AbsRelTime
-            startTime={lastTime}
-          />
+          <AbsRelTime startTime={lastTime} />
         </div>
       </div>
     );
