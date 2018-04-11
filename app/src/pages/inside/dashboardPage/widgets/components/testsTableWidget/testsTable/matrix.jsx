255de93286828d5250cd8690eb13ad228c7064eb
PETERSBURG\Sergei_Baluhto <Sergei_Baluhto@epam.com>
2018-04-11 17:04:04 +0300

    Add Flaky Tests widget

--- app/src/pages/inside/dashboardPage/widgets/components/testsTableWidget/testsTable/matrix.jsx
+++ app/src/pages/inside/dashboardPage/widgets/components/testsTableWidget/testsTable/matrix.jsx
@@ -5,22 +5,34 @@ import styles from '../testsTableWidget.scss';
 
 const cx = classNames.bind(styles);
 
-const Matrix = ({ tests, id }) => (
-  <div className={cx('matrix')}>
-    <div className={cx('squares-wrapper')}>
-      {/* eslint-disable */
-      tests.map((failed, idx) => (
-        <div key={`${id}-square-${idx}`} className={cx('square', { failed })} />
-      ))
-      /* eslint-disable */
-      }
-    </div>
-  </div>
+const renderForBool = (id) => (failed, idx) => (
+  <div key={`${id}-square-${idx}`} className={cx('square', { 'most-failed': failed })} />
 );
 
-Matrix.propTypes = {
-  tests: arrayOf(bool).isRequired,
-  id: string.isRequired,
-};
+const renderForString = (id) => (status, idx) => (
+  <div key={`${id}-square-${idx}`} className={cx('square', status.toLowerCase())} />
+);
 
-export default Matrix;
+function matrixFactory(renderBool) {
+  const renderFn = renderBool ? renderForBool : renderForString;
+  console.log('RENDER BOOL: ', renderBool);
+  const Matrix = ({ tests, id }) => (
+    <div className={cx('matrix')}>
+      <div className={cx('squares-wrapper')}>
+        {/* eslint-disable */
+        tests.map(renderFn(id))
+        /* eslint-disable */
+        }
+      </div>
+    </div>
+  );
+
+  Matrix.propTypes = {
+    tests: arrayOf(bool).isRequired,
+    id: string.isRequired,
+  };
+
+  return Matrix;
+}
+
+export default matrixFactory;
