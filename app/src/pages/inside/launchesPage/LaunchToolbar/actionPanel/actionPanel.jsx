9cd98cfd157f5197d733b6b7aa2c15b48f5c8a5b
Artsiom_Yeliseyenka <artsiom_yeliseyenka@epam.com>
2018-04-12 09:28:06 +0300

    Added GhostMenuButton component.

--- app/src/pages/inside/launchesPage/LaunchToolbar/actionPanel/actionPanel.jsx
+++ app/src/pages/inside/launchesPage/LaunchToolbar/actionPanel/actionPanel.jsx
@@ -1,41 +1,109 @@
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
+import { defineMessages, FormattedMessage, injectIntl, intlShape } from 'react-intl';
 import classNames from 'classnames/bind';
 import { GhostButton } from 'components/buttons/ghostButton';
+import { GhostMenuButton } from 'components/buttons/ghostMenuButton';
 import ImportIcon from './img/import-inline.svg';
 import RefreshIcon from './img/refresh-inline.svg';
 import styles from './actionPanel.scss';
 
 const cx = classNames.bind(styles);
+const messages = defineMessages({
+  actionsBtn: {
+    id: 'ActionPanel.actionsBtn',
+    defaultMessage: 'Actions',
+  },
+  actionMerge: {
+    id: 'ActionPanel.actionMerge',
+    defaultMessage: 'Merge',
+  },
+  actionCompare: {
+    id: 'ActionPanel.actionCompare',
+    defaultMessage: 'Compare',
+  },
+  actionMoveToDebug: {
+    id: 'ActionPanel.actionMoveToDebug',
+    defaultMessage: 'Move to debug',
+  },
+  actionForceFinish: {
+    id: 'ActionPanel.actionForceFinish',
+    defaultMessage: 'Force finish',
+  },
+  actionDelete: {
+    id: 'ActionPanel.actionDelete',
+    defaultMessage: 'Delete',
+  },
+});
 
-export const ActionPanel = ({ showBreadcrumb, hasErrors }) => (
-  <div className={cx('action-panel')}>
-    {showBreadcrumb ? <div className={cx('breadcrumb')} /> : <div />}
-    {hasErrors && <GhostButton>Proceed Valid Items</GhostButton>}
-    <div className={cx('action-buttons')}>
-      <div className={cx('action-button', 'mobile-hidden')}>
-        <GhostButton icon={ImportIcon} disabled>
-          <FormattedMessage id="LaunchesPage.import" defaultMessage="Import" />
-        </GhostButton>
+@injectIntl
+export class ActionPanel extends Component {
+  static propTypes = {
+    hasErrors: PropTypes.bool,
+    showBreadcrumb: PropTypes.bool,
+    intl: intlShape.isRequired,
+  };
+  static defaultProps = {
+    hasErrors: false,
+    showBreadcrumb: false,
+  };
+  onMerge = () => {};
+  onCompare = () => {};
+  onMoveToDebug = () => {};
+  onForceFinish = () => {};
+  onDelete = () => {};
+
+  render() {
+    const { intl, showBreadcrumb, hasErrors } = this.props;
+    return (
+      <div className={cx('action-panel')}>
+        {showBreadcrumb ? <div className={cx('breadcrumb')} /> : <div />}
+        {hasErrors && <GhostButton>Proceed Valid Items</GhostButton>}
+        <div className={cx('action-buttons')}>
+          <div className={cx('action-button', 'mobile-hidden')}>
+            <GhostButton icon={ImportIcon} disabled>
+              <FormattedMessage id="LaunchesPage.import" defaultMessage="Import" />
+            </GhostButton>
+          </div>
+          <div className={cx('action-button', 'mobile-hidden')}>
+            <GhostMenuButton
+              title={intl.formatMessage(messages.actionsBtn)}
+              items={[
+                {
+                  label: intl.formatMessage(messages.actionMerge),
+                  value: 'action-merge',
+                  onClick: this.onMerge,
+                },
+                {
+                  label: intl.formatMessage(messages.actionCompare),
+                  value: 'action-compare',
+                  onClick: this.onCompare,
+                },
+                {
+                  label: intl.formatMessage(messages.actionMoveToDebug),
+                  value: 'action-move-to-debug',
+                  onClick: this.onMoveToDebug,
+                },
+                {
+                  label: intl.formatMessage(messages.actionForceFinish),
+                  value: 'action-force-finish',
+                  onClick: this.onForceFinish,
+                },
+                {
+                  label: intl.formatMessage(messages.actionDelete),
+                  value: 'action-delete',
+                  onClick: this.onDelete,
+                },
+              ]}
+            />
+          </div>
+          <div className={cx('action-button')}>
+            <GhostButton icon={RefreshIcon} disabled>
+              <FormattedMessage id="LaunchesPage.refresh" defaultMessage="Refresh" />
+            </GhostButton>
+          </div>
+        </div>
       </div>
-      <div className={cx('action-button', 'mobile-hidden')}>
-        <GhostButton disabled>
-          <FormattedMessage id="LaunchesPage.actions" defaultMessage="Actions" />
-        </GhostButton>
-      </div>
-      <div className={cx('action-button')}>
-        <GhostButton icon={RefreshIcon} disabled>
-          <FormattedMessage id="LaunchesPage.refresh" defaultMessage="Refresh" />
-        </GhostButton>
-      </div>
-    </div>
-  </div>
-);
-ActionPanel.propTypes = {
-  hasErrors: PropTypes.bool,
-  showBreadcrumb: PropTypes.bool,
-};
-ActionPanel.defaultProps = {
-  hasErrors: false,
-  showBreadcrumb: false,
-};
+    );
+  }
+}
