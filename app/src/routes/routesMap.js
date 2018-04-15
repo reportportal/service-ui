9838f5f75bbfbc4b22084b6a5961d8d56f6fd225
bkushnir <bogdan_kushnir@epam.com>
2018-04-16 01:15:34 +0300

    [EPMRPP-33984] code cleanup

--- app/src/routes/routesMap.js
+++ app/src/routes/routesMap.js
@@ -13,64 +13,52 @@ const redirectRoute = (path, createNewAction) => ({
   path,
   thunk: (dispatch, getState) => {
     const { location } = getState();
-    const action = createNewAction(location.payload);
-    dispatch(redirect(action));
+    const newAction = createNewAction(location.payload);
+    dispatch(redirect(newAction));
   },
 });
 
-const requiresAnonymous = ({ type }) => {
-  switch (type) {
-    case LOGIN_PAGE:
-    case REGISTRATION_PAGE:
-      return true;
-    default:
-      return false;
-  }
-};
-
-const requiresAuthorized = (action) => !requiresAnonymous(action);
-
 export const onBeforeRouteChange = (dispatch, getState, { action }) => {
   const authorized = isAuthorizedSelector(getState());
-  if (authorized) {
-    const projectId = activeProjectSelector(getState());
-    if (requiresAnonymous(action)) {
-      dispatch(redirect({ type: PROJECT_PAGE, payload: { projectId } }));
-    } else {
-      const hashProject = projectId;
-      const userProjects = userInfoSelector(getState()).assigned_projects;
-      if (
-        userProjects &&
-        Object.prototype.hasOwnProperty.call(userProjects, hashProject) &&
-        hashProject !== activeProjectSelector(getState())
-      ) {
-        dispatch(setActiveProjectAction(hashProject));
-        dispatch(fetchProjectAction(hashProject));
-      }
-    }
-  } else if (requiresAuthorized(action)) {
-    if (action.type !== LOGIN_PAGE) {
-      dispatch(redirect({ type: LOGIN_PAGE }));
-    }
+  if (!authorized) {
+    return;
+  }
+
+  const { projectId: hashProject } = action.payload;
+  // TODO: move following after if
+  const activeProjectId = activeProjectSelector(getState());
+  if (!hashProject) {
+    return;
+  }
+
+  const userInfo = userInfoSelector(getState());
+  const userProjects = userInfo ? userInfo.assigned_projects : {};
+  if (
+    userProjects &&
+    Object.prototype.hasOwnProperty.call(userProjects, hashProject) &&
+    hashProject !== activeProjectId
+  ) {
+    dispatch(setActiveProjectAction(hashProject));
+    dispatch(fetchProjectAction(hashProject));
   }
 };
 
 export default {
   HOME_PAGE: redirectRoute('/', (payload) => ({ type: LOGIN_PAGE, payload })),
 
-  LOGIN_PAGE: '/login',
-  REGISTRATION_PAGE: '/registration',
+  [LOGIN_PAGE]: '/login',
+  [REGISTRATION_PAGE]: '/registration',
 
   ADMINISTRATE_PAGE: '/administrate',
   USER_PROFILE_PAGE: '/user-profile',
 
   API_PAGE: '/api',
 
-  PROJECT_PAGE: redirectRoute('/:projectId', () => ({
+  [PROJECT_PAGE]: redirectRoute('/:projectId', (payload) => ({
     type: PROJECT_DASHBOARD_PAGE,
-    payload: { projectId: 'default_project' },
+    payload: { projectId: payload.projectId || 'default_project' },
   })),
-  PROJECT_DASHBOARD_PAGE: '/:projectId/dashboard',
+  [PROJECT_DASHBOARD_PAGE]: '/:projectId/dashboard',
   PROJECT_LAUNCHES_PAGE: '/:projectId/launches',
   PROJECT_FILTERS_PAGE: '/:projectId/filters',
   PROJECT_USERDEBUG_PAGE: '/:projectId/userdebug',
