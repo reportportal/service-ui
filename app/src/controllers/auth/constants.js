d947217e8d8e042c98481e60fc61d11b9ebbe3e4
Ihor <serkov.ihor@gmail.com>
2018-05-11 15:24:54 +0200

    refactored auth controller with sagas

--- app/src/controllers/auth/constants.js
+++ app/src/controllers/auth/constants.js
@@ -1,7 +1,9 @@
 export const TOKEN_KEY = 'token';
 export const DEFAULT_TOKEN = 'Basic dWk6dWltYW4=';
 export const AUTH_SUCCESS = 'authSuccessAction';
+export const AUTH_ERROR = 'authErrorAction';
 export const LOGOUT = 'logoutAction';
+export const LOGIN = 'loginAction';
 export const INITIAL_STATE = {
   authorized: false,
 };
