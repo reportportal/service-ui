9850ba3aff42b3e2281a877d3712d966bd6c2046
Ihor <serkov.ihor@gmail.com>
2018-05-14 11:43:08 +0200

    added placeholders for notifications translations

--- app/src/controllers/notification/actionCreators.js
+++ app/src/controllers/notification/actionCreators.js
@@ -5,14 +5,16 @@ import { HIDE_NOTIFICATION, SHOW_NOTIFICATION } from './constants';
  * @param message? {string}
  * @param type {('error' | 'info' | 'success')}
  * @param messageId? {string}
+ * @param values? {object}
  * @returns {{type: string, payload: {message: string, type: (string), uid: number}}}
  */
-export const showNotification = ({ message, type, messageId }) => ({
+export const showNotification = ({ message, type, messageId, values }) => ({
   type: SHOW_NOTIFICATION,
   payload: {
     message,
     messageId,
     type,
+    values,
     uid: new Date().valueOf(),
   },
 });
