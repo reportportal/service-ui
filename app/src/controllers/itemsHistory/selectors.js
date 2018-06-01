c8c59d7c8960b60671e8a8661f4797ced4e14d22
Yana_Zaitsava <Zaitsava_Yana@epam.com>
2018-06-01 21:09:05 +0300

    EPMRPP-33151 memoization historyItemsSelector
    Signed-off-by: Yana_Zaitsava <Zaitsava_Yana@epam.com>

--- app/src/controllers/itemsHistory/selectors.js
+++ app/src/controllers/itemsHistory/selectors.js
@@ -1,3 +1,4 @@
+import { createSelector } from 'reselect';
 import { itemsSelector } from 'controllers/testItem';
 
 const domainSelector = (state) => state.itemsHistory || {};
@@ -5,14 +6,14 @@ export const itemsHistorySelector = (state) => domainSelector(state).items;
 export const historySelector = (state) => domainSelector(state).history;
 export const visibleItemsCountSelector = (state) => domainSelector(state).visibleItemsCount;
 export const loadingSelector = (state) => domainSelector(state).loading;
-export const historyItemsSelector = (state) => {
+export const historyItemsSelector = createSelector(itemsSelector, (testItems) => {
   const launchesToRender = [];
 
-  itemsSelector(state).forEach((item) => {
+  testItems.forEach((item) => {
     const sameName = launchesToRender.filter((obj) => obj.uniqueId === item.uniqueId);
     if (!sameName.length) {
       launchesToRender.push(item);
     }
   });
   return launchesToRender;
-};
+});
