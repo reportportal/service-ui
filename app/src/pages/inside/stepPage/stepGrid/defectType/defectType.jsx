093a3d2136bc4d2013289189e11f337f8993b1fb
Maxim Tumas <DiscoElevator@users.noreply.github.com>
2018-07-27 15:02:06 +0300

    EPMRPP-33146: Add edit defect type icon

--- app/src/pages/inside/stepPage/stepGrid/defectType/defectType.jsx
+++ app/src/pages/inside/stepPage/stepGrid/defectType/defectType.jsx
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
+import Parser from 'html-react-parser';
 import { MarkdownViewer } from 'components/main/markdown';
+import PencilIcon from 'common/img/pencil-icon-inline.svg';
 import { DefectTypeItem } from './defectTypeItem';
 import { AutoAnalyzedLabel } from './autoAnalyzedLabel';
 import { IssueList } from './issueList';
@@ -19,6 +21,7 @@ export const DefectType = ({ issue }) => (
     <div className={cx('defect-type-labels')}>
       {issue.autoAnalyzed && <AALabel />}
       {issue.issue_type && <DefectTypeItem type={issue.issue_type} />}
+      <div className={cx('edit-icon')}>{Parser(PencilIcon)}</div>
     </div>
     <div className={cx('issues')}>
       <IssueList issues={issue.externalSystemIssues} />
@@ -31,4 +34,8 @@ export const DefectType = ({ issue }) => (
 
 DefectType.propTypes = {
   issue: PropTypes.object.isRequired,
+  onEdit: PropTypes.func,
+};
+DefectType.defaultProps = {
+  onEdit: () => {},
 };
