7d804ea104726b9e88926b427695baad170f6d3b
Maxim Tumas <DiscoElevator@users.noreply.github.com>
2018-08-01 12:10:23 +0300

    EPMRPP-33146: Add ignore in AA label

--- app/src/pages/inside/stepPage/stepGrid/defectType/defectType.jsx
+++ app/src/pages/inside/stepPage/stepGrid/defectType/defectType.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import Parser from 'html-react-parser';
+import { FormattedMessage, injectIntl, defineMessages } from 'react-intl';
 import { MarkdownViewer } from 'components/main/markdown';
 import PencilIcon from 'common/img/pencil-icon-inline.svg';
 import { DefectTypeItem } from './defectTypeItem';
@@ -9,6 +10,19 @@ import { IssueList } from './issueList';
 import styles from './defectType.scss';
 
 const cx = classNames.bind(styles);
+
+const messages = defineMessages({
+  ignoreAATooltip: {
+    id: 'StepGrid.ignoreAutoanalyzerTooltip',
+    defaultMessage: 'Ignore item in Auto-Analysis',
+  },
+});
+
+const IgnoredInAALabel = injectIntl(({ intl }) => (
+  <div className={cx('ignore-aa-label')} title={intl.formatMessage(messages.ignoreAATooltip)}>
+    <FormattedMessage id="StepGrid.ignoreAAShort" defaultMessage="Ignore AA" />
+  </div>
+));
 
 const AALabel = () => (
   <div className={cx('aa-label')}>
@@ -19,6 +33,7 @@ const AALabel = () => (
 export const DefectType = ({ issue }) => (
   <div className={cx('defect-type')}>
     <div className={cx('defect-type-labels')}>
+      {issue.ignoreAnalyzer && <IgnoredInAALabel />}
       {issue.autoAnalyzed && <AALabel />}
       {issue.issue_type && <DefectTypeItem type={issue.issue_type} />}
       <div className={cx('edit-icon')}>{Parser(PencilIcon)}</div>
