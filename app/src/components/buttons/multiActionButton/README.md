6516037a9f81ab0db021913b74ce6be2592db131
AmsterGet <amsterget@gmail.com>
2018-08-27 17:23:08 +0300

    Added MultiActionButton component

---
+++ app/src/components/buttons/multiActionButton/README.md
@@ -0,0 +1,16 @@
+## **Multi action button component**
+
+Has 30px height & width = 100% of it's parent.
+
+> To define a new color your should describe it in multiActionButton.scss file.
+
+### Props:
+
+- **disabled**: _bool_, optional, default = false
+- **items**: _array_ of { value: _string_, label: _string_, onClick: _func_, disabled: _bool_ }, optional, default = []
+- **title**: _string_, optional, default = ""
+- **color**: _string_, optional, default = "booger"
+
+### Events:
+
+- **onClick**
---
+++ app/src/components/buttons/multiActionButton/multiActionButton.stories.jsx
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2017 EPAM Systems
+ *
+ *
+ * This file is part of EPAM Report Portal.
+ * https://github.com/reportportal/service-ui
+ *
+ * Report Portal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Report Portal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { host } from 'storybook-host';
+import { withReadme } from 'storybook-readme';
+import { MultiActionButton } from './multiActionButton';
+import README from './README.md';
+
+const items = [
+  {
+    label: 'Save and post issue',
+    value: 'Post',
+    onClick: action('clicked'),
+  },
+  {
+    label: 'Save and link issue',
+    value: 'Link',
+    onClick: action('clicked'),
+  },
+  {
+    label: 'Save and unlink issue',
+    value: 'Unlink',
+    onClick: action('clicked'),
+    disabled: true,
+  },
+];
+
+storiesOf('Components/Buttons/MultiActionButton', module)
+  .addDecorator(
+    host({
+      title: 'Multi action button component',
+      align: 'center middle',
+      backdrop: 'rgba(70, 69, 71, 0.2)',
+      background: '#fff',
+      height: 130,
+      width: 170,
+    }),
+  )
+  .addDecorator(withReadme(README))
+  .add('default state', () => (
+    <div style={{ display: 'inline-block' }}>
+      <MultiActionButton />
+    </div>
+  ))
+  .add('with title', () => (
+    <div style={{ display: 'inline-block' }}>
+      <MultiActionButton title="Save" />
+    </div>
+  ))
+  .add('with title & disabled', () => (
+    <div style={{ display: 'inline-block' }}>
+      <MultiActionButton title="Save" disabled />
+    </div>
+  ))
+  .add('with title & color=organish', () => (
+    <div style={{ display: 'inline-block' }}>
+      <MultiActionButton color="organish" title="Save" />
+    </div>
+  ))
+  .add('with title & color=organish & disabled', () => (
+    <div style={{ display: 'inline-block' }}>
+      <MultiActionButton color="organish" title="Save" disabled />
+    </div>
+  ))
+  .add('with title & items & actions', () => (
+    <div style={{ display: 'inline-block' }}>
+      <MultiActionButton onClick={action('clicked')} items={items} title="Save" />
+    </div>
+  ));
