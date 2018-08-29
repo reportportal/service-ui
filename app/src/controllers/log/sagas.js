fc8139169313f937436febd43150a50d05a3e9e6
AmsterGet <amsterget@gmail.com>
2018-08-29 19:03:38 +0300

    'Fixed review issues in contollers/log'

--- app/src/controllers/log/sagas.js
+++ app/src/controllers/log/sagas.js
@@ -2,14 +2,9 @@ import { takeEvery, all, put, select } from 'redux-saga/effects';
 import { fetchDataAction } from 'controllers/fetch';
 import { activeProjectSelector } from 'controllers/user';
 import { URLS } from 'common/urls';
-import { logItemIdSelector } from './selectors';
+import { logItemIdSelector } from 'controllers/pages';
 import { setActiveHistoryItemAction } from './actionCreators';
-import {
-  FETCH_HISTORY_ENTRIES,
-  CHANGE_ACTIVE_HISTORY_ITEM,
-  DEFAULT_HISTORY_DEPTH,
-  NAMESPACE,
-} from './constants';
+import { FETCH_HISTORY_ENTRIES, DEFAULT_HISTORY_DEPTH, NAMESPACE } from './constants';
 
 function* getHistoryEntries() {
   const activeProject = yield select(activeProjectSelector);
@@ -23,18 +18,10 @@ function* getHistoryEntries() {
   yield put(setActiveHistoryItemAction(logItemId));
 }
 
-function* changeActiveHistoryItem({ payload }) {
-  yield put(setActiveHistoryItemAction(payload));
-}
-
 function* watchFetchHistoryEntries() {
   yield takeEvery(FETCH_HISTORY_ENTRIES, getHistoryEntries);
 }
 
-function* watchChangeActiveHistoryItem() {
-  yield takeEvery(CHANGE_ACTIVE_HISTORY_ITEM, changeActiveHistoryItem);
-}
-
 export function* logSagas() {
-  yield all([watchFetchHistoryEntries(), watchChangeActiveHistoryItem()]);
+  yield all([watchFetchHistoryEntries()]);
 }
