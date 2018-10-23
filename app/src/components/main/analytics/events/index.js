4adb5de950c10fc9096aefa62fcbd1714869aaf5
Yana_Zaitsava <Zaitsava_Yana@epam.com>
2018-10-23 12:05:52 +0300

    Analytics. Add events import
    Signed-off-by: Yana_Zaitsava <Zaitsava_Yana@epam.com>

--- app/src/components/main/analytics/events/index.js
+++ app/src/components/main/analytics/events/index.js
@@ -1,4 +1,7 @@
 export { FILTERS_PAGE_EVENTS, FILTERS_PAGE } from './filtersPageEvents';
 export { LAUNCHES_PAGE_EVENTS, LAUNCHES_PAGE } from './launchesPageEvents';
 export { LOGIN_PAGE_EVENTS, LOGIN_PAGE } from './loginPageEvents';
-export { SIDEBAR_EVENTS, HEADER_EVENTS } from './sidebarHeaderFooterEvents';
+export { SIDEBAR_EVENTS, HEADER_EVENTS, FOOTER_EVENTS } from './sidebarHeaderFooterEvents';
+export { MEMBERS_PAGE, MEMBERS_PAGE_EVENTS } from './membersPageEvents';
+export { PROFILE_PAGE, PROFILE_PAGE_EVENTS } from './profilePageEvents';
+export { SETTINGS_PAGE, SETTINGS_PAGE_EVENTS } from './settingsPageEvents';
