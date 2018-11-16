c1159387ed355d34f1635f3869ba0a9636bdc314
Andrei Bulagin <Andrei_Bulagin@epam.com>
2018-11-16 12:11:47 +0300

    EPMRPP-33456: Code review fixes - 1

--- app/src/components/widgets/charts/common/utils/chartRange.js
+++ app/src/components/widgets/charts/common/utils/chartRange.js
@@ -1,20 +1,13 @@
+import { range } from 'common/utils/range';
+
 export const rangeMaxValue = (itemsLength) => (itemsLength > 6 ? Math.round(itemsLength / 12) : 1);
 
-export const getLaunchAxisTicks = (itemsLength) => {
-  const step = rangeMaxValue(itemsLength);
-  const array = [];
-  for (let iterator = 0; iterator < itemsLength; iterator += step) {
-    array.push(iterator);
-  }
-  return array;
-};
+export const getLaunchAxisTicks = (itemsLength) =>
+  range(0, itemsLength, rangeMaxValue(itemsLength));
 
 export const getTimelineAxisTicks = (itemsLength) => {
   const start = itemsLength > 5 ? ((itemsLength / 5 / 2).toFixed() / 2).toFixed() : 0;
   const step = itemsLength > 5 ? (itemsLength / 5).toFixed() : 1;
-  const result = [];
-  for (let iterator = start; iterator < itemsLength; iterator += step) {
-    result.push(iterator);
-  }
-  return result;
+
+  return range(start, itemsLength, step);
 };
