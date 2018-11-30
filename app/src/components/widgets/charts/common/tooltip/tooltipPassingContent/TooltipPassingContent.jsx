2e886b93fe1e5e0324770600837191bd5019bf35
tamas-levente-szucs-epam <tamas_levente_szucs@epam.com>
2018-11-30 13:52:46 +0100

    EPMRPP-33463 Passing Rate Per Launch corrections

--- app/src/components/widgets/charts/common/tooltip/tooltipPassingContent/TooltipPassingContent.jsx
+++ app/src/components/widgets/charts/common/tooltip/tooltipPassingContent/TooltipPassingContent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-import styles from './tooltipContent.scss';
+import styles from './TooltipPassingContent.scss';
 
 const cx = classNames.bind(styles);
 
