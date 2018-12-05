b4051652ae79ba26131951c5453368b12b65f8f4
Aleksandra Ivleva <Aleksandra_Ivleva@epam.com>
2018-12-05 15:58:34 +0300

    EPMRPP-37006: Admin part. Event monitoring. Layout

---
+++ app/src/controllers/administrate/actionCreators.js
@@ -0,0 +1,5 @@
+import { FETCH_PROJECT_DATA } from './constants';
+
+export const fetchProjectDataAction = () => ({
+  type: FETCH_PROJECT_DATA,
+});
--- app/src/controllers/administrate/index.js
+++ app/src/controllers/administrate/index.js
@@ -1,4 +1,6 @@
 export { administrateReducer } from './reducer';
 export { projectSelector, fetchProjectAction } from './project';
+export { eventsSelector, fetchEventsAction } from './events';
+export { fetchProjectDataAction } from './actionCreators';
 export { NAMESPACE as ADMIN_NAMESPACE } from './constants';
 export { administrateSaga } from './sagas';
