a0a555fb47a235df33dc3843c9316e85152bca39
AmsterGet <amsterget@gmail.com>
2018-12-10 17:05:36 +0300

    EPMRPP-33316: Code review fixes - 1

--- app/src/common/constants/fieldTypes.js
+++
@@ -1,4 +0,0 @@
-export const TEXT_TYPE = 'text';
-export const ARRAY_TYPE = 'array';
-export const DROPDOWN_TYPE = 'dropdown';
-export const DATE_TYPE = 'date';
---
+++ app/src/components/fields/dynamicFieldsSection/fields/index.js
@@ -0,0 +1,4 @@
+export { TextField } from './textField/index';
+export { DateField } from './dateField/index';
+export { DropdownField } from './dropdownField/index';
+export { ArrayField } from './arrayField/index';
