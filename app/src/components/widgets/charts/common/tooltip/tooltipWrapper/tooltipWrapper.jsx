cb1e03735485ed5b4e9f58064a982ce02fa37f4e
BUDAPEST\Attila_Batky <Attila_Batky@epam.com>
2018-12-11 15:16:57 +0100

    EPMRPP-33454: Code review fixes - 3

--- app/src/components/widgets/charts/common/tooltip/tooltipWrapper/tooltipWrapper.jsx
+++ app/src/components/widgets/charts/common/tooltip/tooltipWrapper/tooltipWrapper.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
+import classnames from 'classnames';
 import styles from './tooltipWrapper.scss';
 
 const cx = classNames.bind(styles);
 
-export const TooltipWrapper = ({ children }) => <div className={cx('tooltip')}>{children}</div>;
+export const TooltipWrapper = ({ children, className }) => (
+  <div className={classnames(cx('tooltip'), className)}>{children}</div>
+);
 
 TooltipWrapper.propTypes = {
   children: PropTypes.node,
+  className: PropTypes.string,
 };
 
 TooltipWrapper.defaultProps = {
   children: null,
+  className: null,
 };
