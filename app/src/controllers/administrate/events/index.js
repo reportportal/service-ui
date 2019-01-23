fae922743a4c1b3e7bc3305f434a9f9a71068591
Aleksandra Ivleva <Aleksandra_Ivleva@epam.com>
2019-01-23 12:01:01 +0300

    EPMRPP-37758: Event monitoring. Fix pagination

--- app/src/controllers/administrate/events/index.js
+++ app/src/controllers/administrate/events/index.js
@@ -2,3 +2,4 @@ export { fetchEventsAction } from './actionCreators';
 export { eventsReducer } from './reducer';
 export { eventsPaginationSelector, eventsSelector, loadingSelector } from './selectors';
 export { eventsSagas } from './sagas';
+export { DEFAULT_PAGE_SIZE, NAMESPACE, FETCH_EVENT } from './constants';
