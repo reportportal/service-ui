9031f4751607c79bcf78a7cab49ebe336b12d4f3
Maxim Tumas <DiscoElevator@users.noreply.github.com>
2019-01-24 16:40:04 +0300

    EPMRPP-36821: Code review fixes - 1

--- app/src/pages/inside/stepPage/stepGrid/itemInfoWithRetries/itemInfoWithRetries.jsx
+++ app/src/pages/inside/stepPage/stepGrid/itemInfoWithRetries/itemInfoWithRetries.jsx
@@ -1,13 +1,18 @@
-import { Component, Fragment, createRef } from 'react';
+import { Component, createRef } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
 import { ItemInfo } from 'pages/inside/common/itemInfo';
 import { RetriesBlock } from './retriesBlock';
+import styles from './itemInfoWithRetries.scss';
+
+const cx = classNames.bind(styles);
 
 export class ItemInfoWithRetries extends Component {
   static propTypes = {
     expanded: PropTypes.bool,
     value: PropTypes.object.isRequired,
     toggleExpand: PropTypes.func.isRequired,
+    refFunction: PropTypes.func.isRequired,
   };
 
   static defaultProps = {
@@ -27,10 +32,7 @@ export class ItemInfoWithRetries extends Component {
   getRetries = () => [...this.props.value.retries, this.props.value];
 
   isRetriesVisible = () =>
-    this.props.expanded &&
-    this.state.retriesVisible &&
-    this.props.value.retries &&
-    this.props.value.retries.length > 0;
+    this.state.retriesVisible && this.props.value.retries && this.props.value.retries.length > 0;
 
   showRetries = () => {
     if (!this.props.expanded) {
@@ -44,17 +46,22 @@ export class ItemInfoWithRetries extends Component {
   };
 
   render() {
+    const { refFunction, ...rest } = this.props;
     return (
-      <Fragment>
-        <ItemInfo onClickRetries={this.showRetries} {...this.props} />
+      <div
+        ref={refFunction}
+        className={cx('item-info-with-retries', { collapsed: !rest.expanded })}
+      >
+        <ItemInfo onClickRetries={this.showRetries} {...rest} />
         {this.isRetriesVisible() && (
           <RetriesBlock
             ref={this.retriesNode}
             testItemId={this.props.value.id}
             retries={this.getRetries()}
+            collapsed={!rest.expanded}
           />
         )}
-      </Fragment>
+      </div>
     );
   }
 }
