5588981733d59838e3cd1b75382964efebe6fb4c
AmsterGet <amsterget@gmail.com>
2019-02-13 12:15:57 +0300

    EPMRPP-37762 React. Log page. Attachments fixes

--- app/src/controllers/log/attachments/index.js
+++ app/src/controllers/log/attachments/index.js
@@ -1,6 +1,6 @@
 export { openAttachmentAction } from './actionCreators';
 export {
-  FILE_EXTENSIONS_MAP,
+  FILE_PREVIEWS_MAP,
   FILE_MODAL_IDS_MAP,
   ATTACHMENT_CODE_MODAL_ID,
   ATTACHMENT_HAR_FILE_MODAL_ID,
--- app/src/controllers/log/attachments/utils.js
+++ app/src/controllers/log/attachments/utils.js
@@ -1,22 +1,30 @@
 import { URLS } from 'common/urls';
 import { IMAGE } from 'common/constants/fileTypes';
 import attachment from 'common/img/attachments/attachment.svg';
-import { FILE_EXTENSIONS_MAP, FILE_MODAL_IDS_MAP } from './constants';
+import { FILE_PREVIEWS_MAP, FILE_MODAL_IDS_MAP, FILE_PATTERNS_MAP } from './constants';
 
 const getAttachmentTypeConfig = (contentType) =>
   (contentType && contentType.toLowerCase().split('/')) || '';
 
 export const extractExtension = (contentType) => getAttachmentTypeConfig(contentType)[1] || '';
 
+export const getExtensionFromPattern = (extensionString) =>
+  Object.keys(FILE_PATTERNS_MAP).find((key) => !!FILE_PATTERNS_MAP[key].exec(extensionString));
+
 export const getFileIconSource = (item) => {
   const [fileType, extension] = getAttachmentTypeConfig(item.contentType);
   if (fileType === IMAGE) {
     return URLS.getFileById(item.id);
   }
-  return FILE_EXTENSIONS_MAP[extension] || attachment;
+  const extensionFromPattern = getExtensionFromPattern(extension);
+  return FILE_PREVIEWS_MAP[extension] || FILE_PREVIEWS_MAP[extensionFromPattern] || attachment;
 };
 
 export const getAttachmentModalId = (contentType) => {
   const [fileType, extension] = getAttachmentTypeConfig(contentType);
-  return FILE_MODAL_IDS_MAP[fileType === IMAGE ? IMAGE : extension];
+  const extensionFromPattern = getExtensionFromPattern(extension);
+  return (
+    FILE_MODAL_IDS_MAP[fileType === IMAGE ? IMAGE : extension] ||
+    FILE_MODAL_IDS_MAP[extensionFromPattern]
+  );
 };
