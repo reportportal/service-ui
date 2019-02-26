96c64bf18f07da3b5058cf0a0b2df7269390c537
AmsterGet <amsterget@gmail.com>
2019-02-26 20:21:52 +0300

    EPMRPP-37762: Code review fixes - 2

--- app/src/controllers/log/attachments/reducer.js
+++ app/src/controllers/log/attachments/reducer.js
@@ -1,9 +1,22 @@
 import { combineReducers } from 'redux';
+import { queueReducers } from 'common/utils';
 import { fetchReducer } from 'controllers/fetch';
 import { loadingReducer } from 'controllers/loading';
-import { ATTACHMENTS_NAMESPACE } from './constants';
+import { ATTACHMENTS_NAMESPACE, CLEAR_ATTACHMENTS_ACTION } from './constants';
+
+export const logsWithAttachmentsReducer = (state = [], { type }) => {
+  switch (type) {
+    case CLEAR_ATTACHMENTS_ACTION:
+      return [];
+    default:
+      return state;
+  }
+};
 
 export const attachmentsReducer = combineReducers({
-  logsWithAttachments: fetchReducer(ATTACHMENTS_NAMESPACE, { contentPath: 'content' }),
+  logsWithAttachments: queueReducers(
+    fetchReducer(ATTACHMENTS_NAMESPACE, { contentPath: 'content' }),
+    logsWithAttachmentsReducer,
+  ),
   loading: loadingReducer(ATTACHMENTS_NAMESPACE),
 });
