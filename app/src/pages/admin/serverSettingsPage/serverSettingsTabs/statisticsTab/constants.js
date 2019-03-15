d239b41a1d1bee78ef14c1e5d534211d0df43cde
AmsterGet <amsterget@gmail.com>
2019-03-15 12:24:39 +0300

    EPMRPP-36620 Analytics fixes

--- app/src/pages/admin/serverSettingsPage/serverSettingsTabs/statisticsTab/constants.js
+++ app/src/pages/admin/serverSettingsPage/serverSettingsTabs/statisticsTab/constants.js
@@ -1,5 +1,5 @@
 export const STATISTICS_ENABLED_KEY = 'enabled';
-export const STATISTICS_TYPE = 'all';
+export const STATISTICS_TYPE = 'server.analytics.all';
 
 export const DEFAULT_FORM_CONFIG = {
   [STATISTICS_ENABLED_KEY]: false,
