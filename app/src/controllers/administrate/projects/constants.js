971c369d53e9643568260fcb6ef0d1343b556ef1
margalencia <margalencia@yandex.ru>
2019-03-15 15:09:29 +0300

    EPMRPP-37314: Add pagination functionality on Administrative/Projects page

--- app/src/controllers/administrate/projects/constants.js
+++ app/src/controllers/administrate/projects/constants.js
@@ -2,7 +2,7 @@ import { PAGE_KEY, SIZE_KEY } from 'controllers/pagination';
 
 export const FETCH_PROJECTS = 'fetchProjects';
 export const NAMESPACE = 'projects';
-export const DEFAULT_PAGE_SIZE = 50;
+export const DEFAULT_PAGE_SIZE = 12;
 export const DEFAULT_PAGINATION = {
   [PAGE_KEY]: 1,
   [SIZE_KEY]: DEFAULT_PAGE_SIZE,
