ef94e9ff8941c9c55ec76cf7d7811e15f8431ce8
Maxim Tumas <DiscoElevator@users.noreply.github.com>
2019-03-18 17:40:53 +0300

    EPMRPP-37903: Code review fix - 1

--- app/src/pages/inside/common/launchFiltersToolbar/filterList/filterItem/filterTooltipIcon/filterTooltipIcon.jsx
+++ app/src/pages/inside/common/launchFiltersToolbar/filterList/filterItem/filterTooltipIcon/filterTooltipIcon.jsx
@@ -1,14 +1,14 @@
 import classNames from 'classnames/bind';
 import Parser from 'html-react-parser';
 import { ALIGN_LEFT } from 'components/main/tooltips/constants';
-import { withHoverableTooltip } from 'components/main/tooltips/hoverableTooltip';
+import { withTooltip } from 'components/main/tooltips/tooltip';
 import { TextTooltip } from 'components/main/tooltips/textTooltip';
 import TooltipIcon from 'common/img/tooltip-icon-inline.svg';
 import styles from './filterTooltipIcon.scss';
 
 const cx = classNames.bind(styles);
 
-export const FilterTooltipIcon = withHoverableTooltip({
+export const FilterTooltipIcon = withTooltip({
   TooltipComponent: TextTooltip,
   data: {
     align: ALIGN_LEFT,
