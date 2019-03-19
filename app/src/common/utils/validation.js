affd54c3e6644d93dea0c9b4600d1ec7009d6f53
AmsterGet <amsterget@gmail.com>
2019-03-19 21:21:32 +0300

    EPMRPP-35630 Max password length fix

--- app/src/common/utils/validation.js
+++ app/src/common/utils/validation.js
@@ -1,6 +1,6 @@
 export const email = (emailValue) =>
   !!/^[a-z0-9.+_-]+@[a-z0-9_.-]+?\.[a-z0-9]{2,}$/i.exec(emailValue);
-export const password = (passwordValue) => !!/^(.){4,25}$/.exec(passwordValue);
+export const password = (passwordValue) => !!/^(.){4,128}$/.exec(passwordValue);
 export const login = (loginValue) => !!/^[0-9a-zA-Z-_.]{1,128}$/.exec(loginValue);
 export const name = (nameValue) => !!/^[a-z0-9._\-\s\u0400-\u04FF]{3,256}$/i.exec(nameValue);
 
