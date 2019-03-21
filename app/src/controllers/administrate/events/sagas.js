194487892ec8d2128de4c6a921cee4509d330dcf
Maxim Tumas <DiscoElevator@users.noreply.github.com>
2019-03-21 13:18:35 +0300

    EPMRPP-38388: Fix project url on events page

--- app/src/controllers/administrate/events/sagas.js
+++ app/src/controllers/administrate/events/sagas.js
@@ -1,12 +1,12 @@
 import { takeEvery, all, put, select } from 'redux-saga/effects';
 import { fetchDataAction } from 'controllers/fetch';
-import { activeProjectSelector } from 'controllers/user';
+import { projectIdSelector } from 'controllers/pages';
 import { URLS } from 'common/urls';
 import { NAMESPACE, FETCH_EVENTS } from './constants';
 import { querySelector } from './selectors';
 
 function* fetchEvents() {
-  const projectId = yield select(activeProjectSelector);
+  const projectId = yield select(projectIdSelector);
   const query = yield select(querySelector);
   yield put(
     fetchDataAction(NAMESPACE)(URLS.events(projectId), {
