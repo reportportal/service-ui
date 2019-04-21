90ef259ce3c84efb90646d14bc86001d55d6e100
Andrey Savitskiy <andrey_savitskiy@epam.com>
2019-04-21 23:37:02 +0300

    EPMRPP-37146. Search on dashboards page

--- app/src/common/urls.js
+++ app/src/common/urls.js
@@ -20,7 +20,10 @@ export const URLS = {
     `${urlBase}data/userphoto${getQueryParams({ id, access_token: getToken() })}`,
 
   dashboard: (activeProject, id) => `${urlBase}${activeProject}/dashboard/${id}`,
-  dashboards: (activeProject) => `${urlBase}${activeProject}/dashboard`,
+  dashboards: (activeProject) =>
+    `${urlBase}${activeProject}/dashboard${getQueryParams({
+      'page.size': 300,
+    })}`,
   dashboardsShared: (activeProject) =>
     `${urlBase}${activeProject}/dashboard/shared${getQueryParams({
       'page.page': 1,
