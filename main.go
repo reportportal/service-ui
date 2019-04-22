9a84619f23cbce637677ee02605e65818fe6b3a9
AmsterGet <amsterget@gmail.com>
2019-04-22 19:59:21 +0300

    Csp fix

--- main.go
+++ main.go
@@ -59,8 +59,9 @@ func main() {
 				"*.epam.com",
 				"*.uservoice.com",
 			},
-			"img-src":    {"'self'", "data:", "www.google-analytics.com", "stats.g.doubleclick.net", "*.epam.com", "*.uservoice.com"},
-			"object-src": {"'self'"},
+			"img-src":    {"'self'", "data:", "www.google-analytics.com", "stats.g.doubleclick.net", "*.epam.com", "*.uservoice.com", "*.saucelabs.com"},
+			"video-src": {"'self'", "*.saucelabs.com"},
+			"object-src": {"'self'", "*.saucelabs.com"},
 		}
 
 		//apply content security policies
