f596aaa09dd351d7435ec394b5563bf86a5c83c3
ElenaRomanchuk <elena_romanchuk@epam.com>
2019-06-03 00:23:00 +0300

    EPMRPP-39153. Redesign pattern tab.

--- app/src/pages/inside/stepPage/stepGrid/defectType/patternAnalyzedLabel/patternAnalyzedLabel.jsx
+++ app/src/pages/inside/stepPage/stepGrid/defectType/patternAnalyzedLabel/patternAnalyzedLabel.jsx
@@ -1,6 +1,8 @@
 import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 import { withTooltip } from 'components/main/tooltips/tooltip';
+import Parser from 'html-react-parser';
+import PAIcon from 'common/img/pa_icon-inline.svg';
 import { PatternAnalyzedTooltip } from './patternAnalyzedTooltip';
 import styles from './patternAnalyzedLabel.scss';
 
@@ -11,7 +13,7 @@ export const PatternAnalyzedLabel = withTooltip({
   data: {
     width: 280,
   },
-})(() => <div className={cx('pa-label')}>PA</div>);
+})(() => <div className={cx('pa-label')}>{Parser(PAIcon)}</div>);
 
 PatternAnalyzedLabel.propTypes = {
   patternTemplates: PropTypes.array,
