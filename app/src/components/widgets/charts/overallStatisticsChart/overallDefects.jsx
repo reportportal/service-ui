2158ccb770df6ccf1fc5da26732da0d33f70ec50
Andrew Omelchenko <Andrii_Omelchenko@epam.com>
2019-06-04 15:34:33 +0300

    EPMRPP-39158 - React. Dashboard. Overall statistics panel widget. Defect types go out of border when the widget is resized

--- app/src/components/widgets/charts/overallStatisticsChart/overallDefects.jsx
+++ app/src/components/widgets/charts/overallStatisticsChart/overallDefects.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
+import { ScrollWrapper } from 'components/main/scrollWrapper';
 import { getItemNameConfig, DEFECTS } from '../common/utils';
 import { DefectTypeItem } from './defectTypeItem';
 import styles from './overallDefects.scss';
@@ -14,17 +15,19 @@ export class OverallDefects extends React.Component {
   render() {
     const { values } = this.props;
     return (
-      <div className={cx('container')}>
-        {Object.keys(values).map((value) => {
-          const defectItem = getItemNameConfig(value);
+      <ScrollWrapper>
+        <div className={cx('container')}>
+          {Object.keys(values).map((value) => {
+            const defectItem = getItemNameConfig(value);
 
-          return (
-            defectItem.itemType === DEFECTS && (
-              <DefectTypeItem key={value} item={defectItem} value={values[value]} />
-            )
-          );
-        })}
-      </div>
+            return (
+              defectItem.itemType === DEFECTS && (
+                <DefectTypeItem key={value} item={defectItem} value={values[value]} />
+              )
+            );
+          })}
+        </div>
+      </ScrollWrapper>
     );
   }
 }
