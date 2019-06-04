61f3299148e1ac44fa468cfa2526b5674837f133
Andrew Omelchenko <Andrii_Omelchenko@epam.com>
2019-06-04 10:33:57 +0300

    EPMRPP-38912 - React. Remove system condition from the UI

--- app/src/components/filterEntities/containers/utils.js
+++ app/src/components/filterEntities/containers/utils.js
@@ -1,10 +1,7 @@
 import { isEmptyValue } from 'common/utils/isEmptyValue';
-import { ENTITY_ATTRIBUTE_KEYS, ENTITY_ATTRIBUTE_VALUES, CONDITION_EQ } from '../constants';
 
 const FILTER_PREFIX = 'filter.';
 const PREDEFINED_FILTER_PREFIX = 'predefinedFilter.';
-const SYSTEM_ATTRIBUTE = 'attributeSystem';
-const SYSTEM_ATTRIBUTE_FILTER_KEY = `${FILTER_PREFIX}${CONDITION_EQ}.${SYSTEM_ATTRIBUTE}`;
 
 const getFilterKey = (entity, key) =>
   entity.condition
@@ -42,27 +39,6 @@ export const collectFilterEntities = (query = {}) =>
     };
   }, {});
 
-const isAttributeFilterApplied = (entities) =>
-  ENTITY_ATTRIBUTE_KEYS in entities || ENTITY_ATTRIBUTE_VALUES in entities;
-
-const createEmptyAttributesQuery = (newEntities, oldEntities) => {
-  const result = {};
-  const newKey = newEntities[ENTITY_ATTRIBUTE_KEYS];
-  const oldKey = oldEntities[ENTITY_ATTRIBUTE_KEYS];
-  const newValue = newEntities[ENTITY_ATTRIBUTE_VALUES];
-  const oldValue = oldEntities[ENTITY_ATTRIBUTE_VALUES];
-
-  const attributeKey = !newKey && oldKey ? null : (newKey || {}).value;
-  const attributeValue = !newValue && oldValue ? null : (newValue || {}).value;
-
-  if (isEmptyValue(attributeKey) && isEmptyValue(attributeValue)) {
-    result[SYSTEM_ATTRIBUTE_FILTER_KEY] = null;
-  } else {
-    result[SYSTEM_ATTRIBUTE_FILTER_KEY] = false;
-  }
-  return result;
-};
-
 const isConditionChangeWithEmptyValue = (entity = {}, oldEntity = {}) => {
   const entityValue = entity.value || null;
   const oldEntityValue = oldEntity.value || null;
@@ -72,13 +48,8 @@ const isConditionChangeWithEmptyValue = (entity = {}, oldEntity = {}) => {
 export const createFilterQuery = (entities = {}, oldEntities = {}) => {
   const mergedEntities = { ...oldEntities, ...entities };
   const keys = Object.keys(mergedEntities);
-  const initialQuery = isAttributeFilterApplied(mergedEntities)
-    ? createEmptyAttributesQuery(entities, oldEntities)
-    : {};
+  const initialQuery = {};
   return keys.reduce((res, key) => {
-    if (key === SYSTEM_ATTRIBUTE) {
-      return res;
-    }
     const entity = entities[key];
     const oldEntity = oldEntities[key];
     if (isConditionChangeWithEmptyValue(entity, oldEntity)) {
