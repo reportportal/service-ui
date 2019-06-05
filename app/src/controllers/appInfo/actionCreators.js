8b04f16177954d3c3b4c520b962c0abdc1a1c689
AmsterGet <amsterget@gmail.com>
2019-06-05 18:32:45 +0300

    EPMRPP-38818 Fix uat info authorization header

--- app/src/controllers/appInfo/actionCreators.js
+++ app/src/controllers/appInfo/actionCreators.js
@@ -4,4 +4,7 @@ import { API_INFO_NAMESPACE, UAT_INFO_NAMESPACE } from './constants';
 
 export const fetchApiInfoAction = () => fetchDataAction(API_INFO_NAMESPACE, true)(URLS.apiInfo());
 
-export const fetchUatInfoAction = () => fetchDataAction(UAT_INFO_NAMESPACE, true)(URLS.uatInfo());
+export const fetchUatInfoAction = () =>
+  fetchDataAction(UAT_INFO_NAMESPACE, true)(URLS.uatInfo(), {
+    headers: { Authorization: undefined },
+  });
