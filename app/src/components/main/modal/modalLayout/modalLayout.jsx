fa5a67f54281d2f3a03753520caf207e1739410f
Andrew Omelchenko <Andrii_Omelchenko@epam.com>
2019-06-06 11:07:44 +0300

    EPMRPP-39007 - REACT.[Internet Explorer]: Edit window and save filter in launch page isn't displaed fully in IE11

--- app/src/components/main/modal/modalLayout/modalLayout.jsx
+++ app/src/components/main/modal/modalLayout/modalLayout.jsx
@@ -170,31 +170,32 @@ export class ModalLayout extends Component {
       <div className={cx('modal-layout')}>
         <div className={cx('scrolling-content')} onClick={this.onClickModal}>
           <Scrollbars>
-            <CSSTransition
-              timeout={300}
-              in={this.state.shown}
-              classNames={cx('modal-window-animation')}
-              onExited={this.props.hideModalAction}
-            >
-              {(status) => (
-                <div
-                  ref={(modal) => {
-                    this.modal = modal;
-                  }}
-                  className={cx('modal-window', this.props.className)}
-                >
-                  <ModalHeader text={title} onClose={this.onClickCloseIcon} />
-                  <ModalContent>{status !== 'exited' ? children : null}</ModalContent>
-
-                  <ModalFooter
-                    {...footerProps}
-                    onClickOk={this.closeModalWithOk}
-                    closeHandler={this.onClickCancelButton}
-                    className={this.props.className}
-                  />
-                </div>
-              )}
-            </CSSTransition>
+            <span>
+              <CSSTransition
+                timeout={300}
+                in={this.state.shown}
+                classNames={cx('modal-window-animation')}
+                onExited={this.props.hideModalAction}
+              >
+                {(status) => (
+                  <div
+                    ref={(modal) => {
+                      this.modal = modal;
+                    }}
+                    className={cx('modal-window', this.props.className)}
+                  >
+                    <ModalHeader text={title} onClose={this.onClickCloseIcon} />
+                    <ModalContent>{status !== 'exited' ? children : null}</ModalContent>
+                    <ModalFooter
+                      {...footerProps}
+                      onClickOk={this.closeModalWithOk}
+                      closeHandler={this.onClickCancelButton}
+                      className={this.props.className}
+                    />
+                  </div>
+                )}
+              </CSSTransition>
+            </span>
           </Scrollbars>
         </div>
         <CSSTransition
