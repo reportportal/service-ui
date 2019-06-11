b577f9ef42b5c7272d75ee6ca61a1dd863870154
ElenaRomanchuk <elena_romanchuk@epam.com>
2019-06-11 17:24:02 +0300

    EPMRPP-39292. Code review fixes -1.

--- app/src/pages/inside/common/itemInfo/itemInfo.jsx
+++ app/src/pages/inside/common/itemInfo/itemInfo.jsx
@@ -147,18 +147,27 @@ export class ItemInfo extends Component {
             <span title={value.name} className={cx('name')}>
               {`${formatItemName(value.name)} `}
             </span>
-            {value.number && <span className={cx('number')}>#{value.number}</span>}
           </NameLink>
-          {canEditLaunch(
-            userAccountRole,
-            userProjectRole,
-            value.owner ? userId === value.owner : userId === launch.owner,
-          ) &&
-            !editDisabled && (
-              <span className={cx('edit-icon')} onClick={this.handleEditItem}>
-                {Parser(PencilIcon)}
-              </span>
-            )}
+          <span className={cx('edit-number-box')}>
+            <NameLink
+              itemId={value.id}
+              ownLinkParams={customProps.ownLinkParams}
+              className={cx('name-link')}
+              onClick={() => tracking.trackEvent(LAUNCHES_PAGE_EVENTS.CLICK_ITEM_NAME)}
+            >
+              {value.number && <span className={cx('number')}>#{value.number}</span>}
+            </NameLink>
+            {canEditLaunch(
+              userAccountRole,
+              userProjectRole,
+              value.owner ? userId === value.owner : userId === launch.owner,
+            ) &&
+              !editDisabled && (
+                <span className={cx('edit-icon')} onClick={this.handleEditItem}>
+                  {Parser(PencilIcon)}
+                </span>
+              )}
+          </span>
           {value.analyzing && <div className={cx('analysis-badge')}>Analysis</div>}
           {isStepLevel && (
             <div className={cx('test-params-icon')} onClick={this.handleShowTestParams}>
