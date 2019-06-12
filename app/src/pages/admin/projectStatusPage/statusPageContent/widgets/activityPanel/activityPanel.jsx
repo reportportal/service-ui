57f49a4a067e8f9f660f1134a9330b74787d45b0
Vitalii Yaremchuk <vitalyar7@gmail.com>
2019-06-12 10:50:12 +0300

    EPMRPP-37005: Code Review - 1

--- app/src/pages/admin/projectStatusPage/statusPageContent/widgets/activityPanel/activityPanel.jsx
+++ app/src/pages/admin/projectStatusPage/statusPageContent/widgets/activityPanel/activityPanel.jsx
@@ -6,58 +6,9 @@ import styles from './activityPanel.scss';
 
 const cx = classNames.bind(styles);
 
-const CONTENT_FIELDS = [
-  'history',
-  'loggedObjectRef',
-  'projectRef',
-  'objectType',
-  'actionType',
-  'lastModified',
-  'user',
-  'name',
-];
-
-const ACTION_TYPE = [
-  'startLaunch',
-  'finishLaunch',
-  'deleteLaunch',
-  'postIssue',
-  'linkIssue',
-  'unlinkIssue',
-  'createUser',
-  'createDashboard',
-  'updateDashboard',
-  'deleteDashboard',
-  'createWidget',
-  'updateWidget',
-  'deleteWidget',
-  'createFilter',
-  'updateFilter',
-  'deleteFilter',
-  'createBts',
-  'updateBts',
-  'deleteBts',
-  'updateProject',
-  'updateAnalyzer',
-  'generateIndex',
-  'deleteIndex',
-  'updateDefect',
-  'deleteDefect',
-  'startImport',
-  'finishImport',
-];
-
-const WIDGET_TYPE = 'activityStream';
-
 export const ActivityPanel = ({ data: { result } }) => {
   const widget = {
     content: { result: result.slice(0, 150) },
-    contentFields: CONTENT_FIELDS,
-    widgetOptions: {
-      actionType: ACTION_TYPE,
-    },
-    share: false,
-    widgetType: WIDGET_TYPE,
   };
 
   return (
