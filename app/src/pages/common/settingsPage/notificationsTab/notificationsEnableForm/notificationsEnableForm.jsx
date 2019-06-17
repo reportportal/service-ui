acbd089beaaf32d7651ddcf436ef4a7305bd4ef1
AmsterGet <amsterget@gmail.com>
2019-06-17 12:27:09 +0300

    EPMRPP-39406 Fix email notification switcher state

--- app/src/pages/common/settingsPage/notificationsTab/notificationsEnableForm/notificationsEnableForm.jsx
+++ app/src/pages/common/settingsPage/notificationsTab/notificationsEnableForm/notificationsEnableForm.jsx
@@ -15,38 +15,28 @@ const messages = defineMessages({
     id: 'NotificationsEnableForm.toggleNotificationsNote',
     defaultMessage: 'Send e-mail notifications about launches finished',
   },
-  turnOn: {
-    id: 'NotificationsEnableForm.turnOn',
-    defaultMessage: 'On',
-  },
-  turnOff: {
-    id: 'NotificationsEnableForm.turnOff',
-    defaultMessage: 'Off',
-  },
 });
 
-@injectIntl
 @reduxForm({
   form: 'notificationsEnableForm',
 })
+@injectIntl
 export class NotificationsEnableForm extends Component {
   static propTypes = {
     intl: intlShape.isRequired,
+    initialize: PropTypes.func.isRequired,
+    initialValues: PropTypes.object,
     readOnly: PropTypes.bool,
   };
+
   static defaultProps = {
+    initialValues: {},
     readOnly: true,
   };
-  getDropdownInputConfig = () => [
-    {
-      value: true,
-      label: this.props.intl.formatMessage(messages.turnOn),
-    },
-    {
-      value: false,
-      label: this.props.intl.formatMessage(messages.turnOff),
-    },
-  ];
+
+  componentDidMount() {
+    this.props.initialize(this.props.initialValues);
+  }
 
   getCustomBlock = () => ({
     node: <p>{this.props.intl.formatMessage(messages.toggleNotificationsNote)}</p>,
