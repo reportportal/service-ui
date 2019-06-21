9c94a6acc53730826fbb4e17c15c19a6e118c826
ElenaRomanchuk <elena_romanchuk@epam.com>
2019-06-21 10:51:14 +0300

    EPMRPP-35516. Tooltip fo AA label.

--- app/src/pages/inside/stepPage/stepGrid/defectType/autoAnalyzedLabel/autoAnalyzedLabel.jsx
+++ app/src/pages/inside/stepPage/stepGrid/defectType/autoAnalyzedLabel/autoAnalyzedLabel.jsx
@@ -1,8 +1,24 @@
 import Parser from 'html-react-parser';
 import classNames from 'classnames/bind';
 import AAIcon from 'common/img/aa_activated-inline.svg';
+import { injectIntl, intlShape, defineMessages } from 'react-intl';
 import styles from './autoAnalyzedLabel.scss';
 
 const cx = classNames.bind(styles);
 
-export const AutoAnalyzedLabel = () => <div className={cx('aa-label')}>{Parser(AAIcon)}</div>;
+const messages = defineMessages({
+  investigatedByAA: {
+    id: 'AALabel.investigatedByAA',
+    defaultMessage: 'Investigated by Auto-Analysis',
+  },
+});
+
+export const AutoAnalyzedLabel = injectIntl(({ intl }) => (
+  <div className={cx('aa-label')} title={intl.formatMessage(messages.investigatedByAA)}>
+    {Parser(AAIcon)}
+  </div>
+));
+
+AutoAnalyzedLabel.propTypes = {
+  intl: intlShape.isRequired,
+};
