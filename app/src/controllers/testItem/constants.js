b3e65381864f3a1097d83b313d883597ffeabb26
AmsterGet <amsterget@gmail.com>
2019-07-16 13:22:51 +0300

    EPMRPP-39736 Add story/scenario test item types support

--- app/src/controllers/testItem/constants.js
+++ app/src/controllers/testItem/constants.js
@@ -1,4 +1,10 @@
 import { formatSortingString, SORTING_ASC } from 'controllers/sorting';
+import {
+  LEVEL_SUITE,
+  LEVEL_TEST,
+  LEVEL_STORY,
+  LEVEL_SCENARIO,
+} from 'common/constants/launchLevels';
 
 export const SET_LEVEL = 'setLevel';
 export const FETCH_TEST_ITEMS = 'fetchTestItems';
@@ -10,3 +16,10 @@ export const SET_PAGE_LOADING = `${NAMESPACE}/setPageLoading`;
 export const DEFAULT_SORTING = formatSortingString(['startTime'], SORTING_ASC);
 export const LIST_VIEW = 'LIST_VIEW';
 export const LOG_VIEW = 'LOG_VIEW';
+
+export const TEST_ITEM_TYPES_MAP = {
+  [LEVEL_SUITE]: LEVEL_SUITE,
+  [LEVEL_STORY]: LEVEL_SUITE,
+  [LEVEL_TEST]: LEVEL_TEST,
+  [LEVEL_SCENARIO]: LEVEL_TEST,
+};
