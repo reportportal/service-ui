5a9dbd2335eb143089ef27eba31c8dd01be9776a
AmsterGet <amsterget@gmail.com>
2019-07-24 20:29:03 +0300

    Hide product status widget & add error handler to bulk edit attributes action

--- app/src/components/main/modal/modalLayout/modalField/modalField.jsx
+++ app/src/components/main/modal/modalLayout/modalField/modalField.jsx
@@ -4,9 +4,9 @@ import styles from './modalField.scss';
 
 const cx = classname.bind(styles);
 
-export const ModalField = ({ className, label, children, tip, labelWidth }) => (
+export const ModalField = ({ className, label, children, tip, labelWidth, alignLeft }) => (
   <div className={cx('modal-field', className)}>
-    {label && <Label label={label} labelWidth={labelWidth} />}
+    {label && <Label label={label} labelWidth={labelWidth} alignLeft={alignLeft} />}
     <Content>
       {children}
       {tip && <Tip tip={tip} />}
@@ -19,6 +19,7 @@ ModalField.propTypes = {
   tip: PropTypes.string,
   children: PropTypes.node,
   labelWidth: PropTypes.number,
+  alignLeft: PropTypes.bool,
 };
 ModalField.defaultProps = {
   className: '',
@@ -26,6 +27,7 @@ ModalField.defaultProps = {
   tip: '',
   children: null,
   labelWidth: null,
+  alignLeft: false,
 };
 
 const Tip = ({ tip }) => <div className={cx('modal-field-tip')}>{tip}</div>;
@@ -36,9 +38,9 @@ Tip.defaultProps = {
   tip: '',
 };
 
-const Label = ({ label, labelWidth }) => (
+const Label = ({ label, labelWidth, alignLeft }) => (
   <div
-    className={cx('modal-field-label', { 'no-min-height': label === ' ' })}
+    className={cx('modal-field-label', { 'no-min-height': label === ' ', 'align-left': alignLeft })}
     style={{ width: labelWidth || 'unset' }}
   >
     {label}
@@ -47,10 +49,12 @@ const Label = ({ label, labelWidth }) => (
 Label.propTypes = {
   label: PropTypes.string,
   labelWidth: PropTypes.number,
+  alignLeft: PropTypes.bool,
 };
 Label.defaultProps = {
   label: '',
   labelWidth: null,
+  alignLeft: false,
 };
 
 const Content = ({ children }) => <div className={cx('modal-field-content')}>{children}</div>;
