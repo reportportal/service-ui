4d7a999fd5559f62d621728e36ac631a1379da40
DiscoElevator <DiscoElevator@users.noreply.github.com>
2019-08-22 17:59:32 +0300

    Just for test Codecov

--- app/src/controllers/lang/reducer.test.js
+++ app/src/controllers/lang/reducer.test.js
@@ -1,5 +1,5 @@
 import { langReducer } from './reducer';
-import { CHANGE_LANG_ACTION, INITIAL_STATE } from './constants';
+import { INITIAL_STATE } from './constants';
 
 describe('lang reducer', () => {
   test('should return initial state', () => {
@@ -9,14 +9,5 @@ describe('lang reducer', () => {
   test('should return old state on unknown action', () => {
     const oldState = { foo: 1 };
     expect(langReducer(oldState, { type: 'foo' })).toBe(oldState);
-  });
-
-  test('should handle CHANGE_LANG_ACTION', () => {
-    const payload = 'ru';
-    const newState = langReducer(undefined, {
-      type: CHANGE_LANG_ACTION,
-      payload,
-    });
-    expect(newState).toEqual(payload);
   });
 });
