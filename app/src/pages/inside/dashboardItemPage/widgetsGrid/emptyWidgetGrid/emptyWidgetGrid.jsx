8fc7e35c8591b7814818bf6cc3df4e623eb6f947
ElenaRomanchuk <elena_romanchuk@epam.com>
2019-08-28 11:29:09 +0300

    EPMRPP-40803. Dashboard. Incorrect view of empty shared dashboard for non-admin user.

--- app/src/pages/inside/dashboardItemPage/widgetsGrid/emptyWidgetGrid/emptyWidgetGrid.jsx
+++ app/src/pages/inside/dashboardItemPage/widgetsGrid/emptyWidgetGrid/emptyWidgetGrid.jsx
@@ -2,6 +2,9 @@ import React, { Fragment, Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import { injectIntl, defineMessages, intlShape } from 'react-intl';
+import { connect } from 'react-redux';
+import { userInfoSelector, activeProjectRoleSelector } from 'controllers/user';
+import { canAddWidget } from 'common/utils/permissions';
 import { GhostButton } from 'components/buttons/ghostButton';
 import AddDashboardIcon from 'common/img/add-widget-inline.svg';
 import styles from './emptyWidgetGrid.scss';
@@ -18,33 +21,46 @@ const messages = defineMessages({
   },
   notMyDashboardEmptyHeader: {
     id: 'DashboardItemPage.notMyDashboardEmptyHeader',
-    defaultMessage: 'You have no widgets on this dashboard',
+    defaultMessage: 'There are no widgets on this dashboard',
   },
 });
 
 @injectIntl
+@connect((state) => ({
+  userInfo: userInfoSelector(state),
+  projectRole: activeProjectRoleSelector(state),
+}))
 export class EmptyWidgetGrid extends Component {
   static propTypes = {
     intl: intlShape.isRequired,
     action: PropTypes.func,
     isDisable: PropTypes.bool,
+    dashboard: PropTypes.object,
+    userInfo: PropTypes.object,
+    projectRole: PropTypes.string,
   };
 
   static defaultProps = {
     action: () => {},
     isDisable: false,
+    dashboard: {},
+    userInfo: {},
+    projectRole: '',
   };
 
   render() {
-    const { action, intl, isDisable } = this.props;
+    const { action, intl, isDisable, dashboard, userInfo, projectRole } = this.props;
+    const isAddWidgetEnabled =
+      canAddWidget(userInfo.userRole, projectRole, dashboard.owner === userInfo.userId) &&
+      !isDisable;
 
     return (
       <div className={cx('empty-widget')}>
-        <div className={cx('empty-widget-img')} />
+        <div className={cx('empty-dashboard', { 'add-enabled': isAddWidgetEnabled })} />
         <p className={cx('empty-widget-headline')}>
           {intl.formatMessage(messages.notMyDashboardEmptyHeader)}
         </p>
-        {!isDisable && (
+        {isAddWidgetEnabled && (
           <Fragment>
             <p className={cx('empty-widget-text')}>
               {intl.formatMessage(messages.dashboardEmptyText)}
