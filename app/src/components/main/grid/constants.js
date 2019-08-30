fb5a12a764ba664e3ee854346d8f4c664211e612
ElenaRomanchuk <elena_romanchuk@epam.com>
2019-08-30 19:20:18 +0300

    EPMRPP-40006. Grid row highlighting

--- app/src/components/main/grid/constants.js
+++ app/src/components/main/grid/constants.js
@@ -1,4 +1,4 @@
 export const ALIGN_LEFT = 'left';
 export const ALIGN_CENTER = 'center';
 export const ALIGN_RIGHT = 'right';
-export const LOG_MESSAGE_HIGHLIGHT_TIMEOUT = 3000;
+export const LOG_MESSAGE_HIGHLIGHT_TIMEOUT = 100;
