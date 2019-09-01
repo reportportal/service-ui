3c0293ddd6e2ec079189a9049bab2fb33d7aefed
amsterget <amsterget@gmail.com>
2019-09-02 00:49:26 +0300

    EPMRPP-40810: Code review fixes - 1

--- app/src/components/fields/fieldErrorHint/fieldErrorHint.jsx
+++ app/src/components/fields/fieldErrorHint/fieldErrorHint.jsx
@@ -104,7 +104,7 @@ const messages = defineMessages({
   },
   descriptionEntityHint: {
     id: 'LaunchLevelEntities.descriptionEntityHint',
-    defaultMessage: 'Description should have size from 1 to 18',
+    defaultMessage: 'Description should have size from 3 to 18',
   },
   demoDataPostfixHint: {
     id: 'DemoDataTabForm.demoDataPostfixHint',
