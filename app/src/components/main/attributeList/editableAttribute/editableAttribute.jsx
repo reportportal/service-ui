09efd8e6cdaa9f49f572b8ce6e422d2310e82faf
DiscoElevator <DiscoElevator@users.noreply.github.com>
2019-09-16 14:39:10 +0300

    EPMRPP-41169: Disable attribute submit in case of duplicate. Remove redux-form from attribute editor.

--- app/src/components/main/attributeList/editableAttribute/editableAttribute.jsx
+++ app/src/components/main/attributeList/editableAttribute/editableAttribute.jsx
@@ -1,50 +1,40 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Attribute } from './attribute';
 import { AttributeEditor } from './attributeEditor';
 
-export class EditableAttribute extends Component {
-  static propTypes = {
-    attribute: PropTypes.object,
-    attributes: PropTypes.array,
-    editMode: PropTypes.bool,
-    disabled: PropTypes.bool,
-    onEdit: PropTypes.func,
-    onRemove: PropTypes.func,
-    onChange: PropTypes.func,
-    onCancelEdit: PropTypes.func,
-    keyURLCreator: PropTypes.func.isRequired,
-    valueURLCreator: PropTypes.func.isRequired,
-  };
+export const EditableAttribute = ({
+  attribute,
+  onChange,
+  onEdit,
+  onCancelEdit,
+  editMode,
+  ...rest
+}) =>
+  editMode ? (
+    <AttributeEditor {...rest} attribute={attribute} onConfirm={onChange} onCancel={onCancelEdit} />
+  ) : (
+    <Attribute {...rest} attribute={attribute} onClick={onEdit} />
+  );
 
-  static defaultProps = {
-    attribute: {},
-    attributes: [],
-    editMode: false,
-    disabled: false,
-    onEdit: () => {},
-    onRemove: () => {},
-    onChange: () => {},
-    onCancelEdit: () => {},
-  };
-
-  calculateFormName = (attribute) =>
-    attribute && attribute.key && attribute.value
-      ? `attributesEditor__${attribute.key}_${attribute.value}`.replace(/\W/g, '_')
-      : 'attributesEditor';
-
-  render() {
-    const { attribute, onChange, onEdit, onCancelEdit, editMode, ...rest } = this.props;
-    return editMode ? (
-      <AttributeEditor
-        {...rest}
-        form={this.calculateFormName(attribute)}
-        initialValues={attribute}
-        onConfirm={onChange}
-        onCancel={onCancelEdit}
-      />
-    ) : (
-      <Attribute {...this.props} onClick={onEdit} />
-    );
-  }
-}
+EditableAttribute.propTypes = {
+  attribute: PropTypes.object,
+  attributes: PropTypes.array,
+  editMode: PropTypes.bool,
+  disabled: PropTypes.bool,
+  onEdit: PropTypes.func,
+  onRemove: PropTypes.func,
+  onChange: PropTypes.func,
+  onCancelEdit: PropTypes.func,
+  keyURLCreator: PropTypes.func.isRequired,
+  valueURLCreator: PropTypes.func.isRequired,
+};
+EditableAttribute.defaultProps = {
+  attribute: {},
+  attributes: [],
+  editMode: false,
+  disabled: false,
+  onEdit: () => {},
+  onRemove: () => {},
+  onChange: () => {},
+  onCancelEdit: () => {},
+};
