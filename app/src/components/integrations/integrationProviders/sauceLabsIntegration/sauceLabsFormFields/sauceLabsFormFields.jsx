6d75547e1f4f9c2ab235125d78879fea124b7bf9
AmsterGet <amsterget@gmail.com>
2019-10-17 13:21:54 +0300

    EPMRPP-42490 Integrations. Sauce Labs. Add data center field

--- app/src/components/integrations/integrationProviders/sauceLabsIntegration/sauceLabsFormFields/sauceLabsFormFields.jsx
+++ app/src/components/integrations/integrationProviders/sauceLabsIntegration/sauceLabsFormFields/sauceLabsFormFields.jsx
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 import { injectIntl, defineMessages, intlShape } from 'react-intl';
 import { FieldErrorHint } from 'components/fields/fieldErrorHint';
 import { Input } from 'components/inputs/input';
+import { InputDropdown } from 'components/inputs/inputDropdown';
 import { IntegrationFormField } from 'components/integrations/elements';
 import { commonValidators } from 'common/utils';
+import { DATA_CENTER_US, DATA_CENTER_EU, DEFAULT_DATA_CENTER } from '../constants';
 
 const messages = defineMessages({
   userNameTitle: {
@@ -14,6 +16,18 @@ const messages = defineMessages({
   accessTokenTitle: {
     id: 'SauceLabsFormFields.accessTokenTitle',
     defaultMessage: 'Access token',
+  },
+  dataCenter: {
+    id: 'SauceLabsFormFields.dataCenter',
+    defaultMessage: 'Data center',
+  },
+  dataCenterUS: {
+    id: 'SauceLabsFormFields.dataCenterUS',
+    defaultMessage: 'United States (US)',
+  },
+  dataCenterEU: {
+    id: 'SauceLabsFormFields.dataCenterEU',
+    defaultMessage: 'Europe (EU)',
   },
 });
 
@@ -31,8 +45,21 @@ export class SauceLabsFormFields extends Component {
   static defaultProps = {
     disabled: false,
     lineAlign: false,
-    initialData: {},
+    initialData: {
+      dataCenter: DEFAULT_DATA_CENTER,
+    },
   };
+
+  constructor(props) {
+    super(props);
+    const {
+      intl: { formatMessage },
+    } = props;
+    this.dataCenterOptions = [
+      { value: DATA_CENTER_US, label: formatMessage(messages.dataCenterUS) },
+      { value: DATA_CENTER_EU, label: formatMessage(messages.dataCenterEU) },
+    ];
+  }
 
   componentDidMount() {
     this.props.initialize(this.props.initialData);
@@ -68,6 +95,14 @@ export class SauceLabsFormFields extends Component {
           <FieldErrorHint>
             <Input mobileDisabled />
           </FieldErrorHint>
+        </IntegrationFormField>
+        <IntegrationFormField
+          name="dataCenter"
+          disabled={disabled}
+          label={formatMessage(messages.dataCenter)}
+          lineAlign={lineAlign}
+        >
+          <InputDropdown options={this.dataCenterOptions} mobileDisabled />
         </IntegrationFormField>
       </Fragment>
     );
