00a9ab7e6b9fdeb9a83710f16c8ee19a568134c3
AmsterGet <amsterget@gmail.com>
2019-10-23 17:52:40 +0300

    EPMRPP-42828 C3.js widget creation guide

--- app/src/components/widgets/common/c3chart/chartContainer.jsx
+++ app/src/components/widgets/common/c3chart/chartContainer.jsx
@@ -27,7 +27,9 @@ export class ChartContainer extends Component {
     isPreview: PropTypes.bool,
     heightOffset: PropTypes.number,
     observer: PropTypes.object,
-    configData: PropTypes.object,
+    configData: PropTypes.shape({
+      getConfig: PropTypes.func,
+    }),
     legendConfig: PropTypes.shape({
       showLegend: PropTypes.bool,
       onChangeLegend: PropTypes.func,
@@ -47,7 +49,9 @@ export class ChartContainer extends Component {
       subscribe: () => {},
       unsubscribe: () => {},
     },
-    configData: {},
+    configData: {
+      getConfig: () => {},
+    },
     chartCreatedCallback: () => {},
     legendConfig: {
       showLegend: false,
