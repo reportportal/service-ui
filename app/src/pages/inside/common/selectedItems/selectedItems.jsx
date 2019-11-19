1279d317a43f6d313a31ff4d36a01087d54405de
AmsterGet <amsterget@gmail.com>
2019-11-19 17:10:09 +0300

    EPMRPP-43960 History page. Fix panels and tooltips

--- app/src/pages/inside/common/selectedItems/selectedItems.jsx
+++ app/src/pages/inside/common/selectedItems/selectedItems.jsx
@@ -27,34 +27,38 @@ const cx = classNames.bind(styles);
 
 export const SelectedItems = ({ selectedItems, errors, onClose, onUnselect }) => (
   <div className={cx('selected-items')}>
-    <div className={cx('header')}>
-      <FormattedMessage id="LaunchesPage.multipleSelectHeader" defaultMessage="Selected items:" />
-      <div className={cx('close-icon')} onClick={onClose}>
-        {Parser(CrossIcon)}
+    <div className={cx('content')}>
+      <span className={cx('header')}>
+        <FormattedMessage id="LaunchesPage.multipleSelectHeader" defaultMessage="Selected items:" />
+      </span>
+      <div>
+        <ScrollWrapper autoHeight autoHeightMax={120} hideTracksWhenNotNeeded>
+          <div className={cx('list')}>
+            {selectedItems.map((item) => (
+              <div className={cx('item')} key={item.id}>
+                <SelectedItem
+                  name={item.name}
+                  number={item.number}
+                  error={errors[item.id]}
+                  onUnselect={() => onUnselect(item)}
+                />
+              </div>
+            ))}
+          </div>
+        </ScrollWrapper>
+        {selectedItems.some((item) => !!errors[item.id]) && (
+          <span className={cx('error-message')}>
+            <FormattedMessage
+              id="LaunchesPage.multiSelectError"
+              defaultMessage="You cannot perform operation to invalid items"
+            />
+          </span>
+        )}
       </div>
     </div>
-    <ScrollWrapper autoHeight autoHeightMax={120} hideTracksWhenNotNeeded>
-      <div className={cx('list')}>
-        {selectedItems.map((item) => (
-          <div className={cx('item')} key={item.id}>
-            <SelectedItem
-              name={item.name}
-              number={item.number}
-              error={errors[item.id]}
-              onUnselect={() => onUnselect(item)}
-            />
-          </div>
-        ))}
-      </div>
-    </ScrollWrapper>
-    {selectedItems.some((item) => !!errors[item.id]) && (
-      <div className={cx('error-message')}>
-        <FormattedMessage
-          id="LaunchesPage.multiSelectError"
-          defaultMessage="You cannot perform operation to invalid items"
-        />
-      </div>
-    )}
+    <div className={cx('close-icon')} onClick={onClose}>
+      {Parser(CrossIcon)}
+    </div>
   </div>
 );
 SelectedItems.propTypes = {
