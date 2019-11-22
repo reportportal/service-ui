f2475e06d8081a9a59c60ed91653bad8b72fc6d7
DiscoElevator <2759038+DiscoElevator@users.noreply.github.com>
2019-11-22 13:23:12 +0300

    EPMRPP-38631: localization fix

--- app/webpack/dev.config.js
+++ app/webpack/dev.config.js
@@ -1,6 +1,22 @@
+/*
+ * Copyright 2019 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const path = require('path');
 const webpack = require('webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -40,7 +56,7 @@ module.exports = () => {
     plugins: [
       new webpack.HotModuleReplacementPlugin(),
       new CleanWebpackPlugin({
-        cleanOnceBeforeBuildPatterns: [path.resolve(__dirname, '../localization/messages')],
+        cleanOnceBeforeBuildPatterns: ['**/*', path.resolve(__dirname, '../localization/messages')],
       }),
     ],
     devServer: {
--- app/webpack/prod.config.js
+++ app/webpack/prod.config.js
@@ -1,5 +1,21 @@
+/*
+ * Copyright 2019 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 
@@ -46,10 +62,7 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: [
-        path.resolve(__dirname, '../localization/messages'),
-        path.resolve(__dirname, '../build/**/*'),
-      ],
+      cleanOnceBeforeBuildPatterns: ['**/*', path.resolve(__dirname, '../localization/messages')],
     }),
     new MiniCssExtractPlugin({
       filename: '[name].[hash:6].css',
