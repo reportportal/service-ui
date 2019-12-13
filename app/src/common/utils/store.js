1f7ad50f91e8ed4a2e2e6cb7e0a3d8952603acaa
AmsterGet <amsterget@gmail.com>
2019-12-13 20:23:03 +0300

    EPMRPP-44791 Clean redux store

---
+++ app/src/common/utils/store.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2019 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { LOGOUT } from 'controllers/auth/constants';
+
+export const createRootReducer = (appReducer) => (state, action) => {
+  let newState = state;
+  if (action.type === LOGOUT) {
+    const { appInfo, lang, initialDataReady, location } = state;
+    newState = { appInfo, lang, initialDataReady, location };
+  }
+  return appReducer(newState, action);
+};
