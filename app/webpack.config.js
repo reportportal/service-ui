e891fee0d743f416805b48ebf88387b509a8abdf
Vadzim Hushchanskou <HardNorth@users.noreply.github.com>
2020-04-30 13:43:32 +0300

    Add sealights
    Add sealights build config to webpack

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,11 +2,15 @@ const merge = require('webpack-merge');
 const baseConfig = require('./webpack/base.config');
 const prodConfig = require('./webpack/prod.config');
 const devConfig = require('./webpack/dev.config');
+const sealightsConfig = require('./webpack/sealights.config');
 
 module.exports = () => {
   const mode = process.env.NODE_ENV;
   if (mode === 'development') {
     return merge(baseConfig, devConfig());
+  }
+  if (mode === 'sealights') {
+    return sealightsConfig;
   }
   return merge(baseConfig, prodConfig);
 };
