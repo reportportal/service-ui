46d101459360a396d992ccf8a8f7347c91f12304
amsterget <amsterget@gmail.com>
2020-05-27 15:06:49 +0300

    EPMRPP-51995 Add selector for old history environment variable

--- app/src/controllers/appInfo/constants.js
+++ app/src/controllers/appInfo/constants.js
@@ -17,3 +17,4 @@
 export const APP_INFO_NAMESPACE = 'appInfo';
 export const ANALYICS_INSTANCE_KEY = 'server.details.instance';
 export const ANALYTICS_ALL_KEY = 'server.analytics.all';
+export const OLD_HISTORY_KEY = 'rp.environment.history.old';
