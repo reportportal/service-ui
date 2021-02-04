cd2933c19cb2d1ef2fb7562f8a44894ff3f42cc7
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2021-02-04 12:05:25 +0300

    EPMRPP-57550 Update view for log item info

--- app/src/pages/inside/logsPage/logItemInfo/logItemInfo.jsx
+++ app/src/pages/inside/logsPage/logItemInfo/logItemInfo.jsx
@@ -18,130 +18,40 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import track from 'react-tracking';
+import Parser from 'html-react-parser';
 import classNames from 'classnames/bind';
 import { defineMessages, injectIntl } from 'react-intl';
-import { GhostButton } from 'components/buttons/ghostButton';
-import { DefectType } from 'pages/inside/stepPage/stepGrid/defectType';
-import { getIssueTitle } from 'pages/inside/common/utils';
 import { LOG_PAGE_EVENTS } from 'components/main/analytics/events';
-import {
-  linkIssueAction,
-  unlinkIssueAction,
-  postIssueAction,
-  editDefectsAction,
-} from 'controllers/step';
-import { showModalAction } from 'controllers/modal';
 import {
   activeLogSelector,
   historyItemsSelector,
   activeRetryIdSelector,
   retriesSelector,
-  updateHistoryItemIssuesAction,
   RETRY_ID,
   NAMESPACE,
 } from 'controllers/log';
-import { getDefectTypeSelector } from 'controllers/project';
-import { TO_INVESTIGATE } from 'common/constants/defectTypes';
-import {
-  availableBtsIntegrationsSelector,
-  isPostIssueActionAvailable,
-  isBtsPluginsExistSelector,
-  enabledBtsPluginsSelector,
-} from 'controllers/plugins';
+import RetryIcon from 'common/img/retry-inline.svg';
 import { connectRouter } from 'common/utils';
-import LinkIcon from 'common/img/link-inline.svg';
-import DownLeftArrowIcon from 'common/img/down-left-arrow-inline.svg';
-import UpRightArrowIcon from 'common/img/up-right-arrow-inline.svg';
-import BugIcon from 'common/img/bug-inline.svg';
 import { LogItemInfoTabs } from './logItemInfoTabs';
 import { Retry } from './retry';
+import { DefectDetails } from './defectDetails';
 import styles from './logItemInfo.scss';
 
 const cx = classNames.bind(styles);
 
 const messages = defineMessages({
-  copyDefect: {
-    id: 'LogItemInfo.copyDefect',
-    defaultMessage: 'Copy defect {prefix} {message}',
-  },
-  sendDefect: {
-    id: 'LogItemInfo.sendDefect',
-    defaultMessage: 'Send defect to {message}',
-  },
-  fromPrefix: {
-    id: 'LogItemInfo.fromPrefix',
-    defaultMessage: 'from',
-  },
-  postIssue: {
-    id: 'LogItemInfo.postIssue',
-    defaultMessage: 'Post issue',
-  },
-  linkIssue: {
-    id: 'LogItemInfo.linkIssue',
-    defaultMessage: 'Link issue',
-  },
-  noDefectTypeToLinkIssue: {
-    id: 'LogItemInfo.noDefectTypeToLinkIssue',
-    defaultMessage: "You can't link issue if item has no defect type",
-  },
-  noDefectTypeToPostIssue: {
-    id: 'LogItemInfo.noDefectTypeToPostIssue',
-    defaultMessage: "You can't post issue if item has no defect type",
-  },
-  noDefectTypeToCopySendDefect: {
-    id: 'LogItemInfo.noDefectTypeToCopySendDefect',
-    defaultMessage: "You can't copy/send defect if item has no defect type",
-  },
   retries: {
     id: 'LogItemInfo.retries',
     defaultMessage: 'Retries',
   },
 });
-const POST_ISSUE_EVENTS_INFO = {
-  postBtn: LOG_PAGE_EVENTS.POST_ISSUE_MODAL_EVENTS.POST_BTN_POST_ISSUE_MODAL,
-  attachmentsSwitcher:
-    LOG_PAGE_EVENTS.POST_ISSUE_MODAL_EVENTS.ATTACHMENTS_SWITCHER_POST_ISSUE_MODAL,
-  logsSwitcher: LOG_PAGE_EVENTS.POST_ISSUE_MODAL_EVENTS.LOGS_SWITCHER_POST_ISSUE_MODAL,
-  commentSwitcher: LOG_PAGE_EVENTS.POST_ISSUE_MODAL_EVENTS.COMMENT_SWITCHER_POST_ISSUE_MODAL,
-  cancelBtn: LOG_PAGE_EVENTS.POST_ISSUE_MODAL_EVENTS.CANCEL_BTN_POST_ISSUE_MODAL,
-  closeIcon: LOG_PAGE_EVENTS.POST_ISSUE_MODAL_EVENTS.CLOSE_ICON_POST_ISSUE_MODAL,
-};
-const LINK_ISSUE_EVENTS_INFO = {
-  loadBtn: LOG_PAGE_EVENTS.LINK_ISSUE_MODAL_EVENTS.LOAD_BTN_LINK_ISSUE_MODAL,
-  cancelBtn: LOG_PAGE_EVENTS.LINK_ISSUE_MODAL_EVENTS.CANCEL_BTN_LINK_ISSUE_MODAL,
-  addNewIssue: LOG_PAGE_EVENTS.LINK_ISSUE_MODAL_EVENTS.ADD_NEW_ISSUE_BTN_LINK_ISSUE_MODAL,
-  closeIcon: LOG_PAGE_EVENTS.LINK_ISSUE_MODAL_EVENTS.CLOSE_ICON_LINK_ISSUE_MODAL,
-};
-const UNLINK_ISSUE_EVENTS_INFO = {
-  unlinkAutoAnalyzedFalse:
-    LOG_PAGE_EVENTS.UNLINK_ISSUE_MODAL_EVENTS.UNLINK_IN_UNLINK_ISSUE_MODAL_AUTO_ANALYZED_FALSE,
-  unlinkAutoAnalyzedTrue:
-    LOG_PAGE_EVENTS.UNLINK_ISSUE_MODAL_EVENTS.UNLINK_IN_UNLINK_ISSUE_MODAL_AUTO_ANALYZED_TRUE,
-  unlinkBtn: LOG_PAGE_EVENTS.UNLINK_ISSUE_MODAL_EVENTS.UNLINK_BTN_UNLINK_ISSUE_MODAL,
-  cancelBtn: LOG_PAGE_EVENTS.UNLINK_ISSUE_MODAL_EVENTS.CANCEL_BTN_UNLINK_ISSUE_MODAL,
-  closeIcon: LOG_PAGE_EVENTS.UNLINK_ISSUE_MODAL_EVENTS.CLOSE_ICON_UNLINK_ISSUE_MODAL,
-};
 
-@connect(
-  (state) => ({
-    logItem: activeLogSelector(state),
-    historyItems: historyItemsSelector(state),
-    btsIntegrations: availableBtsIntegrationsSelector(state),
-    retryItemId: activeRetryIdSelector(state),
-    retries: retriesSelector(state),
-    getDefectType: getDefectTypeSelector(state),
-    isBtsPluginsExist: isBtsPluginsExistSelector(state),
-    enabledBtsPlugins: enabledBtsPluginsSelector(state),
-  }),
-  {
-    linkIssueAction,
-    unlinkIssueAction,
-    postIssueAction,
-    editDefectsAction,
-    showModalAction,
-    updateHistoryItemIssues: updateHistoryItemIssuesAction,
-  },
-)
+@connect((state) => ({
+  logItem: activeLogSelector(state),
+  historyItems: historyItemsSelector(state),
+  retryItemId: activeRetryIdSelector(state),
+  retries: retriesSelector(state),
+}))
 @track()
 @connectRouter(
   () => {},
@@ -156,15 +66,8 @@ export class LogItemInfo extends Component {
     intl: PropTypes.object.isRequired,
     onChangePage: PropTypes.func.isRequired,
     onChangeLogLevel: PropTypes.func.isRequired,
-    editDefectsAction: PropTypes.func.isRequired,
-    linkIssueAction: PropTypes.func.isRequired,
-    unlinkIssueAction: PropTypes.func.isRequired,
-    postIssueAction: PropTypes.func.isRequired,
     historyItems: PropTypes.array.isRequired,
-    btsIntegrations: PropTypes.array.isRequired,
     fetchFunc: PropTypes.func.isRequired,
-    showModalAction: PropTypes.func.isRequired,
-    updateHistoryItemIssues: PropTypes.func.isRequired,
     onToggleSauceLabsIntegrationView: PropTypes.func.isRequired,
     isSauceLabsIntegrationView: PropTypes.bool.isRequired,
     debugMode: PropTypes.bool.isRequired,
@@ -177,111 +80,14 @@ export class LogItemInfo extends Component {
     updateRetryId: PropTypes.func,
     retryItemId: PropTypes.number,
     retries: PropTypes.arrayOf(PropTypes.object),
-    getDefectType: PropTypes.func,
-    isBtsPluginsExist: PropTypes.bool,
-    enabledBtsPlugins: PropTypes.array,
   };
   static defaultProps = {
     logItem: null,
     updateRetryId: () => {},
     retryItemId: null,
     retries: [],
-    getDefectType: () => {},
-    isBtsPluginsExist: false,
-    enabledBtsPlugins: [],
   };
 
-  getIssueActionTitle = (noIssueMessage, isPostIssueUnavailable) => {
-    const {
-      logItem,
-      intl: { formatMessage },
-      btsIntegrations,
-      isBtsPluginsExist,
-      enabledBtsPlugins,
-    } = this.props;
-
-    if (!logItem.issue) {
-      return formatMessage(noIssueMessage);
-    }
-
-    return getIssueTitle(
-      formatMessage,
-      btsIntegrations,
-      isBtsPluginsExist,
-      enabledBtsPlugins,
-      isPostIssueUnavailable,
-    );
-  };
-
-  getCopyDefectButtonText = () => {
-    const {
-      intl: { formatMessage },
-    } = this.props;
-
-    const lastItemWithDefect = this.getLastWithDefect();
-    return formatMessage(messages.copyDefect, {
-      prefix: lastItemWithDefect ? formatMessage(messages.fromPrefix) : '',
-      message: lastItemWithDefect ? `#${lastItemWithDefect.launchNumber}` : '',
-    });
-  };
-
-  getSendDefectButtonText = () => {
-    const lastHistoryItem = this.getLastHistoryItem();
-    return this.props.intl.formatMessage(messages.sendDefect, {
-      message: `#${lastHistoryItem.launchNumber}`,
-    });
-  };
-
-  getLastWithDefect = () => {
-    const reversedHistoryItems = [...this.props.historyItems];
-    reversedHistoryItems.pop();
-    reversedHistoryItems.reverse();
-
-    return reversedHistoryItems.find((item) => !!item.issue);
-  };
-
-  getLastHistoryItem = () => this.props.historyItems[this.props.historyItems.length - 1];
-
-  showCopyDefectModal = () => {
-    this.props.tracking.trackEvent(LOG_PAGE_EVENTS.COPY_DEFECT_FROM_BTN);
-    this.props.showModalAction({
-      id: 'copySendDefectModal',
-      data: {
-        lastHistoryItem: this.getLastHistoryItem(),
-        itemForCopy: this.getLastWithDefect(),
-        isCopy: true,
-        fetchFunc: this.props.fetchFunc,
-        eventsInfo: {
-          okBtn: LOG_PAGE_EVENTS.RECEIVE_BTN_RECEIVE_PREVIOUS_RESULT_MODAL,
-          cancelBtn: LOG_PAGE_EVENTS.CANCEL_BTN_RECEIVE_PREVIOUS_RESULT_MODAL,
-        },
-      },
-    });
-  };
-
-  showSendDefectModal = () => {
-    this.props.tracking.trackEvent(LOG_PAGE_EVENTS.SEND_DEFECT_TO_BTN);
-    this.props.showModalAction({
-      id: 'copySendDefectModal',
-      data: {
-        lastHistoryItem: this.getLastHistoryItem(),
-        itemForCopy: this.props.logItem,
-        isCopy: false,
-        fetchFunc: this.props.fetchFunc,
-        eventsInfo: {
-          okBtn: LOG_PAGE_EVENTS.SEND_BTN_SEND_DEFECT_MODAL,
-          cancelBtn: LOG_PAGE_EVENTS.CANCEL_BTN_SEND_DEFECT_MODAL,
-        },
-      },
-    });
-  };
-
-  checkIfTheLastItemIsActive = () => this.getLastHistoryItem().id === this.props.logItem.id;
-
-  isCopySendButtonDisabled = () => {
-    const lastHistoryItem = this.getLastHistoryItem();
-    return !this.props.logItem.issue || !lastHistoryItem.issue || !this.getLastWithDefect();
-  };
   hasRetries = () => {
     const { retries } = this.props;
     return retries.length > 1;
@@ -291,99 +97,6 @@ export class LogItemInfo extends Component {
     return logItem.issue && logItem.issue.issueType;
   };
   addExtraSpaceTop = () => this.isDefectTypeVisible() && this.hasRetries();
-
-  handleLinkIssue = () => {
-    this.props.tracking.trackEvent(LOG_PAGE_EVENTS.LINK_ISSUE_ACTION);
-    this.props.linkIssueAction([this.props.logItem], {
-      fetchFunc: this.props.fetchFunc,
-      eventsInfo: LINK_ISSUE_EVENTS_INFO,
-    });
-  };
-
-  handleUnlinkTicket = (ticketId) => {
-    const { logItem, fetchFunc, tracking } = this.props;
-    const items = [
-      {
-        ...logItem,
-        issue: {
-          ...logItem.issue,
-          externalSystemIssues: logItem.issue.externalSystemIssues.filter(
-            (issue) => issue.ticketId === ticketId,
-          ),
-        },
-      },
-    ];
-
-    tracking.trackEvent(LOG_PAGE_EVENTS.UNLINK_ISSUES_ACTION);
-
-    this.props.unlinkIssueAction(items, {
-      fetchFunc,
-      eventsInfo: UNLINK_ISSUE_EVENTS_INFO,
-    });
-  };
-
-  handlePostIssue = () => {
-    this.props.tracking.trackEvent(LOG_PAGE_EVENTS.POST_ISSUE_ACTION);
-    this.props.postIssueAction([this.props.logItem], {
-      fetchFunc: this.props.fetchFunc,
-      eventsInfo: POST_ISSUE_EVENTS_INFO,
-    });
-  };
-
-  onDefectEdited = (issues) => {
-    const { fetchFunc, updateHistoryItemIssues } = this.props;
-
-    if (issues) {
-      updateHistoryItemIssues(issues);
-    } else {
-      fetchFunc();
-    }
-  };
-
-  handleEditDefect = () => {
-    const { logItem } = this.props;
-    if (this.isDefectGroupOperationAvailable()) {
-      this.props.showModalAction({
-        id: 'editToInvestigateDefectModal',
-        data: {
-          item: logItem,
-          fetchFunc: this.onDefectEdited,
-          eventsInfo: {
-            changeSearchMode: LOG_PAGE_EVENTS.CHANGE_SEARCH_MODE_EDIT_DEFECT_MODAL,
-            selectAllSimilarItems: LOG_PAGE_EVENTS.SELECT_ALL_SIMILAR_ITEMS_EDIT_DEFECT_MODAL,
-            selectSpecificSimilarItem:
-              LOG_PAGE_EVENTS.SELECT_SPECIFIC_SIMILAR_ITEM_EDIT_DEFECT_MODAL,
-            editDefectsEvents: LOG_PAGE_EVENTS.EDIT_DEFECT_MODAL_EVENTS,
-            unlinkIssueEvents: UNLINK_ISSUE_EVENTS_INFO,
-            postIssueEvents: POST_ISSUE_EVENTS_INFO,
-            linkIssueEvents: LINK_ISSUE_EVENTS_INFO,
-          },
-        },
-      });
-    } else {
-      this.props.editDefectsAction([this.props.logItem], {
-        fetchFunc: this.onDefectEdited,
-        debugMode: this.props.debugMode,
-        eventsInfo: {
-          editDefectsEvents: LOG_PAGE_EVENTS.EDIT_DEFECT_MODAL_EVENTS,
-          unlinkIssueEvents: UNLINK_ISSUE_EVENTS_INFO,
-          postIssueEvents: POST_ISSUE_EVENTS_INFO,
-          linkIssueEvents: LINK_ISSUE_EVENTS_INFO,
-        },
-      });
-    }
-  };
-
-  isDefectGroupOperationAvailable = () => {
-    const { logItem } = this.props;
-    return (
-      logItem.issue &&
-      logItem.issue.issueType &&
-      this.props.getDefectType(logItem.issue.issueType).typeRef.toUpperCase() ===
-        TO_INVESTIGATE.toUpperCase() &&
-      !this.props.debugMode
-    );
-  };
 
   renderRetries = () => {
     const { retryItemId, retries } = this.props;
@@ -408,8 +121,8 @@ export class LogItemInfo extends Component {
   render() {
     const {
       logItem,
-      btsIntegrations,
       loading,
+      fetchFunc,
       onChangePage,
       onChangeLogLevel,
       onToggleSauceLabsIntegrationView,
@@ -417,92 +130,22 @@ export class LogItemInfo extends Component {
       debugMode,
       intl: { formatMessage },
     } = this.props;
-    const isPostIssueUnavailable = !isPostIssueActionAvailable(this.props.btsIntegrations);
-    const copySendDefectTitle =
-      logItem && !logItem.issue ? formatMessage(messages.noDefectTypeToCopySendDefect) : null;
 
     return (
       logItem && (
-        <div className={cx('container')}>
-          <div className={cx('content')}>
-            <div className={cx('description')}>
-              {this.isDefectTypeVisible() && (
-                <DefectType
-                  issue={logItem.issue}
-                  onEdit={this.handleEditDefect}
-                  onRemove={this.handleUnlinkTicket}
-                  editEventInfo={LOG_PAGE_EVENTS.DEFECT_TYPE_TAG}
-                  patternTemplates={logItem.patternTemplates}
-                />
-              )}
-            </div>
-
-            {!debugMode && (
-              <div className={cx('actions')}>
-                <div className={cx('action')}>
-                  {this.checkIfTheLastItemIsActive() ? (
-                    <GhostButton
-                      icon={DownLeftArrowIcon}
-                      disabled={this.isCopySendButtonDisabled()}
-                      onClick={this.showCopyDefectModal}
-                      title={copySendDefectTitle}
-                    >
-                      {this.getCopyDefectButtonText()}
-                    </GhostButton>
-                  ) : (
-                    <GhostButton
-                      icon={UpRightArrowIcon}
-                      disabled={this.isCopySendButtonDisabled()}
-                      onClick={this.showSendDefectModal}
-                      title={copySendDefectTitle}
-                    >
-                      {this.getSendDefectButtonText()}
-                    </GhostButton>
-                  )}
-                </div>
-                <div className={cx('action')}>
-                  <GhostButton
-                    icon={BugIcon}
-                    disabled={!logItem.issue || isPostIssueUnavailable}
-                    onClick={this.handlePostIssue}
-                    title={
-                      !logItem.issue || isPostIssueUnavailable
-                        ? this.getIssueActionTitle(
-                            messages.noDefectTypeToPostIssue,
-                            isPostIssueUnavailable,
-                          )
-                        : ''
-                    }
-                  >
-                    {formatMessage(messages.postIssue)}
-                  </GhostButton>
-                </div>
-                <div className={cx('action')}>
-                  <GhostButton
-                    icon={LinkIcon}
-                    disabled={!logItem.issue || !btsIntegrations.length}
-                    onClick={this.handleLinkIssue}
-                    title={
-                      !logItem.issue || !btsIntegrations.length
-                        ? this.getIssueActionTitle(
-                            messages.noDefectTypeToLinkIssue,
-                            isPostIssueUnavailable,
-                          )
-                        : ''
-                    }
-                  >
-                    {formatMessage(messages.linkIssue)}
-                  </GhostButton>
-                </div>
-              </div>
-            )}
+        <div className={cx('log-item-info')}>
+          <div className={cx('details')}>
+            <DefectDetails logItem={logItem} debugMode={debugMode} fetchFunc={fetchFunc} />
             {this.hasRetries() && (
               <div
                 className={cx('retries', {
                   'extra-space-top': this.addExtraSpaceTop(),
                 })}
               >
-                <div className={cx('retries-label')}>{formatMessage(messages.retries)}:</div>
+                <span className={cx('caption')}>
+                  <span className={cx('retry-icon')}>{Parser(RetryIcon)}</span>
+                  {formatMessage(messages.retries)}
+                </span>
                 {this.renderRetries()}
               </div>
             )}
