01e7a682af96785513bb7c3450502632ef34dfe0
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2021-02-12 16:22:24 +0300

    EPMRPP-59655 Move rule list item content to the separate component

--- app/src/pages/common/settingsPage/ruleList/listItem/itemContent/index.js
+++ app/src/pages/common/settingsPage/ruleList/listItem/itemContent/index.js
@@ -1,5 +1,5 @@
-/*!
- * Copyright 2019 EPAM Systems
+/*
+ * Copyright 2021 EPAM Systems
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,9 +14,4 @@
  * limitations under the License.
  */
 
-.data {
-  display: flex;
-  flex-direction: column;
-  margin: 8px 0;
-  padding-left: 90px;
-}
+export { ItemContent } from './itemContent';
