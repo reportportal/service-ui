cdc82c2dc9e36e7cc9afc8f8728c714da996c0fe
Katsiaryna Kleshchukevich <Katsiaryna_Kleshchukevich@epam.com>
2021-03-03 12:08:35 +0300

    EPMRPP-59882 Fixed comments

--- app/src/pages/inside/logsPage/historyLine/historyLineItem/historyLineItemTooltip/historyLineItemTooltip.jsx
+++ app/src/pages/inside/logsPage/historyLine/historyLineItem/historyLineItemTooltip/historyLineItemTooltip.jsx
@@ -15,6 +15,7 @@
  */
 
 import React from 'react';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { injectIntl, defineMessages } from 'react-intl';
 import Parser from 'html-react-parser';
@@ -25,6 +26,7 @@ import { IN_PROGRESS, NOT_FOUND } from 'common/constants/testStatuses';
 import BugIcon from 'common/img/bug-inline.svg';
 import CommentIcon from 'common/img/comment-inline.svg';
 import { DefectTypeItem } from 'pages/inside/common/defectTypeItem';
+import { includeAllLaunchesSelector } from 'controllers/log';
 import { Triangles } from '../triangles';
 import styles from './historyLineItemTooltip.scss';
 
@@ -58,60 +60,65 @@ const messages = defineMessages({
 });
 
 export const HistoryLineItemTooltip = injectIntl(
-  ({ intl: { formatMessage }, issue, pathNames, status, startTime, endTime, growthDuration }) => (
-    <div className={cx('history-line-item-tooltip')}>
-      <span className={cx('launch')}>
-        {formatMessage(messages.launch)} #{pathNames.launchPathName.number}:
-        <span className={cx('status', `status-${status.toLowerCase()}`)}>
-          {statusLocalization[status] ? formatMessage(statusLocalization[status]) : status}
+  ({ intl: { formatMessage }, issue, pathNames, status, startTime, endTime, growthDuration }) => {
+    const includeAllLaunches = useSelector(includeAllLaunchesSelector);
+    return (
+      <div className={cx('history-line-item-tooltip')}>
+        <span className={cx('launch')}>
+          {formatMessage(messages.launch)} #{pathNames.launchPathName.number}:
+          <span className={cx('status', `status-${status.toLowerCase()}`)}>
+            {statusLocalization[status] ? formatMessage(statusLocalization[status]) : status}
+          </span>
         </span>
-      </span>
-      <div className={cx('launch-name-block')}>
-        <span className={cx('title')}>{formatMessage(messages.lauchName)}:</span>
-        <span className={cx('launch-name-value')}>
-          {pathNames.launchPathName.name} #{pathNames.launchPathName.number}
-        </span>
-      </div>
-      {issue && issue.issueType && (
-        <div className={cx('defect-type-block')}>
-          <span className={cx('title')}>{formatMessage(messages.defectType)}</span>
-          <DefectTypeItem
-            type={issue.issueType}
-            noBorder
-            onClick={null}
-            className={cx('defect-item')}
-          />
-        </div>
-      )}
-      {status !== NOT_FOUND && status !== IN_PROGRESS && (
-        <div className={cx('duration-block')}>
-          <span className={cx('title')}>{formatMessage(messages.duration)}</span>
-          {getDuration(startTime, endTime, true)}
-          {growthDuration && (
-            <span className={cx('growth-duration')}>
-              <Triangles growthDuration={growthDuration} />
-              <span className={cx('value')}>+{growthDuration}%</span>
+        {includeAllLaunches && (
+          <div className={cx('launch-name-block')}>
+            <span className={cx('title')}>{formatMessage(messages.lauchName)}:</span>
+            <span className={cx('launch-name-value')}>
+              {pathNames.launchPathName.name} #{pathNames.launchPathName.number}
             </span>
-          )}
-        </div>
-      )}
-      {issue && issue.comment && (
-        <div className={cx('defect-asset', 'comment')}>
-          <span className={cx('icon')}>{Parser(CommentIcon)}</span>
-          {formatMessage(messages.comment)}
-        </div>
-      )}
-      {issue &&
-        issue.issueType &&
-        issue.externalSystemIssues &&
-        !!issue.externalSystemIssues.length && (
-          <div className={cx('defect-asset')}>
-            <span className={cx('icon')}>{Parser(BugIcon)}</span>
-            {formatMessage(messages.bts)}
           </div>
         )}
-    </div>
-  ),
+        {issue && issue.issueType && (
+          <div className={cx('defect-type-block')}>
+            <span className={cx('title')}>{formatMessage(messages.defectType)}</span>
+            <DefectTypeItem
+              type={issue.issueType}
+              noBorder
+              onClick={null}
+              className={cx('defect-item')}
+            />
+          </div>
+        )}
+        {status !== NOT_FOUND && status !== IN_PROGRESS && (
+          <div className={cx('duration-block')}>
+            <span className={cx('title')}>{formatMessage(messages.duration)}</span>
+            {getDuration(startTime, endTime, true)}
+            {growthDuration && (
+              <span className={cx('growth-duration')}>
+                <Triangles growthDuration={growthDuration} />
+                <span className={cx('value')}>+{growthDuration}%</span>
+              </span>
+            )}
+          </div>
+        )}
+        {issue && issue.comment && (
+          <div className={cx('defect-asset', 'comment')}>
+            <span className={cx('icon')}>{Parser(CommentIcon)}</span>
+            {formatMessage(messages.comment)}
+          </div>
+        )}
+        {issue &&
+          issue.issueType &&
+          issue.externalSystemIssues &&
+          !!issue.externalSystemIssues.length && (
+            <div className={cx('defect-asset')}>
+              <span className={cx('icon')}>{Parser(BugIcon)}</span>
+              {formatMessage(messages.bts)}
+            </div>
+          )}
+      </div>
+    );
+  },
 );
 HistoryLineItemTooltip.propTypes = {
   intl: PropTypes.object.isRequired,
