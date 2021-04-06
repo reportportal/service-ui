68187b9cd1b8ae5723a6c71759fa486626655c60
Katsiaryna Kleshchukevich <Katsiaryna_Kleshchukevich@epam.com>
2021-04-06 17:39:27 +0300

    EPMRPP-61003  Fixed comment

--- app/src/pages/inside/stepPage/modals/editDefectModals/makeDecisionModal/optionsStepForm/optionsStepForm.jsx
+++ app/src/pages/inside/stepPage/modals/editDefectModals/makeDecisionModal/optionsStepForm/optionsStepForm.jsx
@@ -37,16 +37,12 @@ export const OptionsStepForm = ({ info }) => {
 
   const renderCommentBlock = () => {
     return (
-      <>
-        {info.issue.comment && (
-          <div className={cx('comment-block')}>
-            <span className={cx('icon')}>{Parser(CommentIcon)}</span>
-            <ScrollWrapper autoHeight hideTracksWhenNotNeeded autoHeightMax={80}>
-              <p className={cx('comment')}>{info.issue.comment}</p>
-            </ScrollWrapper>
-          </div>
-        )}
-      </>
+      <div className={cx('comment-block')}>
+        <span className={cx('icon')}>{Parser(CommentIcon)}</span>
+        <ScrollWrapper autoHeight hideTracksWhenNotNeeded autoHeightMax={80}>
+          <p className={cx('comment')}>{info.issue.comment}</p>
+        </ScrollWrapper>
+      </div>
     );
   };
   return (
@@ -58,7 +54,7 @@ export const OptionsStepForm = ({ info }) => {
             <div className={cx('execution-item')}>
               <ExecutionInfo item={info} />
             </div>
-            {renderCommentBlock()}
+            {info.issue.comment && renderCommentBlock()}
           </div>
         ) : (
           <div className={cx('defect-type-content')}>
@@ -68,7 +64,7 @@ export const OptionsStepForm = ({ info }) => {
               defectType={defectType}
             />
             <div className={cx('defect-type-description')}>
-              {renderCommentBlock()}
+              {info.issue.comment && renderCommentBlock()}
               <div className={cx('analysis-block')}>
                 <span className={cx('analysis-icon')}>AA</span>
                 <p>
