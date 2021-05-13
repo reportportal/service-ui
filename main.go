71315aaeaa51d58943590a06a6d994cfae2c0a31
Ilya <ilya_hancharyk@epam.com>
2021-05-13 15:39:23 +0300

    Update CSP for stats.g.doubleclick

--- main.go
+++ main.go
@@ -71,7 +71,7 @@ func configureRouter(srv *server.RpServer, rpConf struct {
 			"media-src":      {"'self'", "*.saucelabs.com", "blob:"},
 			"img-src":        {"*", "'self'", "data:", "blob:"},
 			"object-src":     {"'self'"},
-			"connect-src":    {"'self'", "'unsafe-inline'", "www.google-analytics.com", "www.stats.g.doubleclick.net"},
+			"connect-src":    {"'self'", "'unsafe-inline'", "www.google-analytics.com", "stats.g.doubleclick.net"},
 		}
 		// apply content security policies
 		var STSSeconds int64 = 315360000
