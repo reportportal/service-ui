5d3188a6336f20a248d7c7cd6a90b184d20526e0
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2021-06-11 15:15:22 +0300

    EPMRPP-62694 Receive integration fields and settings from extensions

--- app/src/controllers/plugins/uiExtensions/constants.js
+++ app/src/controllers/plugins/uiExtensions/constants.js
@@ -4,6 +4,8 @@ export const EXTENSION_TYPE_ADMIN_PAGE = 'uiExtension:adminPage';
 export const EXTENSION_TYPE_MODAL = 'uiExtension:modal';
 export const EXTENSION_TYPE_SIDEBAR_COMPONENT = 'uiExtension:sidebarComponent';
 export const EXTENSION_TYPE_LAUNCH_ITEM_COMPONENT = 'uiExtension:launchItemComponent';
+export const EXTENSION_TYPE_INTEGRATION_FORM_FIELDS = 'uiExtension:integrationFormFields';
+export const EXTENSION_TYPE_INTEGRATION_SETTINGS = 'uiExtension:integrationSettings';
 
 export const COMMAND_GET_FILE = 'getFile';
 
