52dcf5aa4f530407fe35f84eafe3d7e5dbb2e86b
Aleksandr Zyabrev <aleksandr_zyabrev@epam.com>
2021-06-14 11:58:27 +0300

    EPMRPP-62781: code review fixes - 2

--- app/src/pages/inside/stepPage/modals/postIssueModal/postIssueModal.jsx
+++ app/src/pages/inside/stepPage/modals/postIssueModal/postIssueModal.jsx
@@ -489,7 +489,6 @@ export class PostIssueModal extends Component {
           disabled={false}
           transparentBorder
           transparentBackground
-          transparentLessWidth
           appearance="topaz"
         >
           {formatMessage(messages.cancel)}
