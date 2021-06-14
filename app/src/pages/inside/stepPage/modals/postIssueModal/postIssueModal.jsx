b5d5bb4e4123370d1689c16ec426584c9a724582
Alexander Zyabrev <40700843+oranje322@users.noreply.github.com>
2021-06-14 15:13:34 +0300

    EPMRPP-62888: change fonts and warning message (#2733)
    * EPMRPP-62888: change fonts and warning message
    
    * EPMRPP-62888: code review fixes - 1
    
    Co-authored-by: Aleksandr Zyabrev <aleksandr_zyabrev@epam.com>

--- app/src/pages/inside/stepPage/modals/postIssueModal/postIssueModal.jsx
+++ app/src/pages/inside/stepPage/modals/postIssueModal/postIssueModal.jsx
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { reduxForm } from 'redux-form';
@@ -44,6 +44,8 @@ import { BtsIntegrationSelector } from 'pages/inside/common/btsIntegrationSelect
 import { DarkModalLayout } from 'components/main/modal/darkModalLayout';
 import { GhostButton } from 'components/buttons/ghostButton';
 import { hideModalAction } from 'controllers/modal';
+import ErrorInlineIcon from 'common/img/error-inline.svg';
+import Parser from 'html-react-parser';
 import { ItemsList } from '../makeDecisionModal/optionsSection/itemsList';
 import { ERROR_LOGS_SIZE } from '../makeDecisionModal/constants';
 import { JiraCredentials } from './jiraCredentials';
@@ -568,11 +570,12 @@ export class PostIssueModal extends Component {
               />
             ) : (
               <div className={cx('no-default-properties-message')}>
-                {formatMessage(messages.noDefaultPropertiesMessage)}
+                <div className={cx('icon')}>{Parser(ErrorInlineIcon)}</div>
+                <span>{formatMessage(messages.noDefaultPropertiesMessage)}</span>
               </div>
             )}
             {!this.isBulkOperation && (
-              <Fragment>
+              <div className={cx('include-block-wrapper')}>
                 <h4 className={cx('form-block-header', 'dark-view')}>
                   <span className={cx('header-text', 'dark-view')}>
                     {formatMessage(messages.includeDataHeader)}
@@ -592,7 +595,7 @@ export class PostIssueModal extends Component {
                     </FieldProvider>
                   ))}
                 </div>
-              </Fragment>
+              </div>
             )}
             <div className={cx('credentials-block-wrapper', { expanded })}>
               <h4 className={cx('form-block-header', 'dark-view')}>
