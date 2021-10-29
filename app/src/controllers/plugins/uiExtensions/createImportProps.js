84cf7b79917a99183d08e77c38fa32f412014d1c
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2021-10-29 11:41:18 +0300

    EPMRPP-68468 Extend import props

--- app/src/controllers/plugins/uiExtensions/createImportProps.js
+++ app/src/controllers/plugins/uiExtensions/createImportProps.js
@@ -147,6 +147,7 @@ import {
 } from 'components/integrations/elements';
 import { updateLaunchLocallyAction } from 'controllers/launch';
 import { getDefectTypeLabel } from 'components/main/analytics/events/common/utils';
+import { formatAttribute } from 'common/utils/attributeUtils';
 import { createGlobalNamedIntegrationsSelector } from '../selectors';
 
 const BUTTONS = {
@@ -293,6 +294,7 @@ export const createImportProps = (pluginName) => ({
     getDefectTypeLocators,
     getDefectTypeLabel,
     getDefectFormFields,
+    formatAttribute,
   },
   validators: {
     attributesArray,
