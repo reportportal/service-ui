97b3851635a8ceaef40b26e857d10ae11a383a8b
Mikhail Sidarkevich <mikhail_sidarkevich@epam.com>
2022-01-24 15:15:07 +0300

    EPMRPP-74073 || code review fixes - 1

--- app/src/controllers/uniqueErrors/sagas.js
+++ app/src/controllers/uniqueErrors/sagas.js
@@ -33,8 +33,12 @@ import {
 import { PAGE_KEY, SIZE_KEY } from 'controllers/pagination';
 import { SORTING_KEY } from 'controllers/sorting';
 import { unselectAllItemsAction } from 'controllers/groupOperations';
-import { FETCH_GLOBAL_INTEGRATIONS_SUCCESS } from 'controllers/plugins/constants';
+import {
+  FETCH_GLOBAL_INTEGRATIONS_SUCCESS,
+  NAMESPACE as PLUGINS_NAMESPACE,
+} from 'controllers/plugins/constants';
 import { globalIntegrationsSelector, pluginsSelector } from 'controllers/plugins';
+
 import { COMMAND_GET_CLUSTERS } from 'controllers/plugins/uiExtensions/constants';
 import {
   CLEAR_CLUSTER_ITEMS,
@@ -45,6 +49,8 @@ import { FETCH_CLUSTERS, NAMESPACE, RELOAD_CLUSTERS } from './constants';
 import { setPageLoadingAction } from './actionCreators';
 
 function* getIntegration() {
+  // TODO: In the future plugins with js parts should not depend on integrations, only on plugins.
+  // TODO: This should be removed when common getFile plugin command will be presented in all plugins with js files.
   let integrations = yield select(globalIntegrationsSelector);
   if (!integrations.length) {
     const response = yield take(FETCH_GLOBAL_INTEGRATIONS_SUCCESS);
@@ -59,7 +65,12 @@ function* getIntegration() {
       item.integrationType.details.metadata.supportedFeatures &&
       item.integrationType.details.metadata.supportedFeatures.includes('uniqueErrorsClusters'),
   );
-  const plugins = yield select(pluginsSelector);
+
+  let plugins = yield select(pluginsSelector);
+  if (!plugins.length) {
+    const response = yield take(createFetchPredicate(PLUGINS_NAMESPACE));
+    plugins = response.payload;
+  }
   const plugin = plugins.find((item) => item.name === supportedIntegration.name);
 
   return plugin.enabled && supportedIntegration;
