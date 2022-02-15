c7871e106c5d6f5ee23cbf09a557ef93dd1d98a0
Vadzim Hvazdovich <vadzim_hvazdovich@epam.com>
2022-02-15 17:54:17 +0300

    EPMRPP-46935 || Launches page. Tooltip for launches with long name goes out of borders

--- app/src/components/main/tooltips/tooltip/withTooltip.jsx
+++ app/src/components/main/tooltips/tooltip/withTooltip.jsx
@@ -50,6 +50,8 @@ export const withTooltip = ({ TooltipComponent, data = {} }) => (WrappedComponen
       const styleWidth = data.dynamicWidth ? null : { width: data.width || DEFAULT_TOOLTIP_WIDTH };
       const topOffset = data.topOffset || 0;
       const leftOffset = data.leftOffset || 0;
+      const clientWidth = document.documentElement.clientWidth;
+      const maxWidth = clientWidth - 100;
       return (
         <Manager>
           <Reference>
@@ -86,7 +88,13 @@ export const withTooltip = ({ TooltipComponent, data = {} }) => (WrappedComponen
                     onMouseEnter={data.hoverable ? this.showTooltip : null}
                     onMouseLeave={data.hoverable ? this.hideTooltip : null}
                   >
-                    <div className={cx('tooltip-content')}>
+                    <div
+                      className={cx('tooltip-content')}
+                      style={{
+                        maxWidth: `${maxWidth}px`,
+                        width: 'fit-content',
+                      }}
+                    >
                       <TooltipComponent {...this.props} />
                       {!data.noArrow && (
                         <div
