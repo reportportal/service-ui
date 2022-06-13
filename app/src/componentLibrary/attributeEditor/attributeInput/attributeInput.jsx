9a16455353f89ad74e9ab46a3e629c3fa8b7b617
Aleksandr Zyabrev <40700843+oranje322@users.noreply.github.com>
2022-06-13 10:03:18 +0300

    EPMRPP-76654 || attribute editor & autocomplete component (#3127)
    * EPMRPP-76654 || attribute editor & autocomplete component
    
    * EPMRPP-76654 || code review fixes - 1
    
    Co-authored-by: Aleksandr Zyabrev <aleksandr_zyabrev@epam.com>

---
+++ app/src/componentLibrary/attributeEditor/attributeInput/attributeInput.jsx
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { AsyncAutocomplete } from 'componentLibrary/autocompletes/asyncAutocomplete';
+
+export class AttributeInput extends Component {
+  static propTypes = {
+    attributes: PropTypes.array,
+    attributeKey: PropTypes.string,
+    attributeValue: PropTypes.string,
+    attributeComparator: PropTypes.func,
+  };
+
+  static defaultProps = {
+    attributes: [],
+    attributeKey: null,
+    attributeValue: null,
+    attributeComparator: () => {},
+  };
+
+  filterOption = (item) => {
+    const { attributes, attributeKey, attributeValue, attributeComparator } = this.props;
+    return !attributes.find((attribute) =>
+      attributeComparator(attribute, item, attributeKey, attributeValue),
+    );
+  };
+
+  render() {
+    const { attributes, attributeKey, attributeValue, attributeComparator, ...rest } = this.props;
+    return <AsyncAutocomplete {...rest} filterOption={this.filterOption} />;
+  }
+}
---
+++ app/src/componentLibrary/autocompletes/singleAutocomplete/singleAutocomplete.jsx
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2022 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { Component } from 'react';
+import Downshift from 'downshift';
+import PropTypes from 'prop-types';
+import { Manager, Reference, Popper } from 'react-popper';
+import { FieldText } from 'componentLibrary/fieldText';
+import { AutocompleteMenu } from './../common/autocompleteMenu';
+
+export class SingleAutocomplete extends Component {
+  static propTypes = {
+    options: PropTypes.array,
+    loading: PropTypes.bool,
+    onStateChange: PropTypes.func,
+    value: PropTypes.any,
+    placeholder: PropTypes.string,
+    onChange: PropTypes.func,
+    onFocus: PropTypes.func,
+    onBlur: PropTypes.func,
+    disabled: PropTypes.bool,
+    inputProps: PropTypes.object,
+    parseValueToString: PropTypes.func,
+    renderOption: PropTypes.func,
+    minLength: PropTypes.number,
+    maxLength: PropTypes.number,
+    async: PropTypes.bool,
+    autocompleteVariant: PropTypes.string,
+  };
+
+  static defaultProps = {
+    options: [],
+    loading: false,
+    value: '',
+    placeholder: '',
+    onChange: () => {},
+    onFocus: () => {},
+    onBlur: () => {},
+    disabled: false,
+    inputProps: {},
+    parseValueToString: (value) => value || '',
+    renderOption: null,
+    minLength: 1,
+    maxLength: null,
+    async: false,
+    autocompleteVariant: '',
+  };
+
+  getOptionProps = (getItemProps, highlightedIndex, selectedItem) => ({ item, index, ...rest }) =>
+    getItemProps({
+      item,
+      index,
+      isActive: highlightedIndex === index,
+      isSelected: selectedItem === item,
+      ...rest,
+    });
+
+  render() {
+    const {
+      onStateChange,
+      onChange,
+      onBlur,
+      onFocus,
+      parseValueToString,
+      placeholder,
+      disabled,
+      value,
+      inputProps,
+      maxLength,
+      autocompleteVariant,
+      ...props
+    } = this.props;
+    return (
+      <Manager>
+        <Downshift
+          onChange={onChange}
+          itemToString={parseValueToString}
+          selectedItem={value}
+          onStateChange={onStateChange}
+        >
+          {({ getInputProps, getItemProps, isOpen, inputValue, highlightedIndex, openMenu }) => (
+            <div>
+              <Reference>
+                {({ ref }) => (
+                  <div ref={ref}>
+                    <FieldText
+                      {...getInputProps({
+                        placeholder: !disabled ? placeholder : '',
+                        maxLength,
+                        onFocus: () => {
+                          !value && openMenu();
+                          onFocus();
+                        },
+                        onBlur,
+                        disabled,
+                        defaultWidth: false,
+                        ...inputProps,
+                      })}
+                    />
+                  </div>
+                )}
+              </Reference>
+              <Popper
+                placement="bottom-start"
+                modifiers={{
+                  preventOverflow: { enabled: true },
+                  flip: {
+                    enabled: true,
+                  },
+                }}
+              >
+                {({ placement, ref, style }) => (
+                  <AutocompleteMenu
+                    isOpen={isOpen}
+                    placement={placement}
+                    style={style}
+                    ref={ref}
+                    inputValue={(inputValue || '').trim()}
+                    getItemProps={this.getOptionProps(getItemProps, highlightedIndex, value)}
+                    parseValueToString={parseValueToString}
+                    autocompleteVariant={autocompleteVariant}
+                    {...props}
+                  />
+                )}
+              </Popper>
+            </div>
+          )}
+        </Downshift>
+      </Manager>
+    );
+  }
+}
