88a3dcdbda7441d6d074cee0fcfd78c427f4de03
Ruslan Lyubimov <ruslan_lyubimov@epam.com>
2022-07-08 12:34:46 +0500

    EPMRPP-77854 || Code review fixes - 1

--- app/src/componentLibrary/systemMessage/systemMessage.jsx
+++ app/src/componentLibrary/systemMessage/systemMessage.jsx
@@ -1,46 +1,52 @@
+/*
+ * Copyright 2022 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-import informationImg from './img/information.png';
-import systemErrorImg from './img/SytemError.png';
-import warningImg from './img/warning.png';
 import styles from './systemMessage.scss';
 
 const cx = classNames.bind(styles);
 const MODE_INFORMATION = 'info';
 const MODE_WARNING = 'warning';
 const MODE_SYS_ERROR = 'error';
-let img = null;
-export const SystemMessage = ({ message, textSolution, children, mode }) => {
-  if (mode === MODE_INFORMATION) {
-    img = informationImg;
-  } else if (mode === MODE_WARNING) {
-    img = warningImg;
-  } else {
-    img = systemErrorImg;
-  }
 
+export const SystemMessage = ({ header, caption, children, mode, contentByChildWidth }) => {
   return (
-    <div className={cx('system-message-container')}>
-      <img src={img} alt={message} />
+    <div className={cx('system-message', { contentByChildWidth })}>
+      <div className={cx(`stripes-${mode}`)} />
       <div className={cx('text-container')}>
-        <h1 className={cx(`message-header-${mode}`)}>{message}</h1>
+        {header && <h1 className={cx(`message-header-${mode}`)}>{header}</h1>}
         <h2>{children}</h2>
-        <p>{textSolution}</p>
+        {caption && <p>{caption}</p>}
       </div>
     </div>
   );
 };
 
 SystemMessage.propTypes = {
-  message: PropTypes.string,
-  textSolution: PropTypes.string,
-  children: PropTypes.string,
+  header: PropTypes.string,
+  caption: PropTypes.string,
+  children: PropTypes.node,
   mode: PropTypes.oneOf([MODE_INFORMATION, MODE_WARNING, MODE_SYS_ERROR]),
+  contentByChildWidth: PropTypes.bool,
 };
 
 SystemMessage.defaultProps = {
-  message: '',
-  textSolution: '',
+  header: '',
+  caption: '',
   children: '',
   mode: MODE_INFORMATION,
+  contentByChildWidth: false,
 };
