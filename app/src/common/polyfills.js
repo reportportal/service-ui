b5cad5bb71b4e90d8dad1ea7ffa3716a034fd26a
Dzmitry Kosarau <Dzmitry_Kosarau@epam.com>
2022-08-01 16:41:39 +0500

    EPMRPP-78541 || code review fixes - 2

--- app/src/common/polyfills.js
+++ app/src/common/polyfills.js
@@ -14,19 +14,7 @@
  * limitations under the License.
  */
 
-/* eslint-disable */
-
 import areIntlLocalesSupported from 'intl-locales-supported';
-
-// NodeList.prototype.forEach (https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach#Polyfill)
-if (window.NodeList && !NodeList.prototype.forEach) {
-  NodeList.prototype.forEach = function(callback, thisArg) {
-    thisArg = thisArg || window;
-    for (let i = 0; i < this.length; i++) {
-      callback.call(thisArg, this[i], i, this);
-    }
-  };
-}
 
 // Chrome Intl doesn't support 'be' locale, so we have to manually apply polyfill in this case
 export const polyfillLocales = () =>
