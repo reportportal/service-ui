071c98ce161697b91e158d2865010cc6a17be6f4
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2022-08-18 19:52:23 +0300

    Use undefined instead of null

--- app/src/common/hooks/useOnClickOutside.js
+++ app/src/common/hooks/useOnClickOutside.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 EPAM Systems
+ * Copyright 2022 EPAM Systems
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ import { useEffect } from 'react';
 export const useOnClickOutside = (ref, handler) => {
   useEffect(() => {
     if (!handler) {
-      return null;
+      return undefined;
     }
 
     const listener = (event) => {
