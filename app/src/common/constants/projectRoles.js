e5093489b9ba2e9c0a87468343e67c40b3515875
Dzmitry Kosarau <Dzmitry_Kosarau@epam.com>
2022-09-19 11:37:18 +0500

    EPMRPP-77769 || Refactoring roles
    Delete CUSTOMER and OPERATOR from roles.

--- app/src/common/constants/projectRoles.js
+++ app/src/common/constants/projectRoles.js
@@ -18,11 +18,9 @@ export const PROJECT_MANAGER = 'PROJECT_MANAGER';
 export const MEMBER = 'MEMBER';
 export const CUSTOMER = 'CUSTOMER';
 export const OPERATOR = 'OPERATOR';
-export const PROJECT_ROLES = [CUSTOMER, OPERATOR, MEMBER, PROJECT_MANAGER];
+export const PROJECT_ROLES = [MEMBER, PROJECT_MANAGER];
 export const DEFAULT_PROJECT_ROLE = MEMBER;
 export const ROLES_MAP = [
-  { value: CUSTOMER, label: CUSTOMER },
-  { value: OPERATOR, label: OPERATOR },
   { value: MEMBER, label: MEMBER },
   { value: PROJECT_MANAGER, label: PROJECT_MANAGER.split('_').join(' ') },
 ];
