a8ed2c6d79eff7c17c29effff22ff1eecca7b405
Vadzim Hvazdovich <vadzim_hvazdovich@epam.com>
2023-05-29 11:53:59 +0400

    EPMRPP-83221 || code review fixes - 1

--- app/src/layouts/settingsLayout/settingsLayout.jsx
+++ app/src/layouts/settingsLayout/settingsLayout.jsx
@@ -15,130 +15,30 @@
  */
 
 import React from 'react';
-import { defineMessages, useIntl } from 'react-intl';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-import { connect } from 'react-redux';
-import { referenceDictionary } from 'common/utils';
-import { uiBuildVersionSelector } from 'controllers/appInfo';
-import { instanceTypeSelector } from 'controllers/appInfo/selectors';
-import { EPAM, SAAS } from 'controllers/appInfo/constants';
+import { Footer } from 'layouts/settingsLayout/footer';
 import styles from './settingsLayout.scss';
 
 const cx = classNames.bind(styles);
-const messages = defineMessages({
-  github: {
-    id: 'SettingsLayout.github',
-    defaultMessage: 'Fork us on GitHub',
-  },
-  slack: {
-    id: 'SettingsLayout.slack',
-    defaultMessage: 'Chat with us on Slack',
-  },
-  contactUs: {
-    id: 'SettingsLayout.contactUs',
-    defaultMessage: 'Contact us',
-  },
-  epam: {
-    id: 'SettingsLayout.epam',
-    defaultMessage: 'EPAM',
-  },
-  documentation: {
-    id: 'SettingsLayout.documentation',
-    defaultMessage: 'Documentation',
-  },
-  privacyPolicy: {
-    id: 'SettingsLayout.privacyPolicy',
-    defaultMessage: 'Privacy Policy',
-  },
-  rights: {
-    id: 'SettingsLayout.rights',
-    defaultMessage: '© Report Portal 2023 All rights reserved',
-  },
-  build: {
-    id: 'SettingsLayout.build',
-    defaultMessage: 'Build: {buildVersion}',
-  },
-});
 
-const SettingsLayoutBase = ({ navigation, children, buildVersion, instanceType }) => {
-  const { formatMessage } = useIntl();
-
-  const footerLeftItems = [
-    formatMessage(messages.build, {
-      buildVersion,
-    }),
-    formatMessage(messages.rights),
-  ];
-
-  const footerRightItems = [
-    <a href={referenceDictionary.rpGitHub} target="_blank" className={cx('link')}>
-      <span>{formatMessage(messages.github)}</span>
-    </a>,
-    <a href={referenceDictionary.rpSlack} target="_blank" className={cx('link')}>
-      <span>{formatMessage(messages.slack)}</span>
-    </a>,
-    <a href={referenceDictionary.rpEmail} target="_blank" className={cx('link')}>
-      <span>{formatMessage(messages.contactUs)}</span>
-    </a>,
-    <a href={referenceDictionary.rpEpam} target="_blank" className={cx('link')}>
-      <span>{formatMessage(messages.epam)}</span>
-    </a>,
-    <a href={referenceDictionary.rpDoc} target="_blank" className={cx('link')}>
-      <span>{formatMessage(messages.documentation)}</span>
-    </a>,
-    (instanceType === EPAM || instanceType === SAAS) && (
-      <a href={referenceDictionary.rpEpamPolicy} target="_blank" className={cx('link')}>
-        <span>{formatMessage(messages.privacyPolicy)}</span>
-      </a>
-    ),
-  ];
-
-  return (
-    <div className={cx('container')}>
-      {navigation && <div className={cx('navigation')}>{navigation}</div>}
-      <div className={cx('section')}>
-        <div className={cx('section-content')}>{children}</div>
-        <div className={cx('footer')}>
-          <div className={cx('footer-left-items')}>
-            {footerLeftItems.map((item, i) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <div key={`footer-left-item-${i}`} className={cx('footer-item')}>
-                {item}
-              </div>
-            ))}
-          </div>
-          <div className={cx('footer-right-items')}>
-            {footerRightItems.map((item, i) => (
-              <>
-                {item && (
-                  // eslint-disable-next-line react/no-array-index-key
-                  <div key={`footer-right-item-${i}`} className={cx('footer-item')}>
-                    {item}
-                  </div>
-                )}
-              </>
-            ))}
-          </div>
-        </div>
-      </div>
+export const SettingsLayout = ({ navigation, children }) => (
+  <div className={cx('container')}>
+    {navigation && <div className={cx('navigation')}>{navigation}</div>}
+    <div className={cx('section')}>
+      <div className={cx('section-content')}>{children}</div>
+      <Footer />
     </div>
-  );
-};
+  </div>
+);
 
-SettingsLayoutBase.propTypes = {
+SettingsLayout.propTypes = {
   navigation: PropTypes.node,
   header: PropTypes.node,
   children: PropTypes.node,
-  buildVersion: PropTypes.string.isRequired,
-  instanceType: PropTypes.string.isRequired,
 };
-SettingsLayoutBase.defaultProps = {
+SettingsLayout.defaultProps = {
   navigation: null,
   header: null,
   children: null,
 };
-export const SettingsLayout = connect((state) => ({
-  buildVersion: uiBuildVersionSelector(state),
-  instanceType: instanceTypeSelector(state),
-}))(SettingsLayoutBase);
