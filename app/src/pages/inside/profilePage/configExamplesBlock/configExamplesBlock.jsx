8361f82d12c0e46bce181526e74578bcedb0588a
Vadzim Hvazdovich <vadzim_hvazdovich@epam.com>
2023-06-20 12:58:03 +0400

    EPMRPP-79853 || Update configuration tabs and examples in user profile

--- app/src/pages/inside/profilePage/configExamplesBlock/configExamplesBlock.jsx
+++ app/src/pages/inside/profilePage/configExamplesBlock/configExamplesBlock.jsx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 EPAM Systems
+ * Copyright 2023 EPAM Systems
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { ContainerWithTabs } from 'components/main/containerWithTabs';
-import { activeProjectSelector, userIdSelector } from 'controllers/user';
+import { activeProjectSelector } from 'controllers/user';
 import { PROFILE_PAGE_EVENTS } from 'components/main/analytics/events';
 import styles from './configExamplesBlock.scss';
 import { BlockContainerBody } from '../blockContainer';
@@ -26,22 +26,19 @@ import { BlockContainerBody } from '../blockContainer';
 import { TabsConfig } from './tabsConfig';
 
 const cx = classNames.bind(styles);
-const API_KEY = '<YOUR_API_KEY>';
+const API_KEY = '<API_KEY>';
 @connect((state) => ({
   activeProject: activeProjectSelector(state),
-  login: userIdSelector(state),
 }))
 export class ConfigExamplesBlock extends Component {
   static propTypes = {
-    login: PropTypes.string,
     activeProject: PropTypes.string,
   };
   static defaultProps = {
-    login: '',
     activeProject: '',
   };
   render() {
-    const { activeProject, login } = this.props;
+    const { activeProject } = this.props;
     return (
       <div className={cx('config-example-block')}>
         <BlockContainerBody>
@@ -49,11 +46,11 @@ export class ConfigExamplesBlock extends Component {
             <ContainerWithTabs
               selectTabEventInfo={PROFILE_PAGE_EVENTS.SELECT_CONFIGURATION_TAB}
               data={[
-                TabsConfig.javaConfig(API_KEY, activeProject, login),
-                TabsConfig.rubyConfig(API_KEY, activeProject, login),
-                TabsConfig.soapUiConfig(API_KEY, activeProject, login),
-                TabsConfig.dotNetConfig,
-                TabsConfig.nodejsConfig(API_KEY, activeProject, login),
+                TabsConfig.javaConfig(API_KEY, activeProject),
+                TabsConfig.rubyConfig(API_KEY, activeProject),
+                TabsConfig.pythonConfig(API_KEY, activeProject),
+                TabsConfig.dotNetConfig(API_KEY, activeProject),
+                TabsConfig.nodejsConfig(API_KEY, activeProject),
               ]}
             />
           </div>
