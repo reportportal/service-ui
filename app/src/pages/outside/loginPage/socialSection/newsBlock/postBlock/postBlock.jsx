bb090dc50992124cc0f55ecca3bb31623b241758
raikbitters <raikbitters@gmail.com>
2023-07-06 16:22:50 +0000

    Change getPostContent

--- app/src/pages/outside/loginPage/socialSection/newsBlock/postBlock/postBlock.jsx
+++ app/src/pages/outside/loginPage/socialSection/newsBlock/postBlock/postBlock.jsx
@@ -19,83 +19,15 @@ import PropTypes from 'prop-types';
 import Parser from 'html-react-parser';
 import { LOGIN_PAGE_EVENTS } from 'components/main/analytics/events';
 import DOMPurify from 'dompurify';
+import { marked } from 'marked';
 import styles from './postBlock.scss';
 
 const cx = classNames.bind(styles);
 const HREF_TAG_NAME = 'A';
 
-const getPostContent = (text, entities) => {
-  const replaceObjects = [];
-  let result = '';
-  let currentReplaceObject;
-
-  const parseEntity = (curEntities, getHtml) => {
-    Object.keys(curEntities).map((objEntityKey) => {
-      const entity = curEntities[objEntityKey];
-      if (entity.indices[0] !== entity.indices[1]) {
-        replaceObjects.push({
-          start: entity.indices[0],
-          end: entity.indices[1],
-          html: getHtml(entity),
-        });
-      }
-      return true;
-    });
-  };
-
-  entities.urls &&
-    parseEntity(
-      entities.urls,
-      (entity) =>
-        `<a class=${cx('twit-link')} target="_blank" href="${entity.url}">${
-          entity.display_url
-        }</a>`,
-    );
-  entities.user_mentions &&
-    parseEntity(
-      entities.user_mentions,
-      (entity) =>
-        `<a class=${cx(
-          'twit-link',
-        )} target="_blank" href="https://twitter.com/intent/user?user_id=${entity.id}">@${
-          entity.screen_name
-        }</a>`,
-    );
-  entities.hashtags &&
-    parseEntity(
-      entities.hashtags,
-      (entity) =>
-        `<a class=${cx('twit-link')} target="_blank" href="https://twitter.com/hashtag/${
-          entity.text
-        }">#${entity.text}</a>`,
-    );
-  entities.media &&
-    parseEntity(
-      entities.media,
-      (entity) =>
-        `<a class=${cx('twit-link')} target="_blank" href="${entity.url}">${
-          entity.display_url
-        }</a>`,
-    );
-  replaceObjects.sort((a, b) => a.start - b.start);
-
-  const symbols = [...text];
-
-  symbols.forEach((symbol, index) => {
-    if (!currentReplaceObject && replaceObjects.length) {
-      currentReplaceObject = replaceObjects.shift();
-    }
-
-    if (!currentReplaceObject || index < currentReplaceObject.start) {
-      result += symbol;
-    } else if (currentReplaceObject.start === index) {
-      result += currentReplaceObject.html;
-    } else if (index === currentReplaceObject.end) {
-      currentReplaceObject = null;
-    }
-  });
-
-  return result.replace(/\n/g, '<br>');
+const getPostContent = (text) => {
+  const result = marked.parse(text);
+  return result;
 };
 
 const handleClick = (e, tracking) => {
@@ -107,7 +39,7 @@ const handleClick = (e, tracking) => {
 
 export const PostBlock = ({ tweetData, tracking }) => (
   <div className={cx('post-block')} onClick={(e) => handleClick(e, tracking)}>
-    {Parser(DOMPurify.sanitize(getPostContent(tweetData.text, tweetData.entities)))}
+    {Parser(DOMPurify.sanitize(getPostContent(tweetData.text)))}
   </div>
 );
 
