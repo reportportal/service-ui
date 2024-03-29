cfc51c20b3cf5a4b27f97623c6015b7190acd051
MariaHambardzumian <mariahambardzumian@gmail.com>
2024-03-29 15:01:26 +0400

    - formatter

--- app/src/pages/inside/logsPage/logItemInfo/defectDetails/defectDetails.jsx
+++ app/src/pages/inside/logsPage/logItemInfo/defectDetails/defectDetails.jsx
@@ -411,7 +411,7 @@ export class DefectDetails extends Component {
           {!debugMode && (
             <div className={cx('make-decision-action')}>
               <GhostButton
-                color='white'
+                color="white"
                 disabled={!logItem.issue}
                 onClick={this.handleEditDefect}
                 title={!logItem.issue ? formatMessage(messages.makeDecisionTooltip) : ''}
