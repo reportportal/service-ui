5394ef6cbf32b98cd168d55159af663ba1f811e0
MariaHambardzumian <mariahambardzumian@gmail.com>
2024-04-10 17:26:53 +0400

    EPMRPP-89687 | Include empty state for each plugin

---
+++ app/src/pages/inside/projectSettingsPageContainer/content/notifications/emptyRuleState/emptyRuleState.jsx
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2022 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import classNames from 'classnames/bind';
+import { useIntl } from 'react-intl';
+import plusIcon from 'common/img/plus-button-inline.svg';
+import { Button } from 'componentLibrary/button';
+import styles from './emptyRuleState.scss';
+import { messages } from '../messages';
+
+const cx = classNames.bind(styles);
+
+// eslint-disable-next-line react/prop-types
+export const EmptyRuleState = ({ ruleName, onCreateClick }) => {
+  const { formatMessage } = useIntl();
+  return (
+    <div className={cx('container')}>
+      <span className={cx('label')}>No {ruleName} notification rules yet</span>
+      <Button
+        onClick={onCreateClick}
+        variant={'text'}
+        customClassName={cx('button')}
+        startIcon={plusIcon}
+      >
+        {formatMessage(messages.create)}
+      </Button>
+    </div>
+  );
+};
