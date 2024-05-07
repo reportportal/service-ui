d45382706d8e658e471c21c56f419043909e8a40
Siarhei Iukou <siarhei_iukou@epam.com>
2024-05-07 11:48:29 +0300

    EPMRPP-90359 || Create redux store for organization

--- app/src/controllers/initialData/sagas.js
+++ app/src/controllers/initialData/sagas.js
@@ -25,6 +25,7 @@ import {
   authSuccessAction,
 } from 'controllers/auth';
 import { FETCH_PROJECT_SUCCESS, fetchProjectAction } from 'controllers/project';
+import { FETCH_ORGANIZATIONS_SUCCESS, fetchOrganizationsAction } from 'controllers/organizations';
 import {
   fetchGlobalIntegrationsAction,
   fetchPluginsAction,
@@ -42,8 +43,9 @@ function* fetchInitialData() {
   const userResult = yield take([FETCH_USER_SUCCESS, FETCH_USER_ERROR]);
   if (!userResult.error) {
     const { payload: activeProjectKey } = yield take(SET_ACTIVE_PROJECT_KEY);
+    yield put(fetchOrganizationsAction());
     yield put(fetchProjectAction(activeProjectKey));
-    yield take(FETCH_PROJECT_SUCCESS);
+    yield all([take(FETCH_PROJECT_SUCCESS), take(FETCH_ORGANIZATIONS_SUCCESS)]);
     yield put(fetchPluginsAction());
     yield put(fetchGlobalIntegrationsAction());
     yield put(authSuccessAction());
---
+++ app/src/controllers/organizations/reducer.js
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2024 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { combineReducers } from 'redux';
+import {} from 'controllers/filter/constants';
+import {
+  ORGANIZATIONS_INITIAL_STATE,
+  FETCH_ORGANIZATIONS_SUCCESS,
+  FETCH_ORGANIZATIONS,
+} from './constants';
+
+export const organizationsInfoReducer = (
+  state = ORGANIZATIONS_INITIAL_STATE,
+  { type = '', payload = {} },
+) => {
+  switch (type) {
+    case FETCH_ORGANIZATIONS_SUCCESS:
+      return { ...state, ...payload };
+    default:
+      return state;
+  }
+};
+
+export const organizationsLoadingReducer = (state = false, { type = '' }) => {
+  switch (type) {
+    case FETCH_ORGANIZATIONS:
+      return true;
+    case FETCH_ORGANIZATIONS_SUCCESS:
+      return false;
+    default:
+      return state;
+  }
+};
+
+export const organizationsReducer = combineReducers({
+  info: organizationsInfoReducer,
+  infoLoading: organizationsLoadingReducer,
+});
