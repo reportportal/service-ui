53dea4abdcf00f13f6f4516a315c578d98beee59
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2024-05-07 14:31:52 +0200

    EPMRPP-90318 || Remote plugins support

--- app/src/layouts/adminLayout/adminUiExtensionPageLayout/index.js
+++ app/src/layouts/adminLayout/adminUiExtensionPageLayout/index.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 EPAM Systems
+ * Copyright 2024 EPAM Systems
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -10,14 +10,8 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-$Z-INDEX-PAGE-CONTAINER-HEADER: 2;
-
-.header {
-  position: sticky;
-  top: 0;
-  z-index: $Z-INDEX-PAGE-CONTAINER-HEADER;
-}
+export { AdminUiExtensionPageLayout } from './adminUiExtensionPageLayout';
