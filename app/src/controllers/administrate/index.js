c38956d7dfc53e83a2f71960476fd346a819430e
Siarhei Iukou <siarhei_iukou@epam.com>
2024-06-24 13:25:19 +0300

    EPMRPP-91885 || fix wrong import

--- app/src/controllers/administrate/index.js
+++ app/src/controllers/administrate/index.js
@@ -17,6 +17,6 @@
 export { administrateReducer } from './reducer';
 export { eventsSelector, fetchEventsAction } from './events';
 export { allUsersSelector, fetchAllUsersAction } from './allUsers';
-export { fetchProjectDataAction, fetchActiveProjectDataAction } from './actionCreators';
+export { fetchProjectDataAction } from './actionCreators';
 export { NAMESPACE as ADMIN_NAMESPACE } from './constants';
 export { administrateSagas } from './sagas';
