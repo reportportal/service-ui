abc04ecde888e92fe1fa7ce327fc98f4ee66a068
Siarhei Iukou <siarhei_iukou@epam.com>
2024-07-30 14:39:02 +0300

    EPMRPP-93205 || Code Review fix - 2

--- app/src/layouts/organizationsControl/organizationsControl.jsx
+++ app/src/layouts/organizationsControl/organizationsControl.jsx
@@ -42,7 +42,7 @@ export const OrganizationsControl = ({
       <div>
         <div
           className={cx('organization-btn-wrapper', {
-            'instance-level': !isExtendedNav,
+            'not-extended': !isExtendedNav,
           })}
         >
           {isExtendedNav ? (
