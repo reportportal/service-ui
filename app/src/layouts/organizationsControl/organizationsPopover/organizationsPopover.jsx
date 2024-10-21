ce59956e4fe572fe33766f70aab1d9e179bc1eb9
Siarhei Iukou <siarhei_iukou@epam.com>
2024-10-21 15:49:45 +0300

    EPMRPP-96153 || Code Review fix - 1

--- app/src/layouts/organizationsControl/organizationsPopover/organizationsPopover.jsx
+++ app/src/layouts/organizationsControl/organizationsPopover/organizationsPopover.jsx
@@ -32,6 +32,7 @@ import { COMMON_LOCALE_KEYS } from 'common/constants/localization';
 import { SIDEBAR_EVENTS } from 'components/main/analytics/events';
 import { useTracking } from 'react-tracking';
 import { locationSelector } from 'controllers/pages/selectors';
+import routesMap from 'routes/routesMap';
 import { OrganizationsItem } from './organizationsItem';
 import styles from './organizationsPopover.scss';
 
@@ -59,7 +60,7 @@ export const OrganizationsPopover = ({ closePopover, closeSidebar }) => {
   const { trackEvent } = useTracking();
   const maxHeightPopover = window.innerHeight - MARGIN_TOP_AND_MARGIN_BOTTOM;
   const location = useSelector(locationSelector);
-  const isAllOrganizationPage = location.pathname === '/organizations';
+  const isAllOrganizationPage = location.pathname === routesMap.ORGANIZATIONS_PAGE.path;
 
   const filteredProjects = useMemo(
     () =>
