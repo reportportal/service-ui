2ab3f0ddd7a6061e3eb19dd5977d85614f75bfa1
Siarhei Iukou <siarhei_iukou@epam.com>
2024-10-21 15:38:00 +0300

    EPMRPP-96153 || All Organizations option in the popover is always highlighted

--- app/src/layouts/organizationsControl/organizationsPopover/organizationsPopover.jsx
+++ app/src/layouts/organizationsControl/organizationsPopover/organizationsPopover.jsx
@@ -31,6 +31,7 @@ import { NavLink } from 'components/main/navLink';
 import { COMMON_LOCALE_KEYS } from 'common/constants/localization';
 import { SIDEBAR_EVENTS } from 'components/main/analytics/events';
 import { useTracking } from 'react-tracking';
+import { locationSelector } from 'controllers/pages/selectors';
 import { OrganizationsItem } from './organizationsItem';
 import styles from './organizationsPopover.scss';
 
@@ -57,6 +58,8 @@ export const OrganizationsPopover = ({ closePopover, closeSidebar }) => {
   const [isSearchEventTriggered, setIsSearchEventTriggered] = useState(false);
   const { trackEvent } = useTracking();
   const maxHeightPopover = window.innerHeight - MARGIN_TOP_AND_MARGIN_BOTTOM;
+  const location = useSelector(locationSelector);
+  const isAllOrganizationPage = location.pathname === '/organizations';
 
   const filteredProjects = useMemo(
     () =>
@@ -126,7 +129,7 @@ export const OrganizationsPopover = ({ closePopover, closeSidebar }) => {
                 onClose();
                 trackEvent(SIDEBAR_EVENTS.CLICK_ALL_ORGANIZATION_PROJECTS);
               }}
-              activeClassName={cx('active')}
+              activeClassName={cx({ active: isAllOrganizationPage })}
             >
               {formatMessage(messages.allOrganizations)}
             </NavLink>
