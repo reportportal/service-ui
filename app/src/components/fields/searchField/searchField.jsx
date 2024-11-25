f8490c063932a6337fb546552c40e0d08902fda7
Iukou Siarhei <45054016+BlazarQSO@users.noreply.github.com>
2024-11-25 14:49:44 +0300

    EPMRPP-97156 || Search field does not open upon clicking the left sid… (#4107)
    * EPMRPP-97156 || Search field does not open upon clicking the left side of the loupe icon
    
    * EPMRPP-97156 || update ui-kit

--- app/src/components/fields/searchField/searchField.jsx
+++ app/src/components/fields/searchField/searchField.jsx
@@ -17,7 +17,7 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useTracking } from 'react-tracking';
-import { BaseIconButton, FieldText, SearchIcon } from '@reportportal/ui-kit';
+import { FieldText, SearchIcon } from '@reportportal/ui-kit';
 import classNames from 'classnames/bind';
 import styles from './searchField.scss';
 
@@ -79,11 +79,7 @@ export const SearchField = ({
       onBlur={handleBlur}
       placeholder={placeholder}
       loading={isSearchActive && isLoading}
-      startIcon={
-        <BaseIconButton>
-          <SearchIcon />
-        </BaseIconButton>
-      }
+      startIcon={<SearchIcon />}
       className={cx('search-field')}
       maxLength={256}
       collapsible
