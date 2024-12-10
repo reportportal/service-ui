771b2ddb95afecc1e405c91f84738f6f8413bec4
Siarhei Iukou <siarhei_iukou@epam.com>
2024-12-10 14:07:58 +0300

    remove unnecessary imports

--- app/src/controllers/organization/projects/index.js
+++ app/src/controllers/organization/projects/index.js
@@ -16,12 +16,7 @@
 
 export { fetchOrganizationProjectsAction, fetchFilteredProjectAction } from './actionCreators';
 export { projectsReducer } from './reducer';
-export {
-  projectsPaginationSelector,
-  projectsSelector,
-  loadingSelector,
-  filterQuerySelector,
-} from './selectors';
+export { projectsPaginationSelector, projectsSelector, loadingSelector } from './selectors';
 export { projectsSagas } from './sagas';
 export {
   DEFAULT_LIMITATION,
