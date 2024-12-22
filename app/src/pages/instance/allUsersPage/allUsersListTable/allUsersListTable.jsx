48eb5e0921d6bdf101a5bb64a70dcf635aeb8a23
iso9000t <119494473+iso9000t@users.noreply.github.com>
2024-12-23 00:27:31 +0400

    refactor: reuse messages

--- app/src/pages/instance/allUsersPage/allUsersListTable/allUsersListTable.jsx
+++ app/src/pages/instance/allUsersPage/allUsersListTable/allUsersListTable.jsx
@@ -1,9 +1,14 @@
 import { useMemo } from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage, useIntl, defineMessages } from 'react-intl';
+import { useIntl } from 'react-intl';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames/bind';
 import { AbsRelTime } from 'components/main/absRelTime';
+import { MeatballMenuIcon, Popover, Tooltip } from '@reportportal/ui-kit';
+import { UserAvatar } from 'pages/inside/common/userAvatar';
+import { userInfoSelector, activeProjectKeySelector } from 'controllers/user';
+import { getRoleBadgesData } from 'common/utils/permissions/getRoleTitle';
+import { ADMIN_TYPE } from 'common/utils/permissions/constants';
 import {
   DEFAULT_PAGE_SIZE,
   DEFAULT_PAGINATION,
@@ -11,42 +16,17 @@ import {
   withPagination,
 } from 'controllers/pagination';
 import { SORTING_ASC, withSortingURL } from 'controllers/sorting';
-import { ADMINISTRATOR } from 'common/constants/accountRoles';
 import {
   DEFAULT_SORT_COLUMN,
   allUsersPaginationSelector,
   fetchAllUsersAction,
 } from 'controllers/instance/allUsers';
-import { userInfoSelector } from 'controllers/user';
 import { MembersListTable } from 'pages/organization/common/membersPage/membersListTable';
-import { MeatballMenuIcon } from '@reportportal/ui-kit';
+import { messages } from 'pages/organization/common/membersPage/messages';
 import styles from './allUsersListTable.scss';
 
 const cx = classNames.bind(styles);
 const NAMESPACE = 'allUsers';
-
-const messages = defineMessages({
-  name: {
-    id: 'AllUsersPage.name',
-    defaultMessage: 'Name',
-  },
-  email: {
-    id: 'AllUsersPage.email',
-    defaultMessage: 'Email',
-  },
-  lastLogin: {
-    id: 'AllUsersPage.lastLogin',
-    defaultMessage: 'Last login',
-  },
-  type: {
-    id: 'AllUsersPage.type',
-    defaultMessage: 'Type',
-  },
-  organizations: {
-    id: 'AllUsersPage.organizations',
-    defaultMessage: 'Organizations',
-  },
-});
 
 const AllUsersListTableComponent = ({
   users,
@@ -62,11 +42,21 @@ const AllUsersListTableComponent = ({
   const { formatMessage } = useIntl();
   const dispatch = useDispatch();
   const currentUser = useSelector(userInfoSelector);
+  const activeProjectKey = useSelector(activeProjectKeySelector);
 
   const renderRowActions = () => (
-    <i className={cx('menu-icon')}>
-      <MeatballMenuIcon />
-    </i>
+    <Popover
+      placement={'bottom-end'}
+      content={
+        <div className={cx('row-action-menu')}>
+          <p>Manage assignments</p>
+        </div>
+      }
+    >
+      <i className={cx('menu-icon')}>
+        <MeatballMenuIcon />
+      </i>
+    </Popover>
   );
 
   const data = useMemo(
@@ -74,6 +64,7 @@ const AllUsersListTableComponent = ({
       users.map((user) => {
         const organizationsCount = Object.keys(user.assignedOrganizations || {}).length;
         const isCurrentUser = user.id === currentUser.id;
+        const memberBadges = getRoleBadgesData(user.userRole, null, isCurrentUser);
 
         return {
           id: user.id,
@@ -81,18 +72,33 @@ const AllUsersListTableComponent = ({
             content: user.fullName,
             component: (
               <div className={cx('member-name-column')}>
+                <UserAvatar
+                  className={cx('custom-user-avatar')}
+                  projectKey={activeProjectKey}
+                  userId={user.userId}
+                />
                 <div className={cx('full-name')}>{user.fullName}</div>
                 <div className={cx('badges')}>
-                  {user.userRole === ADMINISTRATOR && (
-                    <div className={cx('badge', 'admin-badge')}>
-                      <FormattedMessage id={'UserBlock.adminBadge'} defaultMessage={'admin'} />
-                    </div>
-                  )}
-                  {isCurrentUser && (
-                    <div className={cx('badge', 'you-badge')}>
-                      <FormattedMessage id={'UserBlock.youBadge'} defaultMessage={'you'} />
-                    </div>
-                  )}
+                  {memberBadges.map(({ title, type }) => {
+                    const badgeContent = (
+                      <div key={`${user.userId}-${type}`} className={cx('badge', type)}>
+                        {formatMessage(title)}
+                      </div>
+                    );
+
+                    return type === ADMIN_TYPE ? (
+                      <Tooltip
+                        key={`${user.userId}-${type}-tooltip`}
+                        content={formatMessage(messages.adminAccessInfo)}
+                        placement="top"
+                        width={248}
+                      >
+                        {badgeContent}
+                      </Tooltip>
+                    ) : (
+                      badgeContent
+                    );
+                  })}
                 </div>
               </div>
             ),
@@ -110,7 +116,7 @@ const AllUsersListTableComponent = ({
           organizations: organizationsCount,
         };
       }),
-    [users, currentUser.id],
+    [users, currentUser.id, activeProjectKey, formatMessage],
   );
 
   const primaryColumn = {
@@ -133,13 +139,13 @@ const AllUsersListTableComponent = ({
     },
     {
       key: 'accountType',
-      header: formatMessage(messages.type),
+      header: formatMessage(messages.role),
       width: 114,
       align: 'left',
     },
     {
       key: 'organizations',
-      header: formatMessage(messages.organizations),
+      header: formatMessage(messages.projects),
       width: 104,
       align: 'right',
     },
