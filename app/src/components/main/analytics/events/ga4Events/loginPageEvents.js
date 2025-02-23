affa87ce61689d81c37ad00676feb6192c8f0c83
Siarhei Iukou <siarhei_iukou@epam.com>
2025-02-24 00:59:23 +0300

    EPMRPP-99318 || Add event for Login page. Button Login.

--- app/src/components/main/analytics/events/ga4Events/loginPageEvents.js
+++ app/src/components/main/analytics/events/ga4Events/loginPageEvents.js
@@ -1,5 +1,7 @@
 import { getBasicClickEventParameters } from '../common/ga4Utils';
 
+export const LOGIN = 'login';
+
 const LOGIN_PAGE = 'login_page';
 
 export const LOGIN_PAGE_EVENTS = {
@@ -14,5 +16,31 @@ export const LOGIN_PAGE_EVENTS = {
     ...getBasicClickEventParameters(LOGIN_PAGE),
     element_name: 'logo',
     place: 'header',
+  },
+  clickOnLoginButton: (authType) => {
+    let elementName = 'login_with_epam';
+
+    switch (authType) {
+      case 'github': {
+        elementName = 'login_with_github';
+        break;
+      }
+      case 'samlProviders': {
+        elementName = 'login_with_saml';
+        break;
+      }
+      case LOGIN: {
+        elementName = LOGIN;
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+
+    return {
+      ...getBasicClickEventParameters(LOGIN_PAGE),
+      element_name: elementName,
+    };
   },
 };
