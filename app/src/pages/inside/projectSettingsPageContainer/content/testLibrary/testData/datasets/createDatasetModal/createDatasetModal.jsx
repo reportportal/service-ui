072cab185b130e55188a09a8ad4245a037ebd6be
Yelyzaveta Khokhlova <yelyzaveta_khokhlova@epam.com>
2025-03-04 18:48:04 +0200

    Implement create dataset modal

---
+++ app/src/pages/inside/projectSettingsPageContainer/content/testLibrary/testData/datasets/createDatasetModal/createDatasetModal.jsx
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2025 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { useState } from 'react';
+import { defineMessages, useIntl } from 'react-intl';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { reduxForm } from 'redux-form';
+import { Modal, FieldText } from '@reportportal/ui-kit';
+import { COMMON_LOCALE_KEYS } from 'common/constants/localization';
+import { hideModalAction, withModal } from 'controllers/modal';
+import { FieldProvider } from 'components/fields';
+import { commonValidators } from 'common/utils/validation';
+import { FieldErrorHint } from 'components/fields/fieldErrorHint';
+
+const messages = defineMessages({
+  createDataset: {
+    id: 'TestData.createDataset',
+    defaultMessage: 'Create Dataset',
+  },
+  name: {
+    id: 'TestData.name',
+    defaultMessage: 'Name',
+  },
+});
+
+export const CREATE_DATASET_MODAL_KEY = 'createDatasetModal';
+
+export const CreateDataset = ({ data: { onSubmit }, handleSubmit }) => {
+  const { formatMessage } = useIntl();
+  const dispatch = useDispatch();
+  const [datasetName, setDatasetName] = useState();
+
+  const okButton = {
+    children: formatMessage(COMMON_LOCALE_KEYS.CREATE),
+    onClick: handleSubmit(onSubmit),
+  };
+
+  return (
+    <Modal
+      title={formatMessage(messages.createDataset)}
+      okButton={okButton}
+      cancelButton={{ children: formatMessage(COMMON_LOCALE_KEYS.CANCEL) }}
+      onClose={() => dispatch(hideModalAction())}
+    >
+      <form>
+        <FieldProvider name="datasetName">
+          <FieldErrorHint provideHint={false}>
+            <FieldText
+              label={formatMessage(messages.name)}
+              defaultWidth={false}
+              value={datasetName}
+              onChange={(e) => setDatasetName(e.target.value)}
+            />
+          </FieldErrorHint>
+        </FieldProvider>
+      </form>
+    </Modal>
+  );
+};
+
+CreateDataset.propTypes = {
+  data: PropTypes.shape({
+    onSubmit: PropTypes.func.isRequired,
+  }),
+  handleSubmit: PropTypes.func.isRequired,
+};
+
+export const CreateDatasetModal = withModal(CREATE_DATASET_MODAL_KEY)(
+  reduxForm({
+    form: 'createDatasetForm',
+    validate: ({ datasetName }) => ({
+      datasetName: commonValidators.requiredField(datasetName),
+    }),
+  })(CreateDataset),
+);
