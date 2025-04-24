3e1d62b423b1a5d6f5f00d378da350fe63b52b40
Oleksandr Dragun <oleksandr_dragun@epam.com>
2025-04-24 13:22:31 +0300

    implement linked test cases content

---
+++ app/src/pages/inside/productVersionPage/linkedTestCasesTab/messages.js
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2025 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export const messages = defineMessages({
+  description: {
+    id: 'LinkedTestCasesTab.description',
+    defaultMessage:
+      'These tests have been updated for this product version. If the functionality is released, we recommend setting them as the default versions to ensure they are used moving forward.',
+  },
+  testCaseName: {
+    id: 'LinkedTestCasesTab.testCaseName',
+    defaultMessage: 'Test case name',
+  },
+  executionStatus: {
+    id: 'LinkedTestCasesTab.executionStatus',
+    defaultMessage: 'Execution status',
+  },
+  defaultVersion: {
+    id: 'LinkedTestCasesTab.defaultVersion',
+    defaultMessage: 'Default version',
+  },
+  testCasesVersions: {
+    id: 'LinkedTestCasesTab.testCasesVersions',
+    defaultMessage: 'Test cases versions',
+  },
+  whatIsDefaultTestTitle: {
+    id: 'LinkedTestCasesTab.whatIsDefaultTestTitle',
+    defaultMessage: 'What is a Default test?',
+  },
+  whatIsDefaultTestDescription: {
+    id: 'LinkedTestCasesTab.whatIsDefaultTestDescription',
+    defaultMessage: 'Learn more about available plugins to extend notifying functionality',
+  },
+  howMilestonesWorkTitle: {
+    id: 'LinkedTestCasesTab.howMilestonesWorkTitle',
+    defaultMessage: 'How Milestones work?',
+  },
+  howMilestonesWorkDescription: {
+    id: 'LinkedTestCasesTab.howMilestonesWorkDescription',
+    defaultMessage: 'Learn more about available plugins to extend notifying functionality',
+  },
+  howManageTestChangesTitle: {
+    id: 'LinkedTestCasesTab.howManageTestChangesTitle',
+    defaultMessage: 'How to manage Test changes?',
+  },
+  howManageTestChangesDescription: {
+    id: 'LinkedTestCasesTab.howManageTestChangesDescription',
+    defaultMessage: 'Learn more about available plugins to extend notifying functionality',
+  },
+});
