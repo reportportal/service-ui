92a6857ea834213e62d276cf367c1def90c56229
OleksandrDragun <42969419+OleksandrDragun@users.noreply.github.com>
2025-06-04 14:04:48 +0300

    implement test case library empty page (#4350)
    * implement test case library empty page
    
    * move emptyStatePage component to common folder, use doc link from constant

---
+++ app/src/pages/inside/testCaseLibraryPage/emptyState/emptyState.jsx
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2025 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import Parser from 'html-react-parser';
+import { useIntl } from 'react-intl';
+
+import { NumerableBlock } from 'pages/common/numerableBlock';
+import { EmptyStatePage } from 'pages/inside/common/emptyStatePage';
+import { referenceDictionary } from 'common/utils';
+
+import ImportIcon from 'common/img/import-thin-inline.svg';
+import { messages } from './messages';
+
+export const EmptyState = () => {
+  const { formatMessage } = useIntl();
+
+  const benefits = [messages.createFolder, messages.addTestCases, messages.tagTestCases].map(
+    (translation) =>
+      formatMessage(translation, {
+        strong: (chunks) => <strong>{chunks}</strong>,
+        br: <br />,
+      }),
+  );
+
+  return (
+    <>
+      <EmptyStatePage
+        title={formatMessage(messages.emptyPageTitle)}
+        description={Parser(formatMessage(messages.emptyPageDescription))}
+        imageType="docs"
+        documentationLink={referenceDictionary.rpDoc}
+        buttons={[
+          {
+            name: formatMessage(messages.createTestCase),
+            dataAutomationId: 'createTestCaseButton',
+            isCompact: true,
+          },
+          {
+            name: formatMessage(messages.importTestCases),
+            dataAutomationId: 'createTestCaseButton',
+            variant: 'ghost',
+            icon: ImportIcon,
+            isCompact: true,
+          },
+        ]}
+      />
+      <NumerableBlock
+        items={benefits}
+        title={formatMessage(messages.numerableBlockTitle)}
+        fullWidth
+      />
+    </>
+  );
+};
