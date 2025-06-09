70e16fdd72e8269718d2883f37f889a829a1e15c
Oleksandr Dragun <oleksandr_dragun@epam.com>
2025-06-09 19:16:36 +0300

    implement custom folders tree for expanded options page

---
+++ app/src/pages/inside/testCaseLibraryPage/expandedOptions/mockData.js
@@ -0,0 +1,305 @@
+export const FOLDERS = [
+  {
+    name: 'Checkout flow',
+    testsCount: 2048,
+  },
+  {
+    name: 'Order history',
+    testsCount: 48,
+    folders: [
+      {
+        name: 'Data migration',
+        testsCount: 8,
+        folders: [
+          {
+            name: 'Email notifications',
+            testsCount: 30,
+            folders: [
+              {
+                name: 'Cart management',
+                testsCount: 25,
+                folders: [
+                  {
+                    name: 'Cart management system',
+                    testsCount: 125,
+                    folders: [
+                      {
+                        name: 'Email notifications 2',
+                        testsCount: 30,
+                        folders: [
+                          {
+                            name: 'Cart management 2',
+                            testsCount: 25,
+                            folders: [
+                              {
+                                name: 'Cart management 2 system',
+                                testsCount: 125,
+                                folders: [
+                                  {
+                                    name: 'Email notifications 3',
+                                    testsCount: 30,
+                                    folders: [
+                                      {
+                                        name: 'Cart management 3',
+                                        testsCount: 25,
+                                        folders: [
+                                          {
+                                            name: 'Cart management 3 system',
+                                            testsCount: 125,
+                                            folders: [
+                                              {
+                                                name: 'Email notifications 4',
+                                                testsCount: 30,
+                                                folders: [
+                                                  {
+                                                    name: 'Cart management 4',
+                                                    testsCount: 25,
+                                                    folders: [
+                                                      {
+                                                        name: 'Cart management 4 system',
+                                                        testsCount: 125,
+                                                      },
+                                                    ],
+                                                  },
+                                                ],
+                                              },
+                                              {
+                                                name: 'User logout 2',
+                                                testsCount: 20,
+                                                folders: [
+                                                  {
+                                                    name: 'User logout 2 system',
+                                                    testsCount: 125,
+                                                  },
+                                                ],
+                                              },
+                                              {
+                                                name: 'User feedback 2',
+                                                testsCount: 10,
+                                                folders: [
+                                                  {
+                                                    name: 'Performance testing 2',
+                                                    testsCount: 2,
+                                                  },
+                                                  {
+                                                    name: 'API integration 2',
+                                                    testsCount: 3,
+                                                  },
+                                                ],
+                                              },
+                                            ],
+                                          },
+                                        ],
+                                      },
+                                    ],
+                                  },
+                                  {
+                                    name: 'User logout 3',
+                                    testsCount: 20,
+                                    folders: [
+                                      {
+                                        name: 'User logout 3 system',
+                                        testsCount: 125,
+                                      },
+                                    ],
+                                  },
+                                  {
+                                    name: 'User feedback 3',
+                                    testsCount: 10,
+                                    folders: [
+                                      {
+                                        name: 'Performance testing 3',
+                                        testsCount: 2,
+                                      },
+                                      {
+                                        name: 'API integration 3',
+                                        testsCount: 3,
+                                      },
+                                    ],
+                                  },
+                                ],
+                              },
+                            ],
+                          },
+                        ],
+                      },
+                      {
+                        name: 'User logout 4',
+                        testsCount: 20,
+                        folders: [
+                          {
+                            name: 'User logout 4 system',
+                            testsCount: 125,
+                          },
+                        ],
+                      },
+                      {
+                        name: 'User feedback 4',
+                        testsCount: 10,
+                        folders: [
+                          {
+                            name: 'Performance testing 4',
+                            testsCount: 2,
+                          },
+                          {
+                            name: 'API integration 4',
+                            testsCount: 3,
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: 'User logout',
+            testsCount: 20,
+            folders: [
+              {
+                name: 'User logout system',
+                testsCount: 125,
+              },
+            ],
+          },
+          {
+            name: 'User feedback',
+            testsCount: 10,
+            folders: [
+              {
+                name: 'Performance testing',
+                testsCount: 2,
+              },
+              {
+                name: 'API integration',
+                testsCount: 3,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: 'Search filters',
+        testsCount: 40,
+        folders: [
+          {
+            name: 'Search filters system',
+            testsCount: 425,
+          },
+        ],
+      },
+      {
+        name: 'Security features',
+        testsCount: 5,
+        folders: [
+          {
+            name: 'Search filters system',
+            testsCount: 55,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Profile update',
+    testsCount: 100,
+    folders: [
+      {
+        name: 'Profile update management',
+        testsCount: 25,
+      },
+    ],
+  },
+  {
+    name: 'Browser compatibility',
+    testsCount: 1200,
+    folders: [
+      {
+        name: 'Browser compatibility system',
+        testsCount: 20,
+      },
+    ],
+  },
+  {
+    name: 'Password Reset Procedures and Guidelines for User Account Management',
+    testsCount: 8192,
+    folders: [
+      {
+        name: 'Password Reset Procedures and Guidelines',
+      },
+    ],
+  },
+  {
+    name: 'Product search',
+    testsCount: 1024,
+    folders: [
+      {
+        name: 'Product search type',
+        testsCount: 4,
+      },
+    ],
+  },
+  {
+    name: 'Payment processing',
+    testsCount: 1024,
+    folders: [
+      {
+        name: 'Payment processing type',
+        testsCount: 4,
+      },
+    ],
+  },
+  {
+    name: 'User login',
+    testsCount: 204,
+    folders: [
+      {
+        name: 'User login type',
+        testsCount: 42,
+      },
+    ],
+  },
+  {
+    name: 'Account registration',
+    testsCount: 4096,
+    folders: [
+      {
+        name: 'Account registration type',
+        testsCount: 42,
+      },
+    ],
+  },
+  {
+    name: 'Folder name',
+    testsCount: 12,
+  },
+  {
+    name: 'Data migration 2',
+    testsCount: 8,
+  },
+  {
+    name: 'Email notifications new',
+    testsCount: 30,
+    folders: [
+      {
+        name: 'Email notifications type',
+        testsCount: 42,
+      },
+    ],
+  },
+  {
+    name: 'Cart management new',
+    testsCount: 25,
+    folders: [
+      {
+        name: 'Cart management new type',
+        testsCount: 42,
+      },
+    ],
+  },
+  {
+    name: 'Wishlist functionality for ',
+    testsCount: 50,
+  },
+];
