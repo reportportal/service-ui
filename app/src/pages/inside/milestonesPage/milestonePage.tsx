1c2cb6f5b80fae81b28d8256c5a807b450e4239e
Artem_Blazhko <artem_blazhko@epam.com>
2025-07-11 00:44:26 +0300

    Milestone initial state

---
+++ app/src/pages/inside/milestonesPage/milestonePage.tsx
@@ -0,0 +1,24 @@
+import { useIntl } from 'react-intl';
+import classNames from 'classnames/bind';
+import { SettingsLayout } from 'layouts/settingsLayout';
+import { ScrollWrapper } from 'components/main/scrollWrapper';
+import { EmptyMilestones } from 'pages/inside/milestonesPage/emptyMilestones';
+import { messages } from './messages';
+import styles from './milestonesPage.scss';
+
+const cx = classNames.bind(styles);
+
+export const MilestonePage = () => {
+  const { formatMessage } = useIntl();
+
+  return (
+    <SettingsLayout>
+      <ScrollWrapper resetRequired>
+        <header className={cx('milestones-page__header')}>
+          <h1>{formatMessage(messages.pageTitle)}</h1>
+        </header>
+        <EmptyMilestones />
+      </ScrollWrapper>
+    </SettingsLayout>
+  );
+};
