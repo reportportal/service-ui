c99d2033d271c3831c10509b3a39e3aa41724f6b
Alla Prishchepa <alla_prishchepa@epam.com>
2025-07-18 16:52:01 +0400

    EPMRPP-93349 || Fix ApiError import

--- app/src/pages/instance/allUsersPage/allUsersHeader/allUsersExport/allUsersExport.tsx
+++ app/src/pages/instance/allUsersPage/allUsersHeader/allUsersExport/allUsersExport.tsx
@@ -24,7 +24,8 @@ import { querySelector } from 'controllers/instance/allUsers';
 import { defineMessages, useIntl } from 'react-intl';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTracking } from 'react-tracking';
-import { ApiError, QueryParams } from 'types/common';
+import { QueryParams } from 'types/common';
+import { ApiError } from 'types/api';
 import { showDefaultErrorNotification, showSuccessNotification } from 'controllers/notification';
 
 interface AllUsersExportProps {
