bfcf566d4ef3a215b23602d12f525b3aa60468fe
Reingold Shekhtel <13565058+raikbitters@users.noreply.github.com>
2025-07-18 12:47:38 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 export API_URL=${API_URL:-../api}
 export AUTH_URL=${AUTH_URL:-../uat}
-export OAUTH_PROXY_ENABLED=${OAUTH_PROXY_ENABLED:-false}
 
 envsubst < /usr/share/nginx/html/config.template.json > /usr/share/nginx/html/config.json
 
