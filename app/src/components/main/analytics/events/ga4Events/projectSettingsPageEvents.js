2bcd441402e51a908943e435d9c89f773d39abcc
Alla Prishchepa <alla_prishchepa@epam.com>
2025-08-11 13:36:12 +0400

    EPMRPP-106335 || Add project attribute for configuring the priority of retry to be analyzed

--- app/src/components/main/analytics/events/ga4Events/projectSettingsPageEvents.js
+++ app/src/components/main/analytics/events/ga4Events/projectSettingsPageEvents.js
@@ -71,7 +71,14 @@ const getStatus = (status) => (status ? 'active' : 'disabled');
 const getSwitcher = (switcher) => (switcher ? 'on' : 'off');
 
 export const PROJECT_SETTINGS_ANALYZER_EVENTS = {
-  clickSubmitInAutoAnalyzerTab: (number, status, condition, switcher, type) => ({
+  clickSubmitInAutoAnalyzerTab: (
+    number,
+    status,
+    condition,
+    switcher,
+    largestRetryPriority,
+    type,
+  ) => ({
     ...BASIC_EVENT_PARAMETERS_ANALYZER_TAB,
     place: `${ANALYZER}_auto_analyzer`,
     element_name: 'button_submit',
@@ -80,6 +87,7 @@ export const PROJECT_SETTINGS_ANALYZER_EVENTS = {
     condition: LAUNCH_ANALYZE_TYPES_TO_ANALYTICS_TITLES_MAP[condition],
     switcher,
     type,
+    icon_name: `${largestRetryPriority ? '' : 'un'}select_largest_retry_priority`,
   }),
 
   clickSubmitInSimilarItemsTab: (number) => ({
