ec1d2881e4735dd73b6cb24c784eb2c3f16b29f9
mcharnou <kvaha@adnoc.ae>
2025-08-14 11:05:45 +0300

    fix: update styles / reuse another button

--- app/src/pages/inside/testCaseLibraryPage/allTestCasesPage/types.ts
+++
@@ -1,9 +0,0 @@
-export interface DotsMenuItem {
-  label: string;
-  value: string;
-  hidden?: boolean;
-  disabled?: boolean;
-  title: string;
-  onClick: VoidFunction;
-  type?: 'danger' | 'separator';
-}
