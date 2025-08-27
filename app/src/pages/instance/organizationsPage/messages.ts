3b1f842e88fab4e7e2f30c17776e78265e4d4cf7
Alla Prishchepa <alla_prishchepa@epam.com>
2025-08-27 12:03:02 +0400

    EPMRPP-105492 || Add menu options for organizations on All organizations page

--- app/src/pages/instance/organizationsPage/messages.ts
+++ app/src/pages/instance/organizationsPage/messages.ts
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2024 EPAM Systems
+ * Copyright 2025 EPAM Systems
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -105,6 +105,10 @@ export const messages = defineMessages({
   activity: {
     id: 'OrganizationsPage.activity',
     defaultMessage: 'Activity',
+  },
+  settings: {
+    id: 'OrganizationsPage.settings',
+    defaultMessage: 'Settings',
   },
   notUploadedPluginMessage: {
     id: 'OrganizationsPage.notUploadedPluginMessage',
