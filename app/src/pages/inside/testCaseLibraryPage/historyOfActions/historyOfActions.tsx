68adf349d7147fe091eb834c90a13c12a0f80b02
OleksandrDragun <42969419+OleksandrDragun@users.noreply.github.com>
2025-10-15 10:11:39 +0300

    fix linked test case table, history of action table, add action for navigating to history of action on side panel (#4657)
    * fix linked test case table, history of action table, add action for navigating to history of action on side panel
    
    * create separate handler instead of passing as a prop

--- app/src/pages/inside/testCaseLibraryPage/historyOfActions/historyOfActions.tsx
+++ app/src/pages/inside/testCaseLibraryPage/historyOfActions/historyOfActions.tsx
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+import { useState } from 'react';
 import { useIntl } from 'react-intl';
 import { Table } from '@reportportal/ui-kit';
 
@@ -33,6 +34,7 @@ const cx = createClassnames(styles);
 
 export const HistoryOfActions = () => {
   const { formatMessage } = useIntl();
+  const [expandedRows, setExpandedRows] = useState<Set<number | string>>(new Set([]));
   // Temporary name. Need to get it in response.
   const testCaseName = '24.2 PV';
 
@@ -108,6 +110,16 @@ export const HistoryOfActions = () => {
     },
   ];
 
+  const handleToggleRowExpansion = (id: string | number) => {
+    const newExpandedRows = new Set(expandedRows);
+    if (newExpandedRows.has(id)) {
+      newExpandedRows.delete(id);
+    } else {
+      newExpandedRows.add(id);
+    }
+    setExpandedRows(newExpandedRows);
+  };
+
   return (
     <SettingsLayout>
       <ScrollWrapper resetRequired={false}>
@@ -124,6 +136,10 @@ export const HistoryOfActions = () => {
               sortableColumns={[]}
               className={cx('history-of-actions__table')}
               rowClassName={cx('history-of-actions__table-row')}
+              isRowsExpandable
+              setExpandedRowIds={setExpandedRows}
+              expandedRowIds={[...expandedRows]}
+              onToggleRowExpansion={handleToggleRowExpansion}
             />
           </div>
         </div>
