c54190601b74fb7f9fcff1ed26b18f3824d0f605
Artem Blazhko <artem_blazhko@epam.com>
2025-11-06 13:35:02 +0200

    Add missed message, change header  font size (#4710)

--- app/src/pages/inside/common/testCaseList/testCaseSidePanel/messages.ts
+++ app/src/pages/inside/common/testCaseList/testCaseSidePanel/messages.ts
@@ -45,4 +45,8 @@ export const messages = defineMessages({
     id: 'TestCaseSidePanel.noStepsAdded',
     defaultMessage: 'No steps for this scenario yet',
   },
+  descriptionNotSpecified: {
+    id: 'TestCaseSidePanel.descriptionNotSpecified',
+    defaultMessage: 'Description not specified',
+  },
 });
