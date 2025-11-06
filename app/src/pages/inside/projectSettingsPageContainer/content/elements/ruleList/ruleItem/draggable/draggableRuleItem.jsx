e0d16b0a19e580a22328319b7e205aa3177aa492
siarheirazuvalau <siarhei_razuvalau@epam.com>
2025-11-06 17:22:16 +0300

    EPMRPP-109255 || Fix linter errors

--- app/src/pages/inside/projectSettingsPageContainer/content/elements/ruleList/ruleItem/draggable/draggableRuleItem.jsx
+++ app/src/pages/inside/projectSettingsPageContainer/content/elements/ruleList/ruleItem/draggable/draggableRuleItem.jsx
@@ -94,16 +94,14 @@ export const DraggableRuleItem = ({
     <div
       ref={(node) => dropRef(dragPreviewRef(node))}
       style={{ opacity: isInDraggingState ? 0 : 1 }}
-      className={cx(
-        'draggable-rule-item',
-        className,
-        {
-          [`drop-target-${dropTargetType}`]: draggedItemIndex !== null,
-          'is-dragging': isDragging,
-          [itemClassNames[`dropTarget${dropTargetType.charAt(0).toUpperCase() + dropTargetType.slice(1)}`]]: draggedItemIndex !== null,
-          [itemClassNames.isDragging]: isDragging
-        }
-      )}
+      className={cx('draggable-rule-item', className, {
+        [`drop-target-${dropTargetType}`]: draggedItemIndex !== null,
+        'is-dragging': isDragging,
+        [itemClassNames[
+          `dropTarget${dropTargetType.charAt(0).toUpperCase() + dropTargetType.slice(1)}`
+        ]]: draggedItemIndex !== null,
+        [itemClassNames.isDragging]: isDragging,
+      })}
     >
       <RuleItem
         item={item}
