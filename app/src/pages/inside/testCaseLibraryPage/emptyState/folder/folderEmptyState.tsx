dfefcb9e9336231a1ea90abb178eeb80fa0f7601
DmytroChebakov <Happyvibe@0987>
2025-11-13 20:49:36 +0200

    fix open import modal

--- app/src/pages/inside/testCaseLibraryPage/emptyState/folder/folderEmptyState.tsx
+++ app/src/pages/inside/testCaseLibraryPage/emptyState/folder/folderEmptyState.tsx
@@ -30,6 +30,7 @@ import { commonMessages } from '../../commonMessages';
 import { ActionButton } from '../../types';
 
 import styles from './folderEmptyState.scss';
+import { useImportTestCaseModal } from 'pages/inside/testCaseLibraryPage/importTestCaseModal';
 
 const cx = createClassnames(styles);
 
