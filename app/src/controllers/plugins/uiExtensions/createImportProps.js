b07fb46c6aa3ab0086d4ead3205d43f99e25a2c2
siarheirazuvalau <siarhei_razuvalau@epam.com>
2025-12-02 00:34:12 +0300

    EPMRPP-109558 || Enhance AttributeEditor with keyMenuClassName/valueMenuClassName and isAttributeValueRequired prop

--- app/src/controllers/plugins/uiExtensions/createImportProps.js
+++ app/src/controllers/plugins/uiExtensions/createImportProps.js
@@ -167,6 +167,7 @@ import { RuleList, ItemContent } from 'components/main/ruleList';
 import { RuleListHeader } from 'components/main/ruleListHeader';
 import { getGroupedDefectTypesOptions } from 'pages/inside/common/utils';
 import { DEFECT_TYPES_SEQUENCE, TO_INVESTIGATE } from 'common/constants/defectTypes';
+import { METHOD_TYPES_SEQUENCE } from 'common/constants/methodTypes';
 import {
   getDefaultTestItemLinkParams,
   getItemNameConfig,
@@ -338,6 +339,7 @@ export const createImportProps = (pluginName) => ({
     PROJECT_SETTINGS_TAB_PAGE,
     ALL,
     DEFECT_TYPES_SEQUENCE,
+    METHOD_TYPES_SEQUENCE,
     TO_INVESTIGATE,
     STATS_PB_TOTAL,
     STATS_AB_TOTAL,
