0ea0fd27ae63078c7a02f7d3cf0229e0e2179503
Yauheni Shypila <yauheni_shypila@epam.com>
2026-01-23 15:00:59 +0300

    fix imports

--- app/src/pages/inside/common/requirementsList/requirementsList.tsx
+++ app/src/pages/inside/common/requirementsList/requirementsList.tsx
@@ -14,11 +14,11 @@
  * limitations under the License.
  */
 
-import { createClassnames, copyToClipboard } from 'common/utils';
 import { Button, CopyIcon } from '@reportportal/ui-kit';
+import { createClassnames, copyToClipboard } from 'common/utils';
 import { Requirement } from 'pages/inside/testCaseLibraryPage/types';
-import styles from './requirementsList.scss';
 import { processText } from './utils';
+import styles from './requirementsList.scss';
 
 const cx = createClassnames(styles);
 
