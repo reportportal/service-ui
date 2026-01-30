a08291c882565de935fb8c90f52c80ffbd3201a7
Ilya_Hancharyk <Ilya_Hancharyk@epam.com>
2026-01-30 14:39:33 +0100

    EPMRPP-110780 || DatePicker single field for organization pages

--- app/src/components/main/dateRange/dateRangeFormField.jsx
+++ app/src/components/main/dateRange/dateRangeFormField.jsx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 EPAM Systems
+ * Copyright 2026 EPAM Systems
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,31 +15,71 @@
  */
 
 import PropTypes from 'prop-types';
-import { DateRange } from './dateRange';
+import classNames from 'classnames/bind';
+import { useIntl, defineMessages } from 'react-intl';
+import { DatePicker } from '@reportportal/ui-kit';
+import styles from './dateRangeFormField.scss';
+import { parseFormattedDate, formatDateRangeToMinutesString, setEndOfDay } from './utils';
+import { useCallback } from 'react';
+
+const cx = classNames.bind(styles);
+
+export const messages = defineMessages({
+  customRange: {
+    id: 'DateRange.customRange',
+    defaultMessage: 'Custom range',
+  },
+});
 
 export const DateRangeFormField = ({
   input,
   popperClassName = '',
   calendarClassName = '',
+  onClose,
 }) => {
-  const { value = {}, onChange } = input;
+  const { formatMessage } = useIntl();
+  const { value = '', onChange } = input;
+  const { startDate, endDate } = parseFormattedDate(value);
+
+  const handleDateChange = useCallback(
+    ([startDate, endDate] = []) => {
+      const normalizedDateRange = {
+        startDate,
+        endDate: setEndOfDay(endDate),
+      };
+      const formattedValue = formatDateRangeToMinutesString(normalizedDateRange);
+
+      if (startDate && endDate && startDate <= endDate) {
+        onChange(formattedValue);
+        onClose();
+      } else {
+        onChange(formattedValue);
+      }
+    },
+    [onChange, onClose],
+  );
 
   return (
-    <DateRange
-      startDate={value?.startDate}
-      endDate={value?.endDate}
-      onChange={onChange}
-      popperClassName={popperClassName}
-      calendarClassName={calendarClassName}
-    />
+    <div className={cx('date-range-wrapper')}>
+      <div className={cx('title')}>{formatMessage(messages.customRange)}</div>
+      <div className={cx('date-picker-container')}>
+        <DatePicker
+          selectsRange
+          value={[startDate, endDate]}
+          onChange={handleDateChange}
+          popperClassName={popperClassName}
+          calendarClassName={calendarClassName}
+        />
+      </div>
+    </div>
   );
 };
-
 DateRangeFormField.propTypes = {
   input: PropTypes.shape({
     value: PropTypes.object,
     onChange: PropTypes.func,
   }).isRequired,
+  onClose: PropTypes.func,
   popperClassName: PropTypes.string,
   calendarClassName: PropTypes.string,
 };
