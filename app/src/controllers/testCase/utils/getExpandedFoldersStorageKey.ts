4664ba706197ae1b4764e4ff5d7dfce586afbf3f
Yauheni Shypila <yauheni_shypila@epam.com>
2026-02-07 18:21:54 +0300

    fix util

--- app/src/controllers/testCase/utils/getExpandedFoldersStorageKey.ts
+++ app/src/controllers/testCase/utils/getExpandedFoldersStorageKey.ts
@@ -1,5 +1,8 @@
 import { EXPANDED_FOLDERS_IDS } from 'common/constants/localStorageKeys';
+import { useTestPlanId } from 'hooks/useTypedSelector';
 import { TMS_INSTANCE_KEY } from 'pages/inside/common/constants';
 
-export const getExpandedFoldersStorageKey = (instanceKey?: TMS_INSTANCE_KEY) =>
-  instanceKey ? `${EXPANDED_FOLDERS_IDS}_${instanceKey}` : EXPANDED_FOLDERS_IDS;
+export const getExpandedFoldersStorageKey = (instanceKey?: TMS_INSTANCE_KEY) =>{
+   const testPlanId = useTestPlanId() || '';
+   
+  return instanceKey ? `${EXPANDED_FOLDERS_IDS}_${instanceKey}${testPlanId}` : EXPANDED_FOLDERS_IDS;}
