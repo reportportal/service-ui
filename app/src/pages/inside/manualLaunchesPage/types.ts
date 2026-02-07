3f36c931a768c2108dbe96696db5aa59c6706a6a
Yauheni Shypila <yauheni_shypila@epam.com>
2026-02-07 17:42:55 +0300

    revert

--- app/src/pages/inside/manualLaunchesPage/types.ts
+++ app/src/pages/inside/manualLaunchesPage/types.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2026 EPAM Systems
+ * Copyright 2025 EPAM Systems
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-import { Folder, TransformedFolder } from 'controllers/testCase';
 import { PageInfo } from 'controllers/testPlan';
 
 export interface LaunchAttribute {
@@ -139,5 +138,3 @@ export interface UrlsHelper {
     query?: Record<string, unknown>,
   ) => string;
 }
-
-export type FoldersSelectorType = (state: unknown) => Folder[] | TransformedFolder[];
