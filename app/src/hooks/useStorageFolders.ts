3d58c670d440460ee2f68431de44b44d2eba0e18
Yauheni Shypila <yauheni_shypila@epam.com>
2026-02-07 18:24:02 +0300

    revert

--- app/src/hooks/useStorageFolders.ts
+++ app/src/hooks/useStorageFolders.ts
@@ -1,19 +1,3 @@
-/*
- * Copyright 2026 EPAM Systems
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import { useCallback, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -23,7 +7,7 @@ import {
   expandedFolderIdsSelector,
   toggleFolderExpansionAction,
   expandFoldersToLevelAction,
-  Folder,
+  foldersSelector,
 } from 'controllers/testCase';
 import {
   manualLaunchExpandedFolderIdsSelector,
@@ -32,8 +16,6 @@ import {
   expandManualLaunchFoldersToLevelAction,
 } from 'controllers/manualLaunch';
 import { getExpandedFoldersStorageKey } from 'controllers/testCase/utils/getExpandedFoldersStorageKey';
-
-import { useTestPlanId } from './useTypedSelector';
 
 // Config mapper for each instance
 const STORAGE_FOLDERS_CONFIG = {
@@ -65,7 +47,7 @@ export const useStorageFolders = (instanceKey: TMS_INSTANCE_KEY) => {
   const expandedIds = useSelector(expandedIdsSelector);
   const folders = useSelector(foldersSelector);
   const isMountedRef = useRef(false);
-  
+
   useEffect(() => {
     if (!isMountedRef.current) {
       isMountedRef.current = true;
