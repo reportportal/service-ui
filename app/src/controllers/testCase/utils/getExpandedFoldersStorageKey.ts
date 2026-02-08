f8a900d828bd6f8ed42382d930421dcc94cafb81
Yauheni Shypila <yauheni_shypila@epam.com>
2026-02-08 16:57:58 +0300

    fix

--- app/src/controllers/testCase/utils/getExpandedFoldersStorageKey.ts
+++ app/src/controllers/testCase/utils/getExpandedFoldersStorageKey.ts
@@ -2,4 +2,4 @@ import { EXPANDED_FOLDERS_IDS } from 'common/constants/localStorageKeys';
 import { TMS_INSTANCE_KEY } from 'pages/inside/common/constants';
 
 export const getExpandedFoldersStorageKey = (instanceKey?: TMS_INSTANCE_KEY) =>
-   instanceKey ? `${EXPANDED_FOLDERS_IDS}_${instanceKey}` : EXPANDED_FOLDERS_IDS;
+  instanceKey ? `${EXPANDED_FOLDERS_IDS}_${instanceKey}` : EXPANDED_FOLDERS_IDS;
