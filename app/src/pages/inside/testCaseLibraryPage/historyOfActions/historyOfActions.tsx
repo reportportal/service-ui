4bc321107ac1f18f079ed2cc0d03a32fa5e1ed5b
Saveli_S <45118964+Saveli0S@users.noreply.github.com>
2026-04-07 14:42:16 +0300

    EPMRPP-104659: Viewer or Editor can view the history of actions for s… (#5183)

--- app/src/pages/inside/testCaseLibraryPage/historyOfActions/historyOfActions.tsx
+++ app/src/pages/inside/testCaseLibraryPage/historyOfActions/historyOfActions.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 EPAM Systems
+ * Copyright 2026 EPAM Systems
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,70 +14,166 @@
  * limitations under the License.
  */
 
+import { useEffect, useMemo, useState } from 'react';
 import { useIntl } from 'react-intl';
-import { Table } from '@reportportal/ui-kit';
+import { useSelector } from 'react-redux';
+import { BubblesLoader, Pagination, Table } from '@reportportal/ui-kit';
 
 import { createClassnames } from 'common/utils';
+import { COMMON_LOCALE_KEYS } from 'common/constants/localization';
+import NoResultsIcon from 'common/img/newIcons/no-results-icon-inline.svg';
+import { EmptyPageState } from 'pages/common/emptyPageState/emptyPageState';
+import { AbsRelTime } from 'components/main/absRelTime';
 import { SettingsLayout } from 'layouts/settingsLayout';
 import { UserAvatar } from 'pages/inside/common/userAvatar';
 import { ScrollWrapper } from 'components/main/scrollWrapper';
-import { AbsRelTime } from 'components/main/absRelTime';
+import { ITEMS_PER_PAGE_OPTIONS } from 'pages/inside/common/testCaseList/constants';
+import { messages as testCaseListMessages } from 'pages/inside/common/testCaseList/messages';
+import { useURLBoundPagination } from 'pages/inside/common/testCaseList/useURLBoundPagination';
+import { projectKeySelector } from 'controllers/project';
+import { payloadSelector, urlTestCaseSlugSelector } from 'controllers/pages';
+import { testCaseDetailsSelector } from 'controllers/testCase';
+import type { Page } from 'types/common';
+import { useProjectDetails } from 'hooks/useTypedSelector';
 
 import { HistoryOfActionsHeader } from './historyOfActionsHeader';
-import { historyOfActions } from './mockData';
+import {
+  HISTORY_OF_ACTIONS_NAMESPACE,
+  HistoryOfActionsPageDefaultValues,
+  TMS_TEST_CASE_OBJECT_TYPE,
+} from './constants';
 import { messages } from './messages';
+import { useTestCaseActivityHistory } from './useTestCaseActivityHistory';
+import { flattenActivityContent } from './utils';
 
 import styles from './historyOfActions.scss';
 
 const cx = createClassnames(styles);
 
+const emptyPageData: Page = {
+  number: 1,
+  size: HistoryOfActionsPageDefaultValues.limit,
+  totalElements: 0,
+  totalPages: 0,
+};
+
 export const HistoryOfActions = () => {
   const { formatMessage } = useIntl();
-  // Temporary name. Need to get it in response.
-  const testCaseName = '24.2 PV';
+  const { organizationSlug, projectSlug } = useProjectDetails();
+  const payload = useSelector(payloadSelector);
+  const testCaseId = useSelector(urlTestCaseSlugSelector);
+  const projectKey = String(useSelector(projectKeySelector) ?? '');
+  const testCaseDetails = useSelector(testCaseDetailsSelector);
 
-  const currentHistory = historyOfActions.map(
-    ({ time, user: { name, id }, action, oldValue, newValue }) => ({
-      id: time,
-      time: {
-        component: time ? (
-          <AbsRelTime startTime={time} customClass={cx('history-of-actions__table-cell-time')} />
-        ) : (
-          <span>n/a</span>
-        ),
-      },
-      user: {
-        component: (
-          <div className={cx('history-of-actions__table-cell-user')}>
-            {id ? (
-              <UserAvatar
-                className={cx('history-of-actions__table-cell-user-avatar')}
-                userId={id}
-                thumbnail
+  const [pageData, setPageData] = useState<Page>(emptyPageData);
+
+  const testCasePageRoute = payload?.testCasePageRoute ?? '';
+  const baseUrl = testCaseId
+    ? `/organizations/${organizationSlug}/projects/${projectSlug}/testLibrary/test-cases/${testCaseId}/historyOfActions`
+    : `/organizations/${organizationSlug}/projects/${projectSlug}/testLibrary/${testCasePageRoute}`;
+
+  const { setPageNumber, setPageSize, captions, activePage, pageSize, offset } =
+    useURLBoundPagination({
+      pageData,
+      defaultQueryParams: HistoryOfActionsPageDefaultValues,
+      namespace: HISTORY_OF_ACTIONS_NAMESPACE,
+      shouldSaveUserPreferences: true,
+      baseUrl,
+    });
+
+  const { content: fetchedContent, page: fetchedPage, isLoading: isFetching } =
+    useTestCaseActivityHistory(projectKey, testCaseId, offset, pageSize);
+
+  useEffect(() => {
+    if (fetchedPage) {
+      setPageData(fetchedPage);
+    }
+  }, [fetchedPage]);
+
+  const tableRows = useMemo(() => flattenActivityContent(fetchedContent), [fetchedContent]);
+
+  const testCaseIdNumber = Number(testCaseId);
+  const testCaseName = useMemo(() => {
+    if (testCaseDetails?.id === testCaseIdNumber && testCaseDetails?.name) {
+      return testCaseDetails.name;
+    }
+    const first = fetchedContent[0];
+
+    if (first?.object_type === TMS_TEST_CASE_OBJECT_TYPE && first.object_name) {
+      return first.object_name;
+    }
+    return testCaseId || '';
+  }, [testCaseDetails, testCaseIdNumber, testCaseId, fetchedContent]);
+
+  const currentHistory = useMemo(
+    () =>
+      tableRows.map(({ rowKey, item, historyEntry }) => {
+        const {
+          created_at: createdAt,
+          subject_name: subjectName,
+          subject_id: subjectId,
+          event_name: eventName,
+        } = item;
+        const userIdNumeric = Number(subjectId);
+        const hasNumericUserId = subjectId !== '' && !Number.isNaN(userIdNumeric);
+
+        const oldRaw = historyEntry?.oldValue;
+        const newRaw = historyEntry?.newValue;
+        const oldDisplay = oldRaw !== undefined && oldRaw !== '' ? oldRaw : '-';
+        const newDisplay = newRaw !== undefined && newRaw !== '' ? newRaw : '-';
+
+        return {
+          id: rowKey,
+          time: {
+            component: createdAt ? (
+              <AbsRelTime
+                startTime={createdAt}
+                customClass={cx('history-of-actions__table-cell-time')}
               />
             ) : (
-              <div className={cx('history-of-actions__table-cell-user-avatar--template')}>
-                {name[0]}
+              <span>{formatMessage(messages.timeNotApplicable)}</span>
+            ),
+          },
+          user: {
+            component: (
+              <div className={cx('history-of-actions__table-cell-user')}>
+                {hasNumericUserId ? (
+                  <UserAvatar
+                    className={cx('history-of-actions__table-cell-user-avatar')}
+                    userId={userIdNumeric}
+                    thumbnail
+                  />
+                ) : (
+                  <div className={cx('history-of-actions__table-cell-user-avatar--template')}>
+                    {subjectName?.[0] ?? '?'}
+                  </div>
+                )}
+                <span>{subjectName}</span>
               </div>
-            )}
-            <span>{name}</span>
-          </div>
-        ),
-      },
-      action,
-      oldValue: {
-        content: oldValue || '-',
-      },
-      newValue: {
-        content: newValue || '-',
-      },
-    }),
+            ),
+          },
+          action: eventName,
+          oldValue: {
+            content: oldDisplay,
+            component: (
+              <div className={cx('history-of-actions__table-cell-value')}>{oldDisplay}</div>
+            ),
+          },
+          newValue: {
+            content: newDisplay,
+            component: (
+              <div className={cx('history-of-actions__table-cell-value')}>{newDisplay}</div>
+            ),
+          },
+        };
+      }),
+    [tableRows, formatMessage],
   );
 
   const primaryColumn = {
     key: 'time',
     header: formatMessage(messages.time),
-    width: 100,
+    width: 120,
     align: 'left' as const,
   };
 
@@ -108,26 +204,63 @@ export const HistoryOfActions = () => {
     },
   ];
 
+  const showPagination = Boolean(fetchedPage?.totalElements);
+  const showEmptyState = !isFetching && tableRows.length === 0;
+
   return (
     <SettingsLayout>
-      <ScrollWrapper resetRequired={false}>
-        <div className={cx('history-of-actions')}>
-          <HistoryOfActionsHeader
-            testCaseName={testCaseName}
-            className={cx('history-of-actions__header')}
-          />
-          <div className={cx('history-of-actions__main-content')}>
-            <Table
-              data={currentHistory}
-              fixedColumns={fixedColumns}
-              primaryColumn={primaryColumn}
-              sortableColumns={[]}
-              className={cx('history-of-actions__table')}
-              rowClassName={cx('history-of-actions__table-row')}
+      <div className={cx('history-of-actions-page')}>
+        <div className={cx('history-of-actions-page__scroll')}>
+          <ScrollWrapper resetRequired={false}>
+            <div className={cx('history-of-actions')}>
+              <HistoryOfActionsHeader
+                testCaseName={testCaseName}
+                className={cx('history-of-actions__header')}
+              />
+              <div className={cx('history-of-actions__main-content')}>
+                {isFetching && (
+                  <div className={cx('history-of-actions__loader')}>
+                    <BubblesLoader />
+                  </div>
+                )}
+                {showEmptyState && (
+                  <div className={cx('history-of-actions__empty-state')}>
+                    <EmptyPageState
+                      label={formatMessage(COMMON_LOCALE_KEYS.NO_RESULTS)}
+                      description={formatMessage(testCaseListMessages.noResultsDescription)}
+                      emptyIcon={NoResultsIcon as unknown as string}
+                    />
+                  </div>
+                )}
+                {!showEmptyState && !isFetching && (
+                  <Table
+                    data={currentHistory}
+                    fixedColumns={fixedColumns}
+                    primaryColumn={primaryColumn}
+                    sortableColumns={[]}
+                    className={cx('history-of-actions__table')}
+                    rowClassName={cx('history-of-actions__table-row')}
+                  />
+                )}
+              </div>
+            </div>
+          </ScrollWrapper>
+        </div>
+        {showPagination && fetchedPage && (
+          <div className={cx('history-of-actions-page__pagination')}>
+            <Pagination
+              pageSize={pageSize}
+              activePage={activePage}
+              totalItems={fetchedPage.totalElements}
+              totalPages={fetchedPage.totalPages}
+              pageSizeOptions={ITEMS_PER_PAGE_OPTIONS}
+              changePage={setPageNumber}
+              changePageSize={setPageSize}
+              captions={captions}
             />
           </div>
-        </div>
-      </ScrollWrapper>
+        )}
+      </div>
     </SettingsLayout>
   );
 };
