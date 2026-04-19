8b87c8ec4d61107301ef97e421deea69be544d90
vahesamsonyan <vahe_samsonyan@epam.com>
2026-04-19 15:15:07 +0400

    Refactor project sidebar to improve icon handling and link payload structure

--- app/src/layouts/projectLayout/projectSidebar/projectSidebar.jsx
+++ app/src/layouts/projectLayout/projectSidebar/projectSidebar.jsx
@@ -217,17 +217,19 @@ export const ProjectSidebar = ({ onClickNavBtn }) => {
       message: formatMessage(messages.projectsSettings),
       menuOrder: (menuCounter += menuStep),
     });
-    projectPageExtensions.forEach(({ icon, internalRoute, name, title, iconName, menuOrder }) => {
-      if (icon) {
+    projectPageExtensions.forEach(({ payload }) => {
+      const { icon, slug, name, title, iconName, menuOrder } = payload;
+      const iconSvg = icon?.content || icon?.svg;
+      if (iconSvg) {
         const itemName = iconName || title;
         sidebarItems.push({
           onClick: (isSidebarCollapsed) => onClickButton({ itemName, isSidebarCollapsed }),
           link: {
             type: PROJECT_PLUGIN_PAGE,
-            payload: { organizationSlug, projectSlug, pluginPage: internalRoute || name },
+            payload: { organizationSlug, projectSlug, pluginPage: slug || name },
           },
-          icon: icon.svg,
-          message: icon.title || title,
+          icon: iconSvg,
+          message: title,
           menuOrder: menuOrder || (menuCounter += menuStep),
         });
       }
