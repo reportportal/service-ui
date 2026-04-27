330ce75b364359b9a47421499a271299446ee920
Naviy Serikzhan <seriknavii@gmail.com>
2026-04-27 18:37:24 +0500

    EPMRPP-114631 || Copy icon has wrong background on hover (#5279)
    * EPMRPP-114631 || Copy icon has wrong background on hover
    
    Co-authored-by: siarheirazuvalau <siarhei_razuvalau@epam.com>

--- app/src/components/buttons/copyClipboardButton/copyClipboardButton.jsx
+++ app/src/components/buttons/copyClipboardButton/copyClipboardButton.jsx
@@ -20,8 +20,7 @@ import classNames from 'classnames/bind';
 import { defineMessages, useIntl } from 'react-intl';
 import Parser from 'html-react-parser';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { Button, Tooltip } from '@reportportal/ui-kit';
-import IconDuplicate from 'common/img/duplicate-inline.svg';
+import { BaseIconButton, CopyIcon, Tooltip } from '@reportportal/ui-kit';
 import IconTick from 'common/img/newIcons/tick-inline.svg';
 import styles from './copyClipboardButton.scss';
 
@@ -31,6 +30,10 @@ const messages = defineMessages({
   copyToClipboard: {
     id: 'ClipboardButton.copyToClipboard',
     defaultMessage: 'Copied to clipboard',
+  },
+  copy: {
+    id: 'ClipboardButton.copy',
+    defaultMessage: 'Copy to clipboard',
   },
 });
 
@@ -50,11 +53,13 @@ export const ClipboardButton = ({ text, className }) => {
           <div className={cx('popover-message')}>{Parser(IconTick)}</div>
         </Tooltip>
       ) : (
-        <Button onClick={copyToClipboardHandler} className={cx('clipboard')}>
-          <CopyToClipboard className={cx('copy')} text={text}>
-            {Parser(IconDuplicate)}
-          </CopyToClipboard>
-        </Button>
+        <CopyToClipboard text={text} onCopy={copyToClipboardHandler}>
+          <BaseIconButton aria-label={formatMessage(messages.copy)} className={cx('clipboard')}>
+            <span className={cx('copy')}>
+              <CopyIcon />
+            </span>
+          </BaseIconButton>
+        </CopyToClipboard>
       )}
     </div>
   );
