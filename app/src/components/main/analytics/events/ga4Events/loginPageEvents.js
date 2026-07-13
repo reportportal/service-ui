8df9539d5a387c92a4820e6f5c5cd630f5d0ba8e
Vahe Samsonyan <90085019+VaheSamsonyan@users.noreply.github.com>
2026-07-13 13:41:53 +0400

    added some missing events on login page (#5533)
    Co-authored-by: vahesamsonyan <vahe_samsonyan@epam.com>

--- app/src/components/main/analytics/events/ga4Events/loginPageEvents.js
+++ app/src/components/main/analytics/events/ga4Events/loginPageEvents.js
@@ -58,4 +58,13 @@ export const LOGIN_PAGE_EVENTS = {
     ...getBasicClickEventParameters(category),
     link_name: normalizeEventParameter(linkText),
   }),
+  clickOnServiceVersionsModal: (category) => ({
+    ...getBasicClickEventParameters(category),
+    modal: 'versions_of_connected_services',
+  }),
+  clickOnServiceUpdateLink: (category, serviceType) => ({
+    ...getBasicClickEventParameters(category),
+    element_name: 'update',
+    type: normalizeEventParameter(serviceType),
+  }),
 };
--- app/src/layouts/common/appSidebar/helpAndService/modals/versionsOfConnectedServicesModal/versionsOfConnectedServicesModal.jsx
+++ app/src/layouts/common/appSidebar/helpAndService/modals/versionsOfConnectedServicesModal/versionsOfConnectedServicesModal.jsx
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { useIntl } from 'react-intl';
@@ -47,6 +47,10 @@ const VersionsOfConnectedServices = ({ data: { latestServiceVersions, analyticsC
   const hideModal = () => dispatch(hideModalAction());
   const releaseNotesCategory = analyticsCategory || SIDEBAR_CATEGORY;
   const productVersionLabel = formatMessage(messages.productVersion, { version: PRODUCT_VERSION });
+
+  useEffect(() => {
+    trackEvent(LOGIN_PAGE_EVENTS.clickOnServiceVersionsModal(releaseNotesCategory));
+  }, [releaseNotesCategory, trackEvent]);
 
   const handleReleaseNotesClick = () => {
     trackEvent(LOGIN_PAGE_EVENTS.clickOnReleaseNotesLink(releaseNotesCategory, productVersionLabel));
@@ -114,6 +118,7 @@ const VersionsOfConnectedServices = ({ data: { latestServiceVersions, analyticsC
         <ServiceVersion
           service={service}
           content={formatMessage(COMMON_LOCALE_KEYS.UPDATE)}
+          analyticsCategory={releaseNotesCategory}
           key={service.linkTo}
         />
       ))}
