9cd44d0242dd3d6939aed9644c644ea414ebedbb
Alla Prishchepa <tyurina.alla@gmail.com>
2026-07-29 11:21:06 +0400

    EPMRPP-117346 || Revert PreservedText changes

--- app/src/components/preservedText/preservedText.tsx
+++ app/src/components/preservedText/preservedText.tsx
@@ -27,13 +27,6 @@ interface PreservedTextProps {
   wrap?: boolean;
 }
 
-export const PreservedText = ({
-  children,
-  className,
-  wrap = false,
-  ...rest
-}: PreservedTextProps) => (
-  <span className={cx('preserved-text', { wrap }, className)} {...rest}>
-    {children}
-  </span>
+export const PreservedText = ({ children, className, wrap = false }: PreservedTextProps) => (
+  <span className={cx('preserved-text', { wrap }, className)}>{children}</span>
 );
