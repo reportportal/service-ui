de27659a1dfb01685ca41c3d872e1e48d505123e
vahesamsonyan <vahe_samsonyan@epam.com>
2026-09-17 13:18:06 +0400

    Update testCases page property to allow null values and adjust totalElements on test case removal

--- app/src/controllers/testCase/reducer.ts
+++ app/src/controllers/testCase/reducer.ts
@@ -83,7 +83,7 @@ export type InitialStateType = {
   testCases: {
     isLoading: boolean;
     list: TestCase[];
-    page: Page[];
+    page: Page | null;
   };
 };
 
@@ -184,6 +184,9 @@ const testCasesReducer = (state = INITIAL_STATE.testCases, action: TestCasesActi
       return {
         ...state,
         list: state.list.filter(({ id }) => action.payload.testCase.id !== id),
+        page: state.page
+          ? { ...state.page, totalElements: state.page.totalElements - 1 }
+          : state.page,
       };
     }
     case START_LOADING_TEST_CASES:
